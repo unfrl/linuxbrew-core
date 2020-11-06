1d3b8bd49f0175ad4caf2f6373f185201c5e91f2
Michka Popoff <michkapopoff@gmail.com>
2020-11-06 23:00:16 +0100

    kahip: revision for gcc

--- Formula/kahip.rb
+++ Formula/kahip.rb
@@ -3,7 +3,7 @@ class Kahip < Formula
   homepage "https://algo2.iti.kit.edu/documents/kahip/index.html"
   url "https://algo2.iti.kit.edu/schulz/software_releases/KaHIP_2.12.tar.gz"
   sha256 "b91abdbf9420e2691ed73cea999630e38dfaf0e03157c7a690a998564c652aac"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     sha256 cellar: :any, arm64_big_sur: "2a3a941d05e8ac41cec5522f11fd835159ffb370452cf788adbcfe0d8c68d654"
@@ -11,17 +11,18 @@ class Kahip < Formula
     sha256 cellar: :any, catalina:      "a05c9bfbd38225e3730e10756f1515d833f09f61eccd7745c55dd8b78690b790"
     sha256 cellar: :any, mojave:        "57e35f0a81e0d22f9d8d4438994efcc30295e54865525ba89236f58647f66174"
     sha256 cellar: :any, high_sierra:   "78fda0b177b22dc65d0d9b5116dc842aa023cb027afccd4c2f968f42ac55fada"
-    sha256 cellar: :any, x86_64_linux:  "05033633bb34c3798b8b284177152f6adc35746031f34390ea600d88d5d41045"
   end
 
   depends_on "cmake" => :build
-  depends_on "gcc"
+  depends_on "gcc" if OS.mac?
   depends_on "open-mpi"
 
   def install
-    gcc_major_ver = Formula["gcc"].any_installed_version.major
-    ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}"
-    ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{gcc_major_ver}"
+    if OS.mac?
+      gcc_major_ver = Formula["gcc"].any_installed_version.major
+      ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{gcc_major_ver}"
+      ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{gcc_major_ver}"
+    end
     mkdir "build" do
       system "cmake", *std_cmake_args, ".."
       system "make", "install"
