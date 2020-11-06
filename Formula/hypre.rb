2b716703cbf929ebf89a42e279d1d2545390d252
Michka Popoff <michkapopoff@gmail.com>
2020-11-06 22:58:30 +0100

    hypre: revision for gcc

--- Formula/hypre.rb
+++ Formula/hypre.rb
@@ -4,6 +4,7 @@ class Hypre < Formula
   url "https://github.com/hypre-space/hypre/archive/v2.20.0.tar.gz"
   sha256 "5be77b28ddf945c92cde4b52a272d16fb5e9a7dc05e714fc5765948cba802c01"
   license any_of: ["MIT", "Apache-2.0"]
+  revision 1 unless OS.mac?
   head "https://github.com/hypre-space/hypre.git"
 
   livecheck do
@@ -17,7 +18,6 @@ class Hypre < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "9d732247f823113d50fd5d5e144541ad92dfab1f8df05298426bea24743e693b"
     sha256 cellar: :any_skip_relocation, mojave:        "ecba86de35962ede83358e77986f65ac66e3632c8b255b91fde2b986c10d8c80"
     sha256 cellar: :any_skip_relocation, high_sierra:   "0c88309a25471939828ac8e9f8851b0a83bb9b2a11103837a12291a59351a520"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "910dfabd18c929d300fc150e0debf27a5255a415b9c79293f0692180fcd7c34f"
   end
 
   depends_on "gcc" # for gfortran
