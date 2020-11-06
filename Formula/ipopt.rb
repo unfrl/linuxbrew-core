0112041158b691319a92ea1c4b1b15c34abd2ff7
Michka Popoff <michkapopoff@gmail.com>
2020-11-06 22:59:34 +0100

    ipopt: revision for gcc

--- Formula/ipopt.rb
+++ Formula/ipopt.rb
@@ -4,20 +4,19 @@ class Ipopt < Formula
   url "https://github.com/coin-or/Ipopt/archive/releases/3.13.4.tar.gz"
   sha256 "1fdd0f8ea637856d66b1ebdd7d52ad1b8b8c1142d1a4ce0976b200ab280e5683"
   license "EPL-1.0"
+  revision 1 unless OS.mac?
   head "https://github.com/coin-or/Ipopt.git"
 
   bottle do
-    sha256 cellar: :any,                 arm64_big_sur: "b565cf2a619ac1925cda935b9ded13ee66f339d8d87716eff7778ea6e8364d9c"
-    sha256 cellar: :any,                 big_sur:       "3b0388a537a291d3078a67cccceff9b3d26042a868da1a48fef3dde71d33f26e"
-    sha256 cellar: :any,                 catalina:      "40f3152e3f24b107acad92e645391ffdee423210a1af5e2c0d286ab14aa92d5b"
-    sha256 cellar: :any,                 mojave:        "13409cd330a4e313b67044d868618168dfd2e6c0adf8ff660243c3afedfada4b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1babbc62b828e9cf8cc69314100902aa9f643c0b676edd3f85b779cac4e1c2e1"
+    sha256 cellar: :any, big_sur:      "c63686d9c36309fc55f88e7d9a97b99e34d20ccabd68e66009fdaab1e7ea4f6c"
+    sha256 cellar: :any, catalina:     "9e14aaecd0e58c1047ea13327314a99f30dd0fbc2049af6681aa879f8dccd617"
+    sha256 cellar: :any, mojave:       "ed286516c3ae473b824b5cefa4186e1519aa371464ec80f8c949a9da3eb50475"
   end
 
   depends_on "openjdk" => :build
   depends_on "pkg-config" => [:build, :test]
   depends_on "ampl-mp"
-  depends_on "gcc"
+  depends_on "gcc" # for gfortran
   depends_on "openblas"
 
   resource "mumps" do
@@ -52,8 +51,8 @@ class Ipopt < Formula
       cp "Make.inc/Makefile.inc.generic.SEQ", "Makefile.inc"
       inreplace "Makefile.inc", "@rpath/", "#{opt_lib}/" if OS.mac?
 
-      # Fix for GCC 10
-      inreplace "Makefile.inc", "OPTF    = -fPIC", "OPTF    = -fPIC -fallow-argument-mismatch" if OS.mac?
+      inreplace "Makefile.inc", "OPTF    = -fPIC",
+                "OPTF    = -fPIC -fallow-argument-mismatch"
 
       ENV.deparallelize { system "make", "d" }
 
