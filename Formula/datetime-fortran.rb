43f177959d3aaa772ba14c35676cf8378309f5d6
Michka Popoff <michkapopoff@gmail.com>
2020-11-06 22:53:00 +0100

    datetime-fortran: revision for gcc

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -4,6 +4,7 @@ class DatetimeFortran < Formula
   url "https://github.com/wavebitscientific/datetime-fortran/releases/download/v1.7.0/datetime-fortran-1.7.0.tar.gz"
   sha256 "cff4c1f53af87a9f8f31256a3e04176f887cc3e947a4540481ade4139baf0d6f"
   license "MIT"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "e2986c9fde31f0bb075f4399c251599b481e2c9f5509e9ba6aa56e2fd8f2c939"
@@ -11,7 +12,6 @@ class DatetimeFortran < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "82d8b0e2a51fb7df321659ed4f5da43c24edd5aba81e5e05250508b541f2eb4b"
     sha256 cellar: :any_skip_relocation, mojave:        "ef59feabc30610c41a5ac4b2e594f1378d3edeb3b13dd7912825c48815d547e2"
     sha256 cellar: :any_skip_relocation, high_sierra:   "cf59b21c0539aa14f5e0274387669d13dae47b3e11267cdb1baed8545f2bd535"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "feb4d7b3d80d6171b60cbaeef71edddc00a6b65467d24750bc4d583f6fd8ad3d"
   end
 
   head do
