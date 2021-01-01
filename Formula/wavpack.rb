e31d7463b765c91116c94d8c470797964f63cde4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-01 12:31:29 +0000

    wavpack: update 5.3.0 bottle.

--- Formula/wavpack.rb
+++ Formula/wavpack.rb
@@ -12,6 +12,7 @@ class Wavpack < Formula
     sha256 "53e06631a0fd39762ad7d858b059df1a45af5244f1d1951832ae6c7faabba3d9" => :arm64_big_sur
     sha256 "faef125fe8557e79bc97e1ded99e0050997fff0aba8cf0628771b686d5251244" => :catalina
     sha256 "f8237f75aa50b6e8bd97bb4e3729a477ac569936d63d67a0df6e665ec4069a59" => :mojave
+    sha256 "838c246e398708e4073246afe94a621e54e091d4a0259b57a96a11814d93ffd3" => :x86_64_linux
   end
 
   head do
