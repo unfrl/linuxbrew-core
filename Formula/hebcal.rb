1086aac2bf9f2aee9702d3cfcd8dac460af07cf9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-04 18:52:29 +0000

    hebcal: update 4.24 bottle.

--- Formula/hebcal.rb
+++ Formula/hebcal.rb
@@ -11,6 +11,7 @@ class Hebcal < Formula
     sha256 "33af9a0346510a27b1e046bcd8d7d4e734216d578ad907a54ebb9ad6aae3ace9" => :arm64_big_sur
     sha256 "21a0b493d2ce497baed5a0badb60a312f5e96afb8b2afbe1d8eecc584d885207" => :catalina
     sha256 "dea0961f7ee0fe3420f4b3eed313016897a1c2aca43e4b50a0936ee2b9c6653e" => :mojave
+    sha256 "0f5e4217004a2b4df6a82a78f0a9f054f57e384975260b780c8d773262077d57" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
