52da42ff5a2bb9d91477b0414b1d98e9414f0c37
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-04 21:10:08 +0000

    forcecli: update 0.29.0 bottle.

--- Formula/forcecli.rb
+++ Formula/forcecli.rb
@@ -12,6 +12,7 @@ class Forcecli < Formula
     sha256 "d67c0177daaa47d31fb17fe1a48dde54ccf14168d7bf821cfc6cc3a1578808ea" => :arm64_big_sur
     sha256 "12c3c9887210c8dc606980b268e5bde2b3ae9ec15dc45e4ee7eac35e38c48ab1" => :catalina
     sha256 "32ed79f122d17880baa3ead235ce8c3624a5663f45213020726bc3f6949c4988" => :mojave
+    sha256 "aaa1e478e1241f9673eb160e9d585b2495c2bfa91dc2318c41d50567fb75ea2f" => :x86_64_linux
   end
 
   depends_on "go" => :build
