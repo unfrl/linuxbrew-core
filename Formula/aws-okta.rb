051ab7dd832434aad16c2bf0b43c60724a481958
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-04 21:11:32 +0000

    aws-okta: update 1.0.10 bottle.

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -11,6 +11,7 @@ class AwsOkta < Formula
     sha256 "1d00b2be99691ee24c1d5f8991aadb197f098bed8525687c0de2b985e31d8bb9" => :arm64_big_sur
     sha256 "91ae8da87a6ce6e55d098e937fbd3ceb442f9e7150c01bd6d277b4e22bb408d5" => :catalina
     sha256 "b015f08f2f46be5f46479e451aed41005e205bccd1aed51e6b4497c6287b8d38" => :mojave
+    sha256 "4908bd6279a225cc32cb70c18677c18cc6cac71c437b3eb1dc0220269891c6c3" => :x86_64_linux
   end
 
   depends_on "go" => :build
