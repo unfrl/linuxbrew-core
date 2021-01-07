567358218cdf513dbd0780a914eb987142ecb147
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-07 12:44:58 +0000

    vcs: update 1.13.4_3 bottle.

--- Formula/vcs.rb
+++ Formula/vcs.rb
@@ -16,6 +16,7 @@ class Vcs < Formula
     sha256 "5a12b2c51afec7626a44463cd9145bf47819e8561f05194f2f17ea8eec0459c9" => :arm64_big_sur
     sha256 "3ae09912577433e9aee40da787b21b278d2e4d625454e6a554a10dfd71a3cb82" => :catalina
     sha256 "2100a37453706602e0bd5941c7fb343cf64659493b27889957bad498934c6daf" => :mojave
+    sha256 "9d4c055cb44e622d78a53d67cab6927f5671780bb515d4f87c9028f514ad6444" => :x86_64_linux
   end
 
   depends_on "ffmpeg"
