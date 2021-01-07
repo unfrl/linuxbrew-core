17dc66da4f7972e4b61b6c9d0bc8892957441d2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-07 12:43:10 +0000

    vgmstream: update r1050-3448-g77cc431b_2 bottle.

--- Formula/vgmstream.rb
+++ Formula/vgmstream.rb
@@ -22,6 +22,7 @@ class Vgmstream < Formula
     sha256 "f344401ea028c6fced781b98573acc97648380cbb3da37fccb614543528d58b3" => :arm64_big_sur
     sha256 "ea5a421a93602621a8bf2a62b2eca9affa50790f16d7153bd3f901ef3edd9d9a" => :catalina
     sha256 "1641ceee1b1849446b3aa2c1ccd07241c1641c9546fff3f785ae5f842b695fcc" => :mojave
+    sha256 "2df44ac21efd85d3bec303e8128499abe3bd88f63a1cea1b7fbcc2ff5e247eed" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
