be7dbca369517a3746035a3919de3e6eac411f9a
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-01-07 21:20:25 +0000

    stormssh: update 0.7.0_6 bottle.

--- Formula/stormssh.rb
+++ Formula/stormssh.rb
@@ -16,6 +16,7 @@ class Stormssh < Formula
     sha256 "aa25e63f9cf6ff6d69e1c7b48dce6834de0ce4ed2d74af6775212ef1c9314f52" => :arm64_big_sur
     sha256 "91c073021a2155e31ecfc09a7e5e2e03172c34408f31c725895b0ddd1bcf9a31" => :catalina
     sha256 "ffeaf80fd8e4ff994985be761d55d90fd4a60a4198b3aa87983b8933dab8b4b4" => :mojave
+    sha256 "3058cc10a40a7fe66e2c9bb369aef73713733e9536fe63a9b793e561638596ab" => :x86_64_linux
   end
 
   depends_on "python@3.9"
