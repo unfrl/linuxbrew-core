6116a74bcdc40077a20720a59843788dd5363adb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-08 06:56:28 +0000

    onednn: update 2.0 bottle.

--- Formula/onednn.rb
+++ Formula/onednn.rb
@@ -13,6 +13,7 @@ class Onednn < Formula
     sha256 "045f62a40883229b10146c715306d1412f6f0bc4828d45942dbba29f03bb3c12" => :arm64_big_sur
     sha256 "669ffb542483c34cf208829b6fe5d8debc60d0e9def35394834676d4db42c4cd" => :catalina
     sha256 "aa2922eb9ba741cde52d2b74f8787fdcec61e478b0d949cd6c8795fa3a2560e4" => :mojave
+    sha256 "7dd76ded246bea8dff1a5029c121586016f372b74ed375a5cf87729c7aeb3b8f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
