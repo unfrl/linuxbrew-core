e6af5814acffe003853b99eca064740c4e2386bf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-08 06:45:45 +0000

    ocaml-zarith: update 1.11 bottle.

--- Formula/ocaml-zarith.rb
+++ Formula/ocaml-zarith.rb
@@ -10,6 +10,7 @@ class OcamlZarith < Formula
     sha256 "f70ad5d11b3a1c4eb35cd249c110a6e764bf2079eaf67f08ff7a253034256170" => :big_sur
     sha256 "ad4a1aa81a4a1d2fc136e07e7574b448bb1a2fa75aaceafc109c5e1426e064f3" => :catalina
     sha256 "51fde5a55a28bba380a16a462d0319626c1340e823d425777e8d9189bcfa6759" => :mojave
+    sha256 "13f6a88798d641b2750abbe00dd57eaeb5eb9e07fc37a6c9bffd112d861e0a31" => :x86_64_linux
   end
 
   depends_on "ocaml-findlib" => :build
