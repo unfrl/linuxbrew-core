ba1dd880fc3b30f69f0712d496fd4d600449b863
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-08 18:15:06 +0000

    ssllabs-scan: update 1.5.0 bottle.

--- Formula/ssllabs-scan.rb
+++ Formula/ssllabs-scan.rb
@@ -12,6 +12,7 @@ class SsllabsScan < Formula
     sha256 "445a6d45341a1e6bf831f04eb00e87ca5e39df27685af0da5504e64e3fbb1efb" => :arm64_big_sur
     sha256 "01c7e2503e8793f79149a3115dffff286ab0db876ccbfe6d8bb11ed54f27ba38" => :catalina
     sha256 "eb44e540aba0e6a209ab3820a168184f39ddd470673093c7a3dc87a0e70eab42" => :mojave
+    sha256 "da652c7b483e2fb5184ac0cb10cf07ff60298e400059495afc2d035028628e48" => :x86_64_linux
   end
 
   depends_on "go" => :build
