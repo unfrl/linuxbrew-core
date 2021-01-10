06b3fe5e6d3ea83d80af0fe08c8ac52841adfcd5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-10 20:29:09 +0000

    plzip: update 1.9 bottle.

--- Formula/plzip.rb
+++ Formula/plzip.rb
@@ -17,6 +17,7 @@ class Plzip < Formula
     sha256 "9d197045d0de19677e6ce0079afb3cfb94e54360aa2c00cda3bf5a74031e6b40" => :arm64_big_sur
     sha256 "9f278923d8c1d12bebbfa37b52816b85b8f4cc49e67ef7dc37de7ff1af9ea4ab" => :catalina
     sha256 "fe9addff91b4833212aa8107a92f1775af9cc7d5d96de1466917c3f0a51ee912" => :mojave
+    sha256 "66ac5afff78ede133c3716148535929dc642ee9454cbe5c3c70630d683abb596" => :x86_64_linux
   end
 
   depends_on "lzlib"
