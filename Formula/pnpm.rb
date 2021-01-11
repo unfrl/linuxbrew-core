65dc463c2c53e01c80be2bc6878a0c53a482afff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-11 17:03:11 +0000

    pnpm: update 5.15.0 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -17,6 +17,7 @@ class Pnpm < Formula
     sha256 "6556a252ac49974103df51b819de20ad5080d8c4a85ac92000db43db23cf97c5" => :arm64_big_sur
     sha256 "58a4f0d7ea8b26fbb0062babf463cd157faaf40dcacf691e4d696a8d5dd774eb" => :catalina
     sha256 "bd7e0346d23f01ada41919829075d3e16fc12ab6df780e39fcf0a3ecec532de4" => :mojave
+    sha256 "ecde604c85f6b5afbe09091d6c789c5a3fbaed726b253dade8f35d3f74e6dc45" => :x86_64_linux
   end
 
   depends_on "node"
