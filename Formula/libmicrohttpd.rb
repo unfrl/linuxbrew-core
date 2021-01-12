b24388e2d681bfda92aaef40b5ff6fc8e5df9542
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-12 06:43:29 +0000

    libmicrohttpd: update 0.9.72 bottle.

--- Formula/libmicrohttpd.rb
+++ Formula/libmicrohttpd.rb
@@ -17,6 +17,7 @@ class Libmicrohttpd < Formula
     sha256 "a5964ebfc90189dd2657f81e258b62de76a90f7d77d8db258c786a59baa33a3b" => :arm64_big_sur
     sha256 "2d6f224e3262bf015d7d98faa9c60aa8098937d7940795eeaad5b57c5a410b75" => :catalina
     sha256 "66441caeadac2391b9a3fbf9001a9ef5bfa7cc47eab016da0e972aa3b0fcdbb8" => :mojave
+    sha256 "32a874b16813dc148f82e2f5088c2781d8b9dca7a4fac2779ff5e6fc911e849e" => :x86_64_linux
   end
 
   depends_on "gnutls"
