04d4f34b8383e25b6a9c91fc5ad64af9446dbe9c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-14 06:34:50 +0000

    circleci: update 0.1.11756 bottle.

--- Formula/circleci.rb
+++ Formula/circleci.rb
@@ -12,6 +12,7 @@ class Circleci < Formula
     sha256 "e3f12c7c869c912bd086ab97372703faaf6d95e39ffb75b5479fac7af9ff0f60" => :big_sur
     sha256 "500a3d3cf798e6355ff90cec4c63b01c14b23ad3f0e67fc5a2694ed1ed104b51" => :catalina
     sha256 "68421c697b12db9496115c5b18aaafa6d27a78f29db30659644e93d27fa7fa7d" => :mojave
+    sha256 "66f522942c989b11e369c53b322dda67f4c5aec521e9d7030563b4fb4662734c" => :x86_64_linux
   end
 
   depends_on "go" => :build
