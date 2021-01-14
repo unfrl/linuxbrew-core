211cc673cd259de4b778985b274a54fc389dabb4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-14 18:55:39 +0000

    infracost: update 0.7.13 bottle.

--- Formula/infracost.rb
+++ Formula/infracost.rb
@@ -12,6 +12,7 @@ class Infracost < Formula
     sha256 "feefbc643626faa12de2177731febd010ff7bc7684c3f9f59798b5e19b857f6b" => :arm64_big_sur
     sha256 "d7b91de058dce9e91ef89aab89b9eaff229b21296655bbaaabb1b0832808410b" => :catalina
     sha256 "ff2d87ad5450309eb2f8333058a56984afb56382866eeeb4e1bb11b963f20d5a" => :mojave
+    sha256 "38f8ed530f0a3fe3f48ceada55f419db4b7eb77579c7bccdc5bfe4a1ae16f668" => :x86_64_linux
   end
 
   depends_on "go" => :build
