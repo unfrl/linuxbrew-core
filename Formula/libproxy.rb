c000ea85b94a12beb1498f44125d0d3109e2ca1d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-15 16:36:41 +0000

    libproxy: update 0.4.17_1 bottle.

--- Formula/libproxy.rb
+++ Formula/libproxy.rb
@@ -12,6 +12,7 @@ class Libproxy < Formula
     sha256 "aa72de0f8f5be2c730d84f20308df804c156e61ff321de0a4b63ba5623517ab7" => :arm64_big_sur
     sha256 "c847a5adafa14e2614351edc46fdf1f8884908912845a9e425ce30925bb55e32" => :catalina
     sha256 "5f6f14d95746e1b4c3328f23c7d9018e7e6a1fab70eba1255276ad89c0c405e5" => :mojave
+    sha256 "cb119590a82dcb1a927083ebf0333186585dc50f8ba13352eb5f20d3f337edb3" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
