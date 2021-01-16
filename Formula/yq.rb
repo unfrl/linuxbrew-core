5be6b7a5bb4971cdce1c71351d0d1de5aa027135
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-16 15:04:37 +0000

    yq: update 4.4.0 bottle.

--- Formula/yq.rb
+++ Formula/yq.rb
@@ -11,6 +11,7 @@ class Yq < Formula
     sha256 "fa0e24c4577a96caadad725096c3df00867a208fcf29b7f2fc08086084ea530d" => :arm64_big_sur
     sha256 "d3e295cdef6093bca1e10b4ee734469679c88abe9321743a441174638222f4f7" => :catalina
     sha256 "ec57fd2062c79771cb00f96ae26945a67ecaf82205cba82b33e2c575966859e9" => :mojave
+    sha256 "5478d54f41b2637178eff78a2d468a6933d8bc0f582c8bec144de1ac1f8f38d1" => :x86_64_linux
   end
 
   depends_on "go" => :build
