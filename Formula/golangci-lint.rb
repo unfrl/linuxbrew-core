33b61368c617eae803bbcc99160e385064818eb1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-16 18:13:12 +0000

    golangci-lint: update 1.35.2 bottle.

--- Formula/golangci-lint.rb
+++ Formula/golangci-lint.rb
@@ -14,6 +14,7 @@ class GolangciLint < Formula
     sha256 "789816953076852c07443ca81976247da4ff4d8dcb4ea6f78167daefb1fb752b" => :arm64_big_sur
     sha256 "240072df692296e13fc72d1e35d538a3397ee15d08d9fb9e8c06b463434ccde5" => :catalina
     sha256 "0e6b7d4df47684269fd3293ae30d0b7472c6cb7bfc75f0a3bb249a2072dec4f5" => :mojave
+    sha256 "1d275bb2470ceed277417e4cdca2fb4e39fe1d13deae1d8a7777b57523641ecf" => :x86_64_linux
   end
 
   depends_on "go"
