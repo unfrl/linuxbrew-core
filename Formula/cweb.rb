570b62ed80690e2df6d7c662a790a299300d7b1a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-17 18:48:46 +0000

    cweb: update 3.64c bottle.

--- Formula/cweb.rb
+++ Formula/cweb.rb
@@ -10,6 +10,7 @@ class Cweb < Formula
     sha256 "9a1237298f33f69283f923525eb5668911226d48043024bbbd44d337ad1bcdbe" => :arm64_big_sur
     sha256 "e100640669a1d066177514aae1c813f7c18b530c4cae5744d0431d850933648c" => :catalina
     sha256 "410376faad622cd11745ca94c877135c1b4837ccdc8c7cab43bf12b4b849a3b9" => :mojave
+    sha256 "05b3cb67649ef73f131fd901d5f2fdf99fbb6e7215e253b9a06bc7486ac169ca" => :x86_64_linux
   end
 
   def install
