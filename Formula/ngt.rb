3734920958e1630d9f4ee0d30ac864b92ece3668
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-18 17:18:22 +0000

    ngt: update 1.13.1 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -16,6 +16,7 @@ class Ngt < Formula
     sha256 "726916f6ef145e1a541a84526842d0a4cdc922ebd990b102a69346aea2d232a5" => :arm64_big_sur
     sha256 "85514f57252ce423ba7499b56ed88783aaed73ecc9c68f5a76337ae73de91ac5" => :catalina
     sha256 "bbf7d4d86273b6f28c05e8e1f736631b7614d67171d3127b8b746d0b730c94e1" => :mojave
+    sha256 "fa9fd00eff5b762cb6fb33677ee8e4672e7c85d55b140f91bf2ae7c7c4f88205" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
