64e37aa00a7f797d85dca469b00665f69c0c084c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-23 06:53:53 +0000

    terrascan: update 1.3.1 bottle.

--- Formula/terrascan.rb
+++ Formula/terrascan.rb
@@ -12,6 +12,7 @@ class Terrascan < Formula
     sha256 "2b44fb20d0dd5794a028d0c897f3335bb4d1aa4f23cec2be26a58c473977c9f2" => :arm64_big_sur
     sha256 "c2b0cb042bf6b3739e77d391eda6e3b1eceffc3d746c81194772daa074426ffd" => :catalina
     sha256 "bab112682da7663edec11998f40bf43753ee8793bd1107722e92376c7ffe2fa4" => :mojave
+    sha256 "8a0e8212847fdc81b9c8b4c573bbadd541cfc4eb5a62fda4eacd4884b2597de6" => :x86_64_linux
   end
 
   depends_on "go" => :build
