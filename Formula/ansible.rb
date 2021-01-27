463b675b9165cef814b3f33235f7272098399e2b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-27 18:41:13 +0000

    ansible: update 2.10.6 bottle.

--- Formula/ansible.rb
+++ Formula/ansible.rb
@@ -13,11 +13,12 @@ class Ansible < Formula
   end
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "bdd2272c70e47285e9ed7ccc9d094767534859c603014a0df6a7de045ac0fb70" => :big_sur
     sha256 "05db477b8271ef4449e33b337b53424f662ba6f486904554acbff73a96ee88c9" => :arm64_big_sur
     sha256 "3ca33d8ce299e790af2cfde89dc70a83ffe616c7825bfeceb4e55b54ca24a58a" => :catalina
     sha256 "0529471e340c371514b0f8cf7f05fb4e14c2733d094cfdd76c0b5f80a8cdb4f6" => :mojave
+    sha256 "dd0fbfccedf7be2ea7cc6c1939c6400f739ad6385b3ed4c156ad76e8515fef4a" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
