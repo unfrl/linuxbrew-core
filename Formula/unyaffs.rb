96bc23d38d68303ab7e5a9ffbff9419be84ff806
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-27 18:22:29 +0000

    unyaffs: update 0.9.7 bottle.

--- Formula/unyaffs.rb
+++ Formula/unyaffs.rb
@@ -13,6 +13,7 @@ class Unyaffs < Formula
     sha256 "d5516a71d691f78f1efb0d7f12f2a8ab2b4500ad2a9c1e1ccd5ace316111a1a1" => :arm64_big_sur
     sha256 "0319fb2b8ee918808e30a0bb5deef42abaf7d4afe35cff538b4ed513f06de16e" => :catalina
     sha256 "1c3b921af84a9fee0bb8faf7d420ff2a3d6e6a4e42aeec235d8587a8ccd5da61" => :mojave
+    sha256 "87fe812b32d400d209db224005c40e7dd5f0c43d6147f4800b415a19f128a1ea" => :x86_64_linux
   end
 
   def install
