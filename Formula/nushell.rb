467f8fe1f5e57092c526b54b41e577ab336a69ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-28 11:29:20 +0000

    nushell: update 0.26.0 bottle.

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -18,6 +18,7 @@ class Nushell < Formula
     sha256 "34500e7bd0ca8a0cd8e187ade89a81b7beb372b1715337f17111c4e1acfd99f6" => :arm64_big_sur
     sha256 "82e2ed4ad465d0bf59a6d2cd042ebd6b51e0da46d32d4df4d0a7d091b5b4a16a" => :catalina
     sha256 "06683151d468a8fbe3dac4fafb9a2133b48e832cf7555fe78e06bc7096e95e3f" => :mojave
+    sha256 "04925529ffd092877c4a7fea6891f6e40bdb4b419a02a13dbfab783a2cfffc39" => :x86_64_linux
   end
 
   depends_on "rust" => :build
