8d94faec99a4efb7d23575549f50115ab7211904
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-29 06:42:23 +0000

    proxytunnel: update 1.10.20210128 bottle.

--- Formula/proxytunnel.rb
+++ Formula/proxytunnel.rb
@@ -10,6 +10,7 @@ class Proxytunnel < Formula
     sha256 cellar: :any, arm64_big_sur: "222f0244f92ca6e2ce051db0da342cd72ae5072a69757a92c20c0423fa017e02"
     sha256 cellar: :any, catalina: "f8aa1e88ac9c787e87bdfb82f4d20af89d86cb3f1cbc479b14efa09e5edcd97f"
     sha256 cellar: :any, mojave: "4583ebc48440fc29779b781490b598ae5acc09e9fcc00752284a26cd831c5e01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b77ace45c27faaf843506fb67c6a54e244323c2f4f758b94b171b38ece793595"
   end
 
   depends_on "asciidoc" => :build
