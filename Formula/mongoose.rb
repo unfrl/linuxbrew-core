5251af1825fc4a1c57eed0e4c954702aa69d8ca0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-29 06:39:06 +0000

    mongoose: update 7.1 bottle.

--- Formula/mongoose.rb
+++ Formula/mongoose.rb
@@ -10,6 +10,7 @@ class Mongoose < Formula
     sha256 cellar: :any, arm64_big_sur: "87374bf95a515e72a2dee8aeb0a9b4d60c42397e7bcd1bb12ed527165f992cfc"
     sha256 cellar: :any, catalina: "4f7959dac7c0258f3de40d371b166dca99b077e2856a16bbd4baab7b34527990"
     sha256 cellar: :any, mojave: "b6613ccdbe10010e0ddc4bc9a2b0edc169ac7a6f7e9bda4b5f471ccc4efa8fe2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1702272749cf929b7a48683c4643706bb284eeaa6900d451a437fc0a447a118b"
   end
 
   depends_on "openssl@1.1"
