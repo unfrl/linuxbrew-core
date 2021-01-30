a83f2897431cbbdcd6c134fda090b807d246f04e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-30 15:46:49 +0000

    hadolint: update 1.20.0 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -9,6 +9,7 @@ class Hadolint < Formula
     sha256 cellar: :any_skip_relocation, big_sur: "966f01b9cd17172eaf2f8ec629bf16329b363ef82faa70310a3f71216e94a8a6"
     sha256 cellar: :any_skip_relocation, catalina: "b1ab86966cdd39a778eb9a641a1c80100f12f3d67c490c1f1a71df1ef4ecd83d"
     sha256 cellar: :any_skip_relocation, mojave: "9341df3a445fcc143c6de6b8e3223b63638937642488693ae7de5572a270b627"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fd3bd42ab2c46e157129526a1efe7ccb73f007ba4168962efd7ee68d90dab78d"
   end
 
   depends_on "ghc" => :build
