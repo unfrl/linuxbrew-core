4ad356d63959b393d252a06eef82453bf9ee16d4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-30 23:15:47 +0000

    ponyc: update 0.38.3 bottle.

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -10,6 +10,7 @@ class Ponyc < Formula
     sha256 cellar: :any_skip_relocation, big_sur: "de86e286b66786a1a15ea3ce8e6bd23a5b227fc9928ae6e6e3a498216f0002d8"
     sha256 cellar: :any_skip_relocation, catalina: "b719919818567d4e2e84841dc091539c1341aa9645507af67b729b91a5c5e260"
     sha256 cellar: :any_skip_relocation, mojave: "76ed9cf292056eb0a65758c5949435bff36522e987f316701cadbd0a13971787"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b5fd79210b5bdd392d45c6f08f3d66227c947279393198705918e269ca004154"
   end
 
   depends_on "cmake" => :build
