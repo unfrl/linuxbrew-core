5aa4088221eef3ef20b74208fb7251120d395490
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-31 11:54:40 +0000

    pangomm@2.46: update 2.46.0 bottle.

--- Formula/pangomm@2.46.rb
+++ Formula/pangomm@2.46.rb
@@ -15,6 +15,7 @@ class PangommAT246 < Formula
     sha256 cellar: :any, arm64_big_sur: "0268b741bd164b6cac87ac38ae0533836fe5b270ffc50b3fba764811e17c075b"
     sha256 cellar: :any, catalina: "2bf76c319136106e007194615bc4eabff96845f7bcc7d256237b24eb15e0d179"
     sha256 cellar: :any, mojave: "34e3d85dd6821af7aa8e14c4a019fd858a16f303f7bbb13164b57348ed69bee7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "7c589364111b3a18ca52443bc33d7c0f128a7cf320940bd8a570cebc207f6512"
   end
 
   depends_on "meson" => :build
