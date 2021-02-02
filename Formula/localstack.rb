49f2cb2cd4c115f73ddf6b301e6faf270e5a50a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-02 06:41:18 +0000

    localstack: update 0.12.6 bottle.

--- Formula/localstack.rb
+++ Formula/localstack.rb
@@ -12,6 +12,7 @@ class Localstack < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "3edc0604eeab95bd796a1ab3069fbd342f342e3b057adc1d1e0d79d651074859"
     sha256 cellar: :any_skip_relocation, catalina: "2f45041bc90935af0e39cc97c90312451369df9fbd2f3e957bc29e47bc0bf9bf"
     sha256 cellar: :any_skip_relocation, mojave: "1d30e11ad996da382ef3ef8e88c24e21273ce127a9e783c5f535f9ff5348c913"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "34514664d5bc57ff94298dea200ceb2b6447cb99f5b809ea3eda3e752279625e"
   end
 
   depends_on "docker" => :test
