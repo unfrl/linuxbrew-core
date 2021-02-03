d4c57c75696664234b4cdcaa3d4c32e66728d0f6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-03 07:20:00 +0000

    dcos-cli: update 1.2.0 bottle.

--- Formula/dcos-cli.rb
+++ Formula/dcos-cli.rb
@@ -17,6 +17,7 @@ class DcosCli < Formula
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "f86f45ed4b5244b46a83cb5bf5bc5ac869dfbae3af926f175bee78a0ebd9b47a"
     sha256 cellar: :any_skip_relocation, catalina: "3f64db455d356a65dbb8be7bce2346b9b8afec968082bdad1efafb174bbde1b8"
     sha256 cellar: :any_skip_relocation, mojave: "759770809a74366f84721771b18702a3d27c9e6aa9099f25895200462df17ab8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ac5309e878ba5fdbd922560053d8a730f3d13d90ba428c1676052d38300a7fa1"
   end
 
   depends_on "go" => :build
