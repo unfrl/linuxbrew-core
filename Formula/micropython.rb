0761f452e9e7be7982bd54c001d9aced4e314a83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-04 18:37:58 +0000

    micropython: update 1.14 bottle.

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -7,9 +7,10 @@ class Micropython < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "ae273ed59589c3dadf5632aa03246b51960c2ff6d84b73ba34fdcafff79c8e7d"
-    sha256 cellar: :any, catalina: "103263d0c625e3fa84337be724a25172c4d95b21bfdfb99b806b75c60ca86df9"
-    sha256 cellar: :any, mojave:   "503609fd1033dbdd0e48be4625cf17ccac547912b4f8b02eacff5aab6c43fde5"
+    sha256 cellar: :any,                 big_sur:      "ae273ed59589c3dadf5632aa03246b51960c2ff6d84b73ba34fdcafff79c8e7d"
+    sha256 cellar: :any,                 catalina:     "103263d0c625e3fa84337be724a25172c4d95b21bfdfb99b806b75c60ca86df9"
+    sha256 cellar: :any,                 mojave:       "503609fd1033dbdd0e48be4625cf17ccac547912b4f8b02eacff5aab6c43fde5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9b619d30ba9c505a8bb3661c761ec4a1246d65d06ac3cc2f09584bbb83f76ea9"
   end
 
   depends_on "pkg-config" => :build
