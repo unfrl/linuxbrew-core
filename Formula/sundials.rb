520ed7c64d86d05074185c4ffea3336fbc69d845
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-04 18:33:05 +0000

    sundials: update 5.7.0 bottle.

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -11,10 +11,11 @@ class Sundials < Formula
   end
 
   bottle do
-    sha256 big_sur:       "eca818ac9876c5f784483f49eaeb6b8bb2a4a7950520f13e7dd99f43ad06d223"
-    sha256 arm64_big_sur: "0fde980804a45806673fce03d868a090d38b8d1615b152dc8da3797f1d59054d"
-    sha256 catalina:      "bacddf8c38b9c1236cd0aa2dc85273f7ffff6726418781f5a0e223bf132e3e07"
-    sha256 mojave:        "9ad76e3dbc09c9d9cb4af9e600376fcae287c63128a100959871ddfe8dba438e"
+    sha256                               big_sur:       "eca818ac9876c5f784483f49eaeb6b8bb2a4a7950520f13e7dd99f43ad06d223"
+    sha256                               arm64_big_sur: "0fde980804a45806673fce03d868a090d38b8d1615b152dc8da3797f1d59054d"
+    sha256                               catalina:      "bacddf8c38b9c1236cd0aa2dc85273f7ffff6726418781f5a0e223bf132e3e07"
+    sha256                               mojave:        "9ad76e3dbc09c9d9cb4af9e600376fcae287c63128a100959871ddfe8dba438e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7d7641f4411383d9ffc803854d59229463b57b231791e5408ae2313dbced833"
   end
 
   depends_on "cmake" => :build
