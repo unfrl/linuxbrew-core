6176a7854ca8f3c9d79f78237a742f9605c2f760
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-04 22:24:27 +0000

    logcheck: update 1.3.22 bottle.

--- Formula/logcheck.rb
+++ Formula/logcheck.rb
@@ -11,10 +11,11 @@ class Logcheck < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:       "f2bfaccdb1a53aec3701d8f35c960b3d253395f648ff32602adcc355741b5c36"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "93c3137e2f64093bbd04d83e291b6703c438c2f54624c48b66812dbe59e3e554"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f2bfaccdb1a53aec3701d8f35c960b3d253395f648ff32602adcc355741b5c36"
     sha256 cellar: :any_skip_relocation, catalina:      "aa5bf95cb6fe848f0577be456ee84fd3ea2e5f5e7c00ecab57d6bbc85bf2d218"
     sha256 cellar: :any_skip_relocation, mojave:        "5d88f3e85dd26050d6e65c4e980de25e7168bf6c254ac7e141c10360c41d8c28"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88d6b010b5d68560204bec4c6ae61aed333ade4a4b2e68a963b2a1117d921d8d"
   end
 
   def install
