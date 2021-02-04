593cd4a9fd39a56b702c79bdcea20d7aa91d054f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-04 22:26:17 +0000

    youtube-dl: update 2021.2.4.1 bottle.

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -8,10 +8,11 @@ class YoutubeDl < Formula
   license "Unlicense"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:       "f3162b3444b1a97e3f5ea669726695e1533f603b6ac6840f0e8541a2c89b95fe"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "ae4d3339e865293d91492f340945c65f959bf48f843410e1f4d02a77be023633"
+    sha256 cellar: :any_skip_relocation, big_sur:       "f3162b3444b1a97e3f5ea669726695e1533f603b6ac6840f0e8541a2c89b95fe"
     sha256 cellar: :any_skip_relocation, catalina:      "b466d68fa69917f9389c1740f09a0f234873638151e446731c2c601fa87ee09d"
     sha256 cellar: :any_skip_relocation, mojave:        "496bfe25e6ed8585ce475edfef0f23f17a34e1fd63cee418bdb512fdb93a4bbd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc289fb4154c03ee8d101a3810907296eb8ceb1550beb45fc64dde4dbe1e476d"
   end
 
   depends_on "python@3.9"
