ac554288f2ebf890ce8709df2db6e457ccaca38f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-04 18:29:04 +0000

    gotop: update 4.1.1 bottle.

--- Formula/gotop.rb
+++ Formula/gotop.rb
@@ -6,9 +6,10 @@ class Gotop < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "5bd55068dd42b2aac57ed81c27a991491c1025e419f1a9a04fc8625ee6b052b9"
-    sha256 cellar: :any_skip_relocation, catalina: "20dcda60bff1a19a0aa266ac9171928435ed2f5100ef4737a9d7c0fc68b5e8d7"
-    sha256 cellar: :any_skip_relocation, mojave:   "7292d06bb5efcbb61f919249c1c7ee5a1ab3547f2c791dc0ee18b80694baef47"
+    sha256 cellar: :any_skip_relocation, big_sur:      "5bd55068dd42b2aac57ed81c27a991491c1025e419f1a9a04fc8625ee6b052b9"
+    sha256 cellar: :any_skip_relocation, catalina:     "20dcda60bff1a19a0aa266ac9171928435ed2f5100ef4737a9d7c0fc68b5e8d7"
+    sha256 cellar: :any_skip_relocation, mojave:       "7292d06bb5efcbb61f919249c1c7ee5a1ab3547f2c791dc0ee18b80694baef47"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "654ff648985f7e6a063953104dd6991eef0a2e2215c7686e3e7928914c43878d"
   end
 
   depends_on "go" => :build
