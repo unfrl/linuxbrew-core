f103ed2fd63d49f5c043fa6f848dff85976ac232
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-06 19:09:18 +0000

    buildkit: update 0.8.1 bottle.

--- Formula/buildkit.rb
+++ Formula/buildkit.rb
@@ -17,6 +17,7 @@ class Buildkit < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3e71ae1f0248956f11e74d4d2d90decc62763e61c526ec77d65353d2bc42eeb3"
     sha256 cellar: :any_skip_relocation, catalina:      "ba36ac1a8b1b3c7c2f8e0e6bbbd403be303fa6ca0d193a5193eeb4e22f21d0cb"
     sha256 cellar: :any_skip_relocation, mojave:        "e6b41db313e3c7fb44dc70931dbab30e6f59274d522e58e194d5307977680d3d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "089966d1691e629dc7cbcdbcdfcd07f35895d318a9c205e8fad0647d79b5c71c"
   end
 
   depends_on "go" => :build
