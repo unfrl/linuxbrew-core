49a0c7777f804c9f1c5d9fe8e925dcef20a7de8c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-07 19:43:50 +0000

    tcptraceroute: update 1.5beta7_2 bottle.

--- Formula/tcptraceroute.rb
+++ Formula/tcptraceroute.rb
@@ -25,10 +25,11 @@ class Tcptraceroute < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:     "cc82e1da8c8ddfcaf62dbf23fdf0aa76817c8f8c57c822577d82282bb51dbcb3"
-    sha256 cellar: :any, catalina:    "26e71f154250d933387eb00a17f93c7fe500c9d6bc69ddec10b7bfe7f39c38eb"
-    sha256 cellar: :any, mojave:      "c688457fecc03c5e881448e3f2bc941bc352bb29488383889f71de3f719dee29"
-    sha256 cellar: :any, high_sierra: "e71cda023bb22dc514fda3d22af13bf8f0db80c1937add70b67cf7447d40a67f"
+    sha256 cellar: :any,                 big_sur:      "cc82e1da8c8ddfcaf62dbf23fdf0aa76817c8f8c57c822577d82282bb51dbcb3"
+    sha256 cellar: :any,                 catalina:     "26e71f154250d933387eb00a17f93c7fe500c9d6bc69ddec10b7bfe7f39c38eb"
+    sha256 cellar: :any,                 mojave:       "c688457fecc03c5e881448e3f2bc941bc352bb29488383889f71de3f719dee29"
+    sha256 cellar: :any,                 high_sierra:  "e71cda023bb22dc514fda3d22af13bf8f0db80c1937add70b67cf7447d40a67f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1dd38252b1c6431e4f4f819be0f7f2d85b2334ac387968e14a668442bbed42d6"
   end
 
   depends_on "libnet"
