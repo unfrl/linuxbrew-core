27eb458d47b4857ef73cb22b6d2c20b0d7eca5c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-07 20:38:47 +0000

    texmath: update 0.12.1.1 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -6,9 +6,10 @@ class Texmath < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "cdc91e162794f7481b0b4d881b8217e7b51d7c1613d5fa73772fd778d1eac7c0"
-    sha256 cellar: :any_skip_relocation, catalina: "dd439f733a8c27d6eabb310d73679e100ffef8de14f0f0a705ac5d715ada5164"
-    sha256 cellar: :any_skip_relocation, mojave:   "1ac2b2157f436c43c9857b7506677fa875780b74a819127dcbbee2c83bab34be"
+    sha256 cellar: :any_skip_relocation, big_sur:      "cdc91e162794f7481b0b4d881b8217e7b51d7c1613d5fa73772fd778d1eac7c0"
+    sha256 cellar: :any_skip_relocation, catalina:     "dd439f733a8c27d6eabb310d73679e100ffef8de14f0f0a705ac5d715ada5164"
+    sha256 cellar: :any_skip_relocation, mojave:       "1ac2b2157f436c43c9857b7506677fa875780b74a819127dcbbee2c83bab34be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fbf3ee8aa8a6d1e2dbc8877404aecb8792ca67047cbf5700c1d0901e7cf1f1d7"
   end
 
   depends_on "cabal-install" => :build
