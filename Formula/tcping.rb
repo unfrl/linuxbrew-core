d8821f0e7b974f161c25143a9671d8ef45ce881a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-08 22:43:21 +0000

    tcping: update 1.3.6 bottle.

--- Formula/tcping.rb
+++ Formula/tcping.rb
@@ -17,6 +17,7 @@ class Tcping < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "2cf829fa6b3feab933a12f8fbc9fc1e8d585a304f31d918f26ba0d502f4772ab"
     sha256 cellar: :any_skip_relocation, mojave:        "d769f344e5bdda11b8f0ce6c931e865e982166b3b504cdce33d58c9029786c60"
     sha256 cellar: :any_skip_relocation, high_sierra:   "e0d7d617ac3f98158cd25a08728f9f44cce132101368cae250cced7dbb6a0f7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0cf9b1b608cf328ef6e54788da12eec3cd959c53a09d7677e0921e3c7dac9631"
   end
 
   def install
