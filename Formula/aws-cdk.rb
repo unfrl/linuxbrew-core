73dae6367d9ab0613f73a73cc6130072727d1d5d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-09 21:26:30 +0000

    aws-cdk: update 1.89.0 bottle.

--- Formula/aws-cdk.rb
+++ Formula/aws-cdk.rb
@@ -12,6 +12,7 @@ class AwsCdk < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c7f9c360f7617c85afd4db83c41d1d20cf78ac4158adbcdf51a4ac67fa890359"
     sha256 cellar: :any_skip_relocation, catalina:      "a43b5886cc388b79b703bb802935f34d2a602f674a41ba13f6043d68ae9ee928"
     sha256 cellar: :any_skip_relocation, mojave:        "4557edd170aaac6e86d4de32d439fcfb4bfeb7ba4f017848281bb6b551c884ae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66c23205dc3d64cab385d54fdb163fb7b8246b7299fb53b6cc403475a872cb6e"
   end
 
   depends_on "node"
