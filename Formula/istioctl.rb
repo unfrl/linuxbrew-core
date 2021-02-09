5060ffc4f6a1b93262ae7790a1f120a28c8c3a4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-09 07:01:27 +0000

    istioctl: update 1.8.3 bottle.

--- Formula/istioctl.rb
+++ Formula/istioctl.rb
@@ -8,9 +8,10 @@ class Istioctl < Formula
   head "https://github.com/istio/istio.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "51f95a0a78d7bda884aaa3c480921399a6879ccd6d2ca40ca1221f1c72b99cd2"
-    sha256 cellar: :any_skip_relocation, catalina: "e0b6f5a6369c34ddc92a483327148e9e9db4f378f2df448b70c0641e2a3f36a8"
-    sha256 cellar: :any_skip_relocation, mojave:   "e9f71192aaf6d3f314820336c99b5df0209299b4bd1ce3790aa36515a7b7aba0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "51f95a0a78d7bda884aaa3c480921399a6879ccd6d2ca40ca1221f1c72b99cd2"
+    sha256 cellar: :any_skip_relocation, catalina:     "e0b6f5a6369c34ddc92a483327148e9e9db4f378f2df448b70c0641e2a3f36a8"
+    sha256 cellar: :any_skip_relocation, mojave:       "e9f71192aaf6d3f314820336c99b5df0209299b4bd1ce3790aa36515a7b7aba0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c3faa43ba173b18cf0802b4220b372095ee69adde4db4756f11f43b96b892f02"
   end
 
   depends_on "go" => :build
