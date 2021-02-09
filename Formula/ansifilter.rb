eb19997f06085fb14ad20f80b297f47ea3043ed3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-09 12:54:46 +0000

    ansifilter: update 2.18 bottle.

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -15,6 +15,7 @@ class Ansifilter < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "bce4e87b0f16217d84a01e664c6e96c530d561da301912bfd9e39ec7ff584a74"
     sha256 cellar: :any_skip_relocation, catalina:      "95487d963289a6fbc97a933a492a8f82ae88ad7087ca525d710edea34e100f0a"
     sha256 cellar: :any_skip_relocation, mojave:        "06193d389cb3f1ba85f07fda25e0f17ac732efd289597a6ea2e6ce440123bb5c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5edfb164bc4f95f18f07939df244f62c35e6d21964180ea6bd86c507e3c208f8"
   end
 
   def install
