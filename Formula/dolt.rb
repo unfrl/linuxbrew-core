eec6b7978cf741d9312de6376ddcfd581e8863dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-11 06:46:35 +0000

    dolt: update 0.23.2 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -15,6 +15,7 @@ class Dolt < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b7bf38fb35d97f600612e95b3a5bb430860fc4bf589452734fb933bbe6ae5d1e"
     sha256 cellar: :any_skip_relocation, catalina:      "34abe4cdd02373a0002a08a4a1712edfc3cc1a1316afb007c26bfb550d47f734"
     sha256 cellar: :any_skip_relocation, mojave:        "17266fe30602b15a29f79f80e780737add038d175d77c66a5f4bdabe7625da03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4092ff11b8e900a1d89f02bf46766d86be26390667d97fec9ec13a496c6a2877"
   end
 
   depends_on "go" => :build
