c08df49f08d56a77ff107a3902ad9be6cc1443bb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-15 06:42:34 +0000

    vttest: update 20210210 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -15,6 +15,7 @@ class Vttest < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "bcaa7bfde1caffa18b43a357d684b92e6cfc1db34e5f77a47fb4be22f9ed2051"
     sha256 cellar: :any_skip_relocation, catalina:      "07691c12896f2adfd4cae9ddd7f04b3fcd67ccd6727fe59b7ba79200e7f4961c"
     sha256 cellar: :any_skip_relocation, mojave:        "a73bba40a8459292c25b2999286c29278c9789cbaed70e5382fb1f2b116a0b21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2234edbdf8b69ac119a1cb54cc2926836c4da1999860a77ebcaa8835e7d87b1"
   end
 
   def install
