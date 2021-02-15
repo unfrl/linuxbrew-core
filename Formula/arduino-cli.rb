9d459df45ddf196ed59a8006f61eb9cf0a49f7d0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-15 20:44:43 +0000

    arduino-cli: update 0.16.0 bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -13,9 +13,10 @@ class ArduinoCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "aa9f645bf71d7f69d8ff98f2d38171f82223dd4ee88213f4d0d36016637c62fa"
-    sha256 cellar: :any_skip_relocation, catalina: "7d7ea8bc7f4747fbd50acb519ee9aa74fdceae1a36cef7e1321f5f07deb7758e"
-    sha256 cellar: :any_skip_relocation, mojave:   "9db8fbc419ff3d03af1ba071102e073d1ba326c92446b4f7b052bc9c111b247c"
+    sha256 cellar: :any_skip_relocation, big_sur:      "aa9f645bf71d7f69d8ff98f2d38171f82223dd4ee88213f4d0d36016637c62fa"
+    sha256 cellar: :any_skip_relocation, catalina:     "7d7ea8bc7f4747fbd50acb519ee9aa74fdceae1a36cef7e1321f5f07deb7758e"
+    sha256 cellar: :any_skip_relocation, mojave:       "9db8fbc419ff3d03af1ba071102e073d1ba326c92446b4f7b052bc9c111b247c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c6ef173f63cea0aed37f34098b3848a5af1b11c369d03da12a0b5a2346d42536"
   end
 
   depends_on "go" => :build
