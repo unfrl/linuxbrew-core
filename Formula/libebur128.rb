4273c393f7418d86f7ee1b55d599eb21526dc48d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-15 12:02:26 +0000

    libebur128: update 1.2.6 bottle.

--- Formula/libebur128.rb
+++ Formula/libebur128.rb
@@ -6,10 +6,11 @@ class Libebur128 < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "99450597a660d645800d8d0488b657efee8d7ff9b886a80f964fe3394c8a2552"
-    sha256 cellar: :any, big_sur:       "43567ee920b45921fb0d7787f40d3274ff42360c3048df470aee33be902694e7"
-    sha256 cellar: :any, catalina:      "a9612342890303e8859ee23c7ce8d154f1d3eb134158322aa4ca0968d471281a"
-    sha256 cellar: :any, mojave:        "ebe29eb9b5918eabf720410feb2ac711f5b062458e1d3129ffd29fb7da0f66b5"
+    sha256 cellar: :any,                 arm64_big_sur: "99450597a660d645800d8d0488b657efee8d7ff9b886a80f964fe3394c8a2552"
+    sha256 cellar: :any,                 big_sur:       "43567ee920b45921fb0d7787f40d3274ff42360c3048df470aee33be902694e7"
+    sha256 cellar: :any,                 catalina:      "a9612342890303e8859ee23c7ce8d154f1d3eb134158322aa4ca0968d471281a"
+    sha256 cellar: :any,                 mojave:        "ebe29eb9b5918eabf720410feb2ac711f5b062458e1d3129ffd29fb7da0f66b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bf65fdc840360e92745ca371c9fb8c2654ec66df51a8909732b78f7e3090501"
   end
 
   depends_on "cmake" => :build
