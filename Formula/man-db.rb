3ba5f0f6cb0075fffd585eb1eae84b9710f0e8d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-15 07:01:08 +0000

    man-db: update 2.9.4 bottle.

--- Formula/man-db.rb
+++ Formula/man-db.rb
@@ -16,6 +16,7 @@ class ManDb < Formula
     sha256 big_sur:       "3215d9a7659251e84cfc973926cf25fbd913b787a219e05697b2bf69067de7bf"
     sha256 catalina:      "9c8abb3fe66f90f4bc28ff8bc657fa8eabdacee5b69e0dbc554f8d319fa0948e"
     sha256 mojave:        "1c1760b6a478253caeb8d5535cac5a9466e2f03b1e6e5e9a1f6ee519b752adfb"
+    sha256 x86_64_linux:  "2dad155891f914461a021591193e63f3db4671914202964b1cafb55953ee44ef"
   end
 
   depends_on "pkg-config" => :build
