e427c90c8beb11f074de7c94aa7bc57a54d0a4db
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-15 07:56:00 +0000

    ppsspp: update 1.11.2 bottle.

--- Formula/ppsspp.rb
+++ Formula/ppsspp.rb
@@ -8,10 +8,11 @@ class Ppsspp < Formula
   head "https://github.com/hrydgard/ppsspp.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "900c2debd4f74d4d47a05d11ebd7cdce883871309db0fa891f1f02f9e1465425"
-    sha256 cellar: :any, big_sur:       "dd09c244a87566c74855c0f17c020f297a561d7deb043bcaad47fac905dc484a"
-    sha256 cellar: :any, catalina:      "56bf501a2c69b0b778df5d05cfda6d1e07293dde51cb29f4ca83914efd852715"
-    sha256 cellar: :any, mojave:        "4697af74328830e185514d2201b83683fd88b77975b4e72a8a36ac70e0f3a50f"
+    sha256 cellar: :any,                 arm64_big_sur: "900c2debd4f74d4d47a05d11ebd7cdce883871309db0fa891f1f02f9e1465425"
+    sha256 cellar: :any,                 big_sur:       "dd09c244a87566c74855c0f17c020f297a561d7deb043bcaad47fac905dc484a"
+    sha256 cellar: :any,                 catalina:      "56bf501a2c69b0b778df5d05cfda6d1e07293dde51cb29f4ca83914efd852715"
+    sha256 cellar: :any,                 mojave:        "4697af74328830e185514d2201b83683fd88b77975b4e72a8a36ac70e0f3a50f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "195c9ad16f262104964decb438d60f463a6397dc46e5247e7ddecaca1dd7f5d7"
   end
 
   depends_on "cmake" => :build
