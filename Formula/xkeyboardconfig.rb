3489bf28ce7e3eaa2b5bf22d2f6b4496334c182c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-16 12:47:58 +0000

    xkeyboardconfig: update 2.32 bottle.

--- Formula/xkeyboardconfig.rb
+++ Formula/xkeyboardconfig.rb
@@ -11,6 +11,7 @@ class Xkeyboardconfig < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "8baa880e15f628d34d896a1132b99923b64df53794ddb29cb366fa3272b46495"
     sha256 cellar: :any_skip_relocation, catalina:      "292c62a8b20d29a384c7f8140205e3d9a2a5d21bc3364426e3540b874a6396e0"
     sha256 cellar: :any_skip_relocation, mojave:        "fe7a46767c174a67d05b3342611d112716441f8d5e7f13a9c317f64b59cca3dd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7cabcc342c6e93aa8796a162c8ecc5822de194502ab3b252b9fd62311c97cbc4"
   end
 
   depends_on "gettext" => :build
