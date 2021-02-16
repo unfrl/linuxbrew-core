a2c5346289fb0919c978c6b8a842f101608f0598
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-02-16 12:44:22 +0000

    taglib: update 1.12 bottle.

--- Formula/taglib.rb
+++ Formula/taglib.rb
@@ -7,10 +7,11 @@ class Taglib < Formula
   head "https://github.com/taglib/taglib.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e76adce741330b6648c33971004f1101b42fac129fc78303e83932c27cb38b2d"
-    sha256 cellar: :any, big_sur:       "d24fee8c8c6f491a2c078a84fbcba5f36e0381bf230ff3c35893a46cfe3e3c70"
-    sha256 cellar: :any, catalina:      "766531146ab62a88352ad1718b14ec2461951d25feb3c1111a3005071b4c2e9a"
-    sha256 cellar: :any, mojave:        "f63f8ade1e478f04697481873468017fa06fb78fbe7a8fe42ba2a67533496f57"
+    sha256 cellar: :any,                 arm64_big_sur: "e76adce741330b6648c33971004f1101b42fac129fc78303e83932c27cb38b2d"
+    sha256 cellar: :any,                 big_sur:       "d24fee8c8c6f491a2c078a84fbcba5f36e0381bf230ff3c35893a46cfe3e3c70"
+    sha256 cellar: :any,                 catalina:      "766531146ab62a88352ad1718b14ec2461951d25feb3c1111a3005071b4c2e9a"
+    sha256 cellar: :any,                 mojave:        "f63f8ade1e478f04697481873468017fa06fb78fbe7a8fe42ba2a67533496f57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "03d85461be8c7d93b9f6f4812a588863eaaf87bee5c15335fad6a6bef7f4f230"
   end
 
   depends_on "cmake" => :build
