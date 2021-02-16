efb0a25140e44745a12fc3f09b2786422bdf5f72
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-16 06:42:04 +0000

    janet: update 1.15.2 bottle.

--- Formula/janet.rb
+++ Formula/janet.rb
@@ -7,10 +7,11 @@ class Janet < Formula
   head "https://github.com/janet-lang/janet.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "dff29c0c26cec2311b2f000522d8ee9ead4292c1da3aed2bf3923b59f155d75b"
-    sha256 cellar: :any, big_sur:       "ebd3cecfc33fdd293537957c60236a303195ffd6fc78ced1c4c6be1b6701dfb1"
-    sha256 cellar: :any, catalina:      "c18a3e5f88ec8a1ba67d2cc756b98417e2352acef7b7fb02b8703c6b9e47e6ef"
-    sha256 cellar: :any, mojave:        "390ff0f65a113cdd9edd0beb7822bd2e440700dbc3ceaa8559f3697c19786b98"
+    sha256 cellar: :any,                 arm64_big_sur: "dff29c0c26cec2311b2f000522d8ee9ead4292c1da3aed2bf3923b59f155d75b"
+    sha256 cellar: :any,                 big_sur:       "ebd3cecfc33fdd293537957c60236a303195ffd6fc78ced1c4c6be1b6701dfb1"
+    sha256 cellar: :any,                 catalina:      "c18a3e5f88ec8a1ba67d2cc756b98417e2352acef7b7fb02b8703c6b9e47e6ef"
+    sha256 cellar: :any,                 mojave:        "390ff0f65a113cdd9edd0beb7822bd2e440700dbc3ceaa8559f3697c19786b98"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "64e45fb2778535ea2e483bbf156f91327c4dd7c4f4fb7b61f2b7d87afb4f1b69"
   end
 
   depends_on "meson" => :build
