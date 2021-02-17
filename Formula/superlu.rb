8d39ffc3fae49223b4c66ca65c1fb9aa6d16966b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-17 23:41:49 +0000

    superlu: update 5.2.2_1 bottle.

--- Formula/superlu.rb
+++ Formula/superlu.rb
@@ -16,6 +16,7 @@ class Superlu < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "62393851b2e93277e5420852f6a40ce680fb3d606620984731395024e708a2cc"
     sha256 cellar: :any_skip_relocation, catalina:      "5cc18b04209b3d65f7b1c44413db97251c3bf2933d3a82e9783e269bb21e3d1b"
     sha256 cellar: :any_skip_relocation, mojave:        "84070217c8d262573eacc0d5e5b08ac7e19c68574d0cc229863b6f9d0615d404"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b9cb0f403bb2ea83ba26501a0376fc423b6865b4cb134d536223231dad01b28"
   end
 
   depends_on "cmake" => :build
