14508b3067de7f957233a707e18b2a9dc7c92ae8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-17 17:51:04 +0000

    charm-tools: update 2.8.3 bottle.

--- Formula/charm-tools.rb
+++ Formula/charm-tools.rb
@@ -8,9 +8,10 @@ class CharmTools < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "b174a401458fb0574b263bd51ee4f055f0595a0a380d28ddebb6de0e201a8702"
-    sha256 cellar: :any, catalina: "ec4c42bd27b33958b2fe10a0dc000147a099a72dde3a313e77d88640ca1b1a32"
-    sha256 cellar: :any, mojave:   "95ca45689f0233c756a6979e5e19404d14d705a2fd69b74c6b1cda6d3fe157c4"
+    sha256 cellar: :any,                 big_sur:      "b174a401458fb0574b263bd51ee4f055f0595a0a380d28ddebb6de0e201a8702"
+    sha256 cellar: :any,                 catalina:     "ec4c42bd27b33958b2fe10a0dc000147a099a72dde3a313e77d88640ca1b1a32"
+    sha256 cellar: :any,                 mojave:       "95ca45689f0233c756a6979e5e19404d14d705a2fd69b74c6b1cda6d3fe157c4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ecb3bd7e90d4a80a40636ab24ca8cad289236f4260633b395a7d26b003a98fb4"
   end
 
   depends_on "rust" => :build
