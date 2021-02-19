3251f7c7fb558b24c9e56fcba43ccf0748e9077c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-19 07:04:23 +0000

    cargo-c: update 0.7.3 bottle.

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -6,10 +6,11 @@ class CargoC < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ae5d43a195dc94aeb80c0110822f8eb94fd4d2d758dd51e394fd81b5cbf14afc"
-    sha256 cellar: :any, big_sur:       "bef3ef700fd4e9cce5b7287a7103b2128b06fdb530c20d936baddb0708fe7987"
-    sha256 cellar: :any, catalina:      "5adc9772a6c58eff96522a8505b726d380c8627e9e9d9fe9b3a22d68fe42b4b0"
-    sha256 cellar: :any, mojave:        "97a83b63745a14cd44f97bc2de584b6888207b8120cf91d4d6f8b2c0cf7c877f"
+    sha256 cellar: :any,                 arm64_big_sur: "ae5d43a195dc94aeb80c0110822f8eb94fd4d2d758dd51e394fd81b5cbf14afc"
+    sha256 cellar: :any,                 big_sur:       "bef3ef700fd4e9cce5b7287a7103b2128b06fdb530c20d936baddb0708fe7987"
+    sha256 cellar: :any,                 catalina:      "5adc9772a6c58eff96522a8505b726d380c8627e9e9d9fe9b3a22d68fe42b4b0"
+    sha256 cellar: :any,                 mojave:        "97a83b63745a14cd44f97bc2de584b6888207b8120cf91d4d6f8b2c0cf7c877f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f5dcf324ba73c4ce3ee891dbb77f6602503503b8fd0188cb04af8a7c1f24b4e"
   end
 
   depends_on "rust" => :build
