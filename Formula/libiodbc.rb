97e126d2ee68586966871ebd780fa7c04b400797
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-19 06:31:54 +0000

    libiodbc: update 3.52.14 bottle.

--- Formula/libiodbc.rb
+++ Formula/libiodbc.rb
@@ -6,10 +6,11 @@ class Libiodbc < Formula
   license any_of: ["BSD-3-Clause", "LGPL-2.0-only"]
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9179490e07b80540b31a531babb6168cbd0ea2e10dc994a38f6e5ff8ad2c9f67"
-    sha256 cellar: :any, big_sur:       "981db56543c7f668350a12248aa9047de688e9223bf502a93762ec0e53867d40"
-    sha256 cellar: :any, catalina:      "288802927c2719db23449d5df0ca88c7b9c267c5755a9fa742f5c031d31db552"
-    sha256 cellar: :any, mojave:        "40f53ec2a76bac0bb87697bd2e9cd06ee68d4e83c0e73cd901e569227d6df486"
+    sha256 cellar: :any,                 arm64_big_sur: "9179490e07b80540b31a531babb6168cbd0ea2e10dc994a38f6e5ff8ad2c9f67"
+    sha256 cellar: :any,                 big_sur:       "981db56543c7f668350a12248aa9047de688e9223bf502a93762ec0e53867d40"
+    sha256 cellar: :any,                 catalina:      "288802927c2719db23449d5df0ca88c7b9c267c5755a9fa742f5c031d31db552"
+    sha256 cellar: :any,                 mojave:        "40f53ec2a76bac0bb87697bd2e9cd06ee68d4e83c0e73cd901e569227d6df486"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7ef4b9a009dacbfeb2863210723f8fe6bb5b398cd42d45d395f4dc793e3ba27"
   end
 
   depends_on "autoconf" => :build
