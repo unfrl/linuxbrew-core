d3566e4ff17f09e619f9fa8706f1b5b47cf09997
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-19 06:46:11 +0000

    libebml: update 1.4.2 bottle.

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -7,10 +7,11 @@ class Libebml < Formula
   head "https://github.com/Matroska-Org/libebml.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "fce6d01b12243501223e4e9294528b8eab1818815b18e4ffe777fd14cec0e525"
-    sha256 cellar: :any, big_sur:       "de4edaae6d3f42a388be996f448b582262e39e923acc9ccef881a20ffa817d38"
-    sha256 cellar: :any, catalina:      "20a71bb0c2babdc04f179dc77c7a03c2f2f2031e7d8d87fbf9d3c41ee831addc"
-    sha256 cellar: :any, mojave:        "c3c91dc9f86978012a06f299115bc088e5ea0af6aec2e915d0f8338c4c0edd03"
+    sha256 cellar: :any,                 arm64_big_sur: "fce6d01b12243501223e4e9294528b8eab1818815b18e4ffe777fd14cec0e525"
+    sha256 cellar: :any,                 big_sur:       "de4edaae6d3f42a388be996f448b582262e39e923acc9ccef881a20ffa817d38"
+    sha256 cellar: :any,                 catalina:      "20a71bb0c2babdc04f179dc77c7a03c2f2f2031e7d8d87fbf9d3c41ee831addc"
+    sha256 cellar: :any,                 mojave:        "c3c91dc9f86978012a06f299115bc088e5ea0af6aec2e915d0f8338c4c0edd03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d3914eee9670526434692f458cb15ce62d632b8ae79fc57bc54d91a22c6ba55"
   end
 
   depends_on "cmake" => :build
