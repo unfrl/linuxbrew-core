5cd57b023368a1f2eaf35bd448073bc1e56b059b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-19 06:48:54 +0000

    pgpool-ii: update 4.2.2 bottle.

--- Formula/pgpool-ii.rb
+++ Formula/pgpool-ii.rb
@@ -14,6 +14,7 @@ class PgpoolIi < Formula
     sha256 big_sur:       "79b4f7f5bdc76ca9aeedea952b13bf49b63c7258fa18e7e2551fb62969060140"
     sha256 catalina:      "da66c1f679aa1eabf98246210c3428cfc956c61ebf0ca14652efb3fe7d3e6df8"
     sha256 mojave:        "41f43474dbf1daac87ad3b0d71b050884687909a01fd7fb252d6aed6efa40794"
+    sha256 x86_64_linux:  "40c1aa0306c0c26c0b0c63baafdc98c165244e1b9a1612867a3b1a345ebc6286"
   end
 
   depends_on "postgresql"
