9634f04f9b6f5ab10e6485739d5ae3b09e1d7ee4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-19 05:33:45 +0000

    libedit: update 20210216-3.1 bottle.

--- Formula/libedit.rb
+++ Formula/libedit.rb
@@ -11,10 +11,11 @@ class Libedit < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4d147b3faf124f55ff09117d8882ba96b605c29939c238083c13ad750d2749f2"
-    sha256 cellar: :any, big_sur:       "c56ab224f48f895bcd3220fbb6c0b05d625d5e720785abab4c10f28ff8af1837"
-    sha256 cellar: :any, catalina:      "6991169e0e1908adbcbdc1a578a27a47f237f9a1497bac29cfffa65055b0c9c7"
-    sha256 cellar: :any, mojave:        "a5ba27ac97b4033f4277624712038bdae55865258b74bc2302c3c810218204a4"
+    sha256 cellar: :any,                 arm64_big_sur: "4d147b3faf124f55ff09117d8882ba96b605c29939c238083c13ad750d2749f2"
+    sha256 cellar: :any,                 big_sur:       "c56ab224f48f895bcd3220fbb6c0b05d625d5e720785abab4c10f28ff8af1837"
+    sha256 cellar: :any,                 catalina:      "6991169e0e1908adbcbdc1a578a27a47f237f9a1497bac29cfffa65055b0c9c7"
+    sha256 cellar: :any,                 mojave:        "a5ba27ac97b4033f4277624712038bdae55865258b74bc2302c3c810218204a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "713fc214775aff0f86bcc4a8c99d77d99e1cf6ffc1d175114060144b55e58e06"
   end
 
   keg_only :provided_by_macos
