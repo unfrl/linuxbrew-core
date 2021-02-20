4b2b08abe5a8671723da75546cc2a226cbbea344
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-20 09:06:14 +0000

    amazon-ecs-cli: update 1.21.0 bottle.

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -8,9 +8,10 @@ class AmazonEcsCli < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any_skip_relocation, big_sur:  "66dcb9af8a67215a8a1f4fef00dbf0c16e836cc65985a86d113cb4f208dff50c"
-    sha256 cellar: :any_skip_relocation, catalina: "0bb03d95203b20aebc66ee008946951dfc66a991d6015f38d9158cda3dc36b8c"
-    sha256 cellar: :any_skip_relocation, mojave:   "ac8ff57a4b7de517c767f53626f19d134732c9b3a9d68143cac9d4440f01cc2a"
+    sha256 cellar: :any_skip_relocation, big_sur:      "66dcb9af8a67215a8a1f4fef00dbf0c16e836cc65985a86d113cb4f208dff50c"
+    sha256 cellar: :any_skip_relocation, catalina:     "0bb03d95203b20aebc66ee008946951dfc66a991d6015f38d9158cda3dc36b8c"
+    sha256 cellar: :any_skip_relocation, mojave:       "ac8ff57a4b7de517c767f53626f19d134732c9b3a9d68143cac9d4440f01cc2a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "09a38a44b923120215c4b37fc7b455035078ceebf4c66756d93ba0f9e0c9739d"
   end
 
   depends_on "go" => :build
