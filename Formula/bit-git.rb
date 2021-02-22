fb710c7687e5fcc017488e97c737aa6e5c11a0a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-22 06:51:53 +0000

    bit-git: update 1.0.1 bottle.

--- Formula/bit-git.rb
+++ Formula/bit-git.rb
@@ -10,6 +10,7 @@ class BitGit < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "706a291c13596bdb81799fa3f8133b715c4f95f23d825bfa37d4ea91bae30995"
     sha256 cellar: :any_skip_relocation, catalina:      "5eb72f40d3862c3e44a5c5d12384a58fe013105624a3bf874f55a0164d1625ac"
     sha256 cellar: :any_skip_relocation, mojave:        "c8e8120297a38e532a3f0c48f8f5e36d83ae450b973d5224feed3bb9b6fe7048"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8a3ba092661d505e38f7496575685b40aba99d923fba15209e40ff6c11f1046"
   end
 
   depends_on "go" => :build
