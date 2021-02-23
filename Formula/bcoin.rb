6711f5312cad9cdf6b577fec218bb822d2c32bd2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-23 06:33:06 +0000

    bcoin: update 2.1.2_2 bottle.

--- Formula/bcoin.rb
+++ Formula/bcoin.rb
@@ -11,11 +11,12 @@ class Bcoin < Formula
 
   bottle do
     rebuild 1
-    sha256 arm64_big_sur: "f401dfcda058e68165418880973b275201dd372734fb866de7a51f6a48c0e6ce"
-    sha256 big_sur:       "18e9b1f2074c5c98b15cb118b72a6104db12e9c80bc56d4c74374d86f394dece"
-    sha256 catalina:      "3f46d506838e38420dc9ed5bd5b8c5c3ae8038bea02fad83f36f1ed36cda9046"
-    sha256 mojave:        "5adbc476b4e3ef00c5e8fe7fc1554a891b4a3b09bd5d318103d0ac44f7540d80"
-    sha256 high_sierra:   "c4bdb0a86a7fbfce14261af58ab4954c93f2eba09806a6933959fdfe8698878d"
+    sha256                               arm64_big_sur: "f401dfcda058e68165418880973b275201dd372734fb866de7a51f6a48c0e6ce"
+    sha256                               big_sur:       "18e9b1f2074c5c98b15cb118b72a6104db12e9c80bc56d4c74374d86f394dece"
+    sha256                               catalina:      "3f46d506838e38420dc9ed5bd5b8c5c3ae8038bea02fad83f36f1ed36cda9046"
+    sha256                               mojave:        "5adbc476b4e3ef00c5e8fe7fc1554a891b4a3b09bd5d318103d0ac44f7540d80"
+    sha256                               high_sierra:   "c4bdb0a86a7fbfce14261af58ab4954c93f2eba09806a6933959fdfe8698878d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34673102f5bb17ea7b493821e1db753c516e6cf67711e3eac992926ce3fb9356"
   end
 
   depends_on "python@3.9" => :build
