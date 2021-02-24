086dbfb71d3b1f0fdfdf0b518b49726abcb403f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-24 21:44:03 +0000

    croc: update 8.6.8 bottle.

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -11,6 +11,7 @@ class Croc < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "670cba99c12da0f15d4a9d5d79e87518d340008e5aded1e1377924a6b6a64ae4"
     sha256 cellar: :any_skip_relocation, catalina:      "8a0bd42725325468ffb1d4dd3d2d9217957b18e6e052f08ccc5412cff3ab29fd"
     sha256 cellar: :any_skip_relocation, mojave:        "5c08ae3e01edccb9c2c0910704813d7760208be1930b530ccd836d5f01968609"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ab9c516fa9132545b3be28150f69a6054505bba6a6f6c4c784bd3dded6b7d6c"
   end
 
   depends_on "go" => :build
