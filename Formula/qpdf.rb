dff2d191baa272b04d4d6b68283a2c9976e55608
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-24 13:07:28 +0000

    qpdf: update 10.2.0 bottle.

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -6,10 +6,11 @@ class Qpdf < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "0f78ce71f607721920f1d556ce92e36ff35677cdf0191e672f33b8961a846027"
-    sha256 cellar: :any, big_sur:       "22dfc12bbcb9eb5ae529e9765e555b4f0320712f315cce6d00d607d7399ae98e"
-    sha256 cellar: :any, catalina:      "0782c454782100fbbcc34686d8d1d09506b5ab9ed6188a09ecea4fe9dfa93b00"
-    sha256 cellar: :any, mojave:        "ee3f9c7170c36b9d3429904f07e873e3f9939b054ba7a8af69a6a9b54814cd2e"
+    sha256 cellar: :any,                 arm64_big_sur: "0f78ce71f607721920f1d556ce92e36ff35677cdf0191e672f33b8961a846027"
+    sha256 cellar: :any,                 big_sur:       "22dfc12bbcb9eb5ae529e9765e555b4f0320712f315cce6d00d607d7399ae98e"
+    sha256 cellar: :any,                 catalina:      "0782c454782100fbbcc34686d8d1d09506b5ab9ed6188a09ecea4fe9dfa93b00"
+    sha256 cellar: :any,                 mojave:        "ee3f9c7170c36b9d3429904f07e873e3f9939b054ba7a8af69a6a9b54814cd2e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "94bd1ff6ab0d0be84fd35b6dd67fb064fb76fe8b20d4a597b9925621c5c8cf8f"
   end
 
   depends_on "jpeg"
