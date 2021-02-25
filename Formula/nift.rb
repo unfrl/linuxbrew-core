fcb966814943aa2df2ac7b1072c7e2f0bbf06033
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-25 07:39:33 +0000

    nift: update 2.3.11 bottle.

--- Formula/nift.rb
+++ Formula/nift.rb
@@ -6,9 +6,10 @@ class Nift < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e91f7c5ca5c8562b0c5f21a611387a4c84d0231268e2fb02d9dcab5c5a0028cf"
-    sha256 cellar: :any_skip_relocation, catalina: "566aa80fa3b9b54144ebd2d95e9c8392c23324a061c4d1c443c9b85e9e8ddd5b"
-    sha256 cellar: :any_skip_relocation, mojave:   "e5abb77d1e7ae165e9007926c8df655351259c9130ecab421c25a8ed9db3522d"
+    sha256 cellar: :any_skip_relocation, big_sur:      "e91f7c5ca5c8562b0c5f21a611387a4c84d0231268e2fb02d9dcab5c5a0028cf"
+    sha256 cellar: :any_skip_relocation, catalina:     "566aa80fa3b9b54144ebd2d95e9c8392c23324a061c4d1c443c9b85e9e8ddd5b"
+    sha256 cellar: :any_skip_relocation, mojave:       "e5abb77d1e7ae165e9007926c8df655351259c9130ecab421c25a8ed9db3522d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8089e09708a75d7d7a1803f5da0659b7b3c70e90c57d149bcefaaaf8323d20ae"
   end
 
   depends_on "luajit" unless OS.mac?
