dec00dbf43d0905d1429e635a2c907a50ad14cf5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-02-28 13:09:01 +0000

    hfstospell: update 0.5.2_1 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -12,10 +12,11 @@ class Hfstospell < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "758ed5ec9ea9fc762b891ff8f27d83a840195fe86636cd9706115063841dcf0d"
-    sha256 cellar: :any, big_sur:       "9bef29b829d94f52c22206e365f364a604620c194fefea2134ba0827fee6b5a8"
-    sha256 cellar: :any, catalina:      "93b6e9890e78fcde61b18277861e3c3ec77039e22f4c9c723078791d787521e0"
-    sha256 cellar: :any, mojave:        "f33ba3c1c11a54a7c349b41a6c230735b54cf6c58314a3a4d3fa3a8d9b1fe809"
+    sha256 cellar: :any,                 arm64_big_sur: "758ed5ec9ea9fc762b891ff8f27d83a840195fe86636cd9706115063841dcf0d"
+    sha256 cellar: :any,                 big_sur:       "9bef29b829d94f52c22206e365f364a604620c194fefea2134ba0827fee6b5a8"
+    sha256 cellar: :any,                 catalina:      "93b6e9890e78fcde61b18277861e3c3ec77039e22f4c9c723078791d787521e0"
+    sha256 cellar: :any,                 mojave:        "f33ba3c1c11a54a7c349b41a6c230735b54cf6c58314a3a4d3fa3a8d9b1fe809"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b91cb75b70cd422f9e00c639988767f7dbef84f997dd1e06e1d8fba3fd77ab4"
   end
 
   depends_on "autoconf" => :build
