cc52d48cf1ad4de167ad6fdaee03dd641a7d3616
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-01 18:10:38 +0000

    librdkafka: update 1.6.1 bottle.

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -12,10 +12,11 @@ class Librdkafka < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ec7240e9422a25af6c9504fbf5bea1fdb2bcb8765f4c6c09cb0b5a0094fb71b3"
-    sha256               big_sur:       "665c15af3ef760e026c0dc441a0c750485228128416775589defbf90f1001644"
-    sha256               catalina:      "193819ab935d16d82da2b143d0509d6aa734ee6c6258868a8dd26d987ecc5c22"
-    sha256               mojave:        "94140b0992f3bdc27792c03e1be2d58215947ea2e22df42f4f0d53bf4a1e9e49"
+    sha256 cellar: :any,                 arm64_big_sur: "ec7240e9422a25af6c9504fbf5bea1fdb2bcb8765f4c6c09cb0b5a0094fb71b3"
+    sha256                               big_sur:       "665c15af3ef760e026c0dc441a0c750485228128416775589defbf90f1001644"
+    sha256                               catalina:      "193819ab935d16d82da2b143d0509d6aa734ee6c6258868a8dd26d987ecc5c22"
+    sha256                               mojave:        "94140b0992f3bdc27792c03e1be2d58215947ea2e22df42f4f0d53bf4a1e9e49"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "da71b7fddade5ab5d4504a7ff5e5813d3f4ad874b6dddf9bc4a5b7b4920b4352"
   end
 
   depends_on "pkg-config" => :build
