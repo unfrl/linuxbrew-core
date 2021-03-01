434633fb4d6bce179008211b0168228552cc4317
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-01 20:03:28 +0000

    gitmoji: update 3.3.0 bottle.

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -12,6 +12,7 @@ class Gitmoji < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "7bb76cec02ceae5df03280cd50bf8f06776b24cdb0cd1e9ae2995f6fefe6cff5"
     sha256 cellar: :any_skip_relocation, catalina:      "43c16a44087dd58059c1b4d3d5cdd401663bcd0d1328f4c412062465dc738f51"
     sha256 cellar: :any_skip_relocation, mojave:        "ab44b3fe5e4951c3e96b4ea47b15dac769ede3449668e3e0f66ecd59a5af7687"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b65cea6b4f72b2959303b7b41070572594dd4324a3e05b37db4966a60c87c81"
   end
 
   depends_on "node"
