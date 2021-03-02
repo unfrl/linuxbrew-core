e9a4ddc81dd82498e67388116216394445754667
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-02 21:30:08 +0000

    libimagequant: update 2.14.1 bottle.

--- Formula/libimagequant.rb
+++ Formula/libimagequant.rb
@@ -6,10 +6,11 @@ class Libimagequant < Formula
   license :cannot_represent
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b1b6b13823a3a8590dc94296c891c447a1aec3f17f2d0847b33c9bb3cb25ccf3"
-    sha256 cellar: :any, big_sur:       "e0cc2ed03f455bed35c3cf56c897c3430efa22d6b0646e083b41ec253248fdf8"
-    sha256 cellar: :any, catalina:      "eed2b90a2cbeb0394177056b5f6d1b4776b0051439526c4e826e8e5cc6ceebaa"
-    sha256 cellar: :any, mojave:        "7d3930509859c89f13b0c9adf1c50615241b839a96be746bbce9bd6e3e35f236"
+    sha256 cellar: :any,                 arm64_big_sur: "b1b6b13823a3a8590dc94296c891c447a1aec3f17f2d0847b33c9bb3cb25ccf3"
+    sha256 cellar: :any,                 big_sur:       "e0cc2ed03f455bed35c3cf56c897c3430efa22d6b0646e083b41ec253248fdf8"
+    sha256 cellar: :any,                 catalina:      "eed2b90a2cbeb0394177056b5f6d1b4776b0051439526c4e826e8e5cc6ceebaa"
+    sha256 cellar: :any,                 mojave:        "7d3930509859c89f13b0c9adf1c50615241b839a96be746bbce9bd6e3e35f236"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5fadcb5545d74b4371746b2ac15952f99b0914d2c7d3a8198264556e3720b54"
   end
 
   def install
