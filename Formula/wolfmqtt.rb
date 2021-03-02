27ed69ed670aa7b1652de6d1a982acad4eb87b47
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-02 21:31:23 +0000

    wolfmqtt: update 1.8.0 bottle.

--- Formula/wolfmqtt.rb
+++ Formula/wolfmqtt.rb
@@ -7,10 +7,11 @@ class Wolfmqtt < Formula
   head "https://github.com/wolfSSL/wolfMQTT.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e3e66f284473f7e646646a7383f41fe09a939b160151692756600f790755df0a"
-    sha256 cellar: :any, big_sur:       "70cc3493f685ea09b9bda05688cfcdd9fb8dd7bbfd386ad1c4c6f58b29769dad"
-    sha256 cellar: :any, catalina:      "495e994e80e47776f4f58c9e87a6b8cc74d0cc91abe4981bf1fca314e0d0ad83"
-    sha256 cellar: :any, mojave:        "a6a39104cd27c320b7e99a303e191fca638a54aca69cdbd10b9ca9a8e26b8a8d"
+    sha256 cellar: :any,                 arm64_big_sur: "e3e66f284473f7e646646a7383f41fe09a939b160151692756600f790755df0a"
+    sha256 cellar: :any,                 big_sur:       "70cc3493f685ea09b9bda05688cfcdd9fb8dd7bbfd386ad1c4c6f58b29769dad"
+    sha256 cellar: :any,                 catalina:      "495e994e80e47776f4f58c9e87a6b8cc74d0cc91abe4981bf1fca314e0d0ad83"
+    sha256 cellar: :any,                 mojave:        "a6a39104cd27c320b7e99a303e191fca638a54aca69cdbd10b9ca9a8e26b8a8d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fbe22ed7f5f5639037dab4f458ac82676ed1285f59722adc58f2dbe7dfda5521"
   end
 
   depends_on "autoconf" => :build
