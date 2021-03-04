698592460387375e8bab0352f741683db7defef0
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-03-04 15:05:23 +0000

    modules: update 4.7.0_1 bottle.

--- Formula/modules.rb
+++ Formula/modules.rb
@@ -12,10 +12,11 @@ class Modules < Formula
   end
 
   bottle do
-    sha256               arm64_big_sur: "9efa2847cd3b742278569de48251e6e4a3828a4faaee55621c399f8d01efe5ed"
-    sha256               big_sur:       "637ca6ff4592b1c2392a927f289bdb8d867d922f90a9df91aed57c45d6c1d1d4"
-    sha256 cellar: :any, catalina:      "768a0050642449eea6a5d714e39a83f23ca32b2e19a8f3c15e29a75854aebf55"
-    sha256 cellar: :any, mojave:        "9778d58e3a2f1a41236f26c634e75062c1b2c2be2569c65e758c2226a81a0463"
+    sha256                               arm64_big_sur: "9efa2847cd3b742278569de48251e6e4a3828a4faaee55621c399f8d01efe5ed"
+    sha256                               big_sur:       "637ca6ff4592b1c2392a927f289bdb8d867d922f90a9df91aed57c45d6c1d1d4"
+    sha256 cellar: :any,                 catalina:      "768a0050642449eea6a5d714e39a83f23ca32b2e19a8f3c15e29a75854aebf55"
+    sha256 cellar: :any,                 mojave:        "9778d58e3a2f1a41236f26c634e75062c1b2c2be2569c65e758c2226a81a0463"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9f6b5235fd4da0b3e38f2a24953ba6d11c3b2e1d4ca0ad54eea2747b03b803db"
   end
 
   depends_on "less" unless OS.mac?
