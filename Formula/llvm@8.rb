3c2a7d575445096087b8af61c870c067d54a76ab
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-03-07 20:10:00 +0000

    llvm@8: update 8.0.1_5 bottle.

--- Formula/llvm@8.rb
+++ Formula/llvm@8.rb
@@ -9,9 +9,10 @@ class LlvmAT8 < Formula
   revision OS.mac? ? 3 : 5
 
   bottle do
-    sha256 cellar: :any, catalina:     "ab099d84e5f0a58ea37172fd85753336d855fc25e9459ceff12ddc2dbb56ef71"
-    sha256 cellar: :any, mojave:       "ee795cbebce64f79bbcf7c42526093df7bd2e5e986a721197bca5cf6c822e87a"
-    sha256 cellar: :any, high_sierra:  "3f80b7119307b128b1e3ae8a2fea97a9878afb5a7436a7d35615b1e743bc7622"
+    sha256 cellar: :any,                 catalina:     "ab099d84e5f0a58ea37172fd85753336d855fc25e9459ceff12ddc2dbb56ef71"
+    sha256 cellar: :any,                 mojave:       "ee795cbebce64f79bbcf7c42526093df7bd2e5e986a721197bca5cf6c822e87a"
+    sha256 cellar: :any,                 high_sierra:  "3f80b7119307b128b1e3ae8a2fea97a9878afb5a7436a7d35615b1e743bc7622"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b38a990522dccb6aca06300934044648d6df21e86259045f0dc0c693437a749"
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
