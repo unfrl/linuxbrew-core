05100bf2240e85115bb8dff2f11a79cdede5f95e
Michka Popoff <michkapopoff@gmail.com>
2021-03-08 13:12:21 +0100

    securefs: unbottle for libfuse rev bump

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -5,13 +5,13 @@ class Securefs < Formula
       tag:      "0.11.1",
       revision: "dfeebf8406871d020848edde668234715356158c"
   license "MIT"
+  revision 1 unless OS.mac?
   head "https://github.com/netheril96/securefs.git"
 
   bottle do
     sha256 cellar: :any, catalina:     "8a8c7dd74f9b3082b2b128cc058714a27206d910273e4148959a25b7d30c51b5"
     sha256 cellar: :any, mojave:       "9bee23af87518f68df56b862ad0ef88f842ebe65f8feff02e90bf5ab8e91522e"
     sha256 cellar: :any, high_sierra:  "632496d8e9ed9fe91d18e9a2c9fef49c920dc091e10108246f8ab2056f75ea38"
-    sha256 cellar: :any, x86_64_linux: "284e62bbc94eba3cfbe57561eb41ddc6c42b22c4c18da74190429fb13debeb74"
   end
 
   depends_on "cmake" => :build
