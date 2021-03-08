6bf007653ef161968887d59e60f3d78d12a35cde
Michka Popoff <michkapopoff@gmail.com>
2021-03-08 13:11:46 +0100

    bindfs: unbottle for libfuse rev bump

--- Formula/bindfs.rb
+++ Formula/bindfs.rb
@@ -4,12 +4,12 @@ class Bindfs < Formula
   url "https://bindfs.org/downloads/bindfs-1.14.8.tar.gz"
   sha256 "e5ca5aff55204b993a025a77c3f8c0e2ee901ba8059d71bea11de2cc685ec497"
   license "GPL-2.0"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 cellar: :any, catalina:     "bf4fb90d788651299973a3f48300824ae6b4ec4ce1441dd94d544180f54379bd"
     sha256 cellar: :any, mojave:       "cc9d4950a1b74a27307bac429892fd85ad439f5707f53a66e800a39b23a32fdf"
     sha256 cellar: :any, high_sierra:  "50df08ee8d3cc6f141d8488341f12c32ce478f02dfb8016f533bfeababfe4537"
-    sha256 cellar: :any, x86_64_linux: "fb4b84127bac0e1c7559e5f0b8d3b8d62aea5e0d1192d8f516aec5e9eee0cfe1"
   end
 
   head do
