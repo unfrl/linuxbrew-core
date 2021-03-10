8819de7f0afc62447a28b97e9f0f187b941860fa
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-03-10 11:52:54 +0000

    fdroidserver: update 2.0.1 bottle.

--- Formula/fdroidserver.rb
+++ Formula/fdroidserver.rb
@@ -8,10 +8,11 @@ class Fdroidserver < Formula
   license "AGPL-3.0-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "cbfdfb10ca063cd47b8af6d3318da64b2e230227508a48b91979ca52d6b71a17"
-    sha256 cellar: :any, big_sur:       "40d16e2a0a12e9bcf044684b19eda54787fc2d547b244b3055aa9ce0c295931f"
-    sha256 cellar: :any, catalina:      "f876b327aba1abf709382708c8a8132672e32dde37fc20afb165ed154bb807b0"
-    sha256 cellar: :any, mojave:        "a661255fdb4d787fb86dba64b68d0fa1080fb576f2533aef95afd35bf4abbc41"
+    sha256 cellar: :any,                 arm64_big_sur: "cbfdfb10ca063cd47b8af6d3318da64b2e230227508a48b91979ca52d6b71a17"
+    sha256 cellar: :any,                 big_sur:       "40d16e2a0a12e9bcf044684b19eda54787fc2d547b244b3055aa9ce0c295931f"
+    sha256 cellar: :any,                 catalina:      "f876b327aba1abf709382708c8a8132672e32dde37fc20afb165ed154bb807b0"
+    sha256 cellar: :any,                 mojave:        "a661255fdb4d787fb86dba64b68d0fa1080fb576f2533aef95afd35bf4abbc41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d65a3f2485abf4cce2aeec4848be7ad0abec435fc720af9d48ecdd3a41b77159"
   end
 
   depends_on "pkg-config" => :build
