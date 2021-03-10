1e79d54cc21bc5ca93bfb59d67209bedd9b1dd84
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-10 21:39:49 +0000

    periscope: update 0.2.0 bottle.

--- Formula/periscope.rb
+++ Formula/periscope.rb
@@ -12,6 +12,7 @@ class Periscope < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "9824c9c61ed092523c30284612f1ddfa9c0725cd6c13c854340f47a4fa0ff6d3"
     sha256 cellar: :any_skip_relocation, catalina:      "9b11b76bc3d4a06f735e88293a5c2263dbea9c4ed6d023bd6b1d88c6ba73a5f6"
     sha256 cellar: :any_skip_relocation, mojave:        "b86dff9739ead37c41f5a40f6641c1795d515517d99d134ad900bf47b383b453"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69942f9b759857d40dcfa46e555c849f03490e06226fa638e1d9acd0596b9aaa"
   end
 
   depends_on "go" => :build
