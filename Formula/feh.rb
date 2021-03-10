2636a2c4d3db6c217721c087e333d5a5698744b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-10 22:31:12 +0000

    feh: update 3.6.3 bottle.

--- Formula/feh.rb
+++ Formula/feh.rb
@@ -15,6 +15,7 @@ class Feh < Formula
     sha256 big_sur:       "cc6e98325f1233c484f700f04cabb9b73061e2d1cb6c24de954ad33a9a4cfa7c"
     sha256 catalina:      "a26d86d34fca0fe0a87aa9330e20614a5c1aaede90d79a8fc482fc64259008cc"
     sha256 mojave:        "4328378783d1519676d8d53be065cd9ee52e583b9e917fc507afeff30eb85f2f"
+    sha256 x86_64_linux:  "300a112da6fa4c19ada441bd058aa3b62c9e739292c6adf0f0e4ff629f18e311"
   end
 
   depends_on "imlib2"
