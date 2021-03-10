3225ef54f88e563580d70f2f139d4616b24d3528
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-10 08:29:38 +0000

    diesel: update 1.4.6 bottle.

--- Formula/diesel.rb
+++ Formula/diesel.rb
@@ -7,10 +7,11 @@ class Diesel < Formula
   head "https://github.com/diesel-rs/diesel.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "591196c09f6b466667293f73bdce43feb651e03d37b68f2760b10a0210029b43"
-    sha256 cellar: :any, big_sur:       "f33decdb3dbff3f44ab88cf933437deed70bfaaa52b934e7badec2f64073524a"
-    sha256 cellar: :any, catalina:      "68886f00182c23bcb8e76e861e194292328fa185ef60ce5ba567c9c029061845"
-    sha256 cellar: :any, mojave:        "3b680f376125f6e96f556e8ead859e1d5d0d081ea1dd5bbf3c61fd79e3976f1b"
+    sha256 cellar: :any,                 arm64_big_sur: "591196c09f6b466667293f73bdce43feb651e03d37b68f2760b10a0210029b43"
+    sha256 cellar: :any,                 big_sur:       "f33decdb3dbff3f44ab88cf933437deed70bfaaa52b934e7badec2f64073524a"
+    sha256 cellar: :any,                 catalina:      "68886f00182c23bcb8e76e861e194292328fa185ef60ce5ba567c9c029061845"
+    sha256 cellar: :any,                 mojave:        "3b680f376125f6e96f556e8ead859e1d5d0d081ea1dd5bbf3c61fd79e3976f1b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa4f7322fbca8ba4babecaaeab3de01b796fc64eec24f3bc0bbc0416d238a9d0"
   end
 
   depends_on "rust" => [:build, :test]
