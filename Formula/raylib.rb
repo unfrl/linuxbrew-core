6d6fc36a3be3302c694b77a0e339e8822b36ef33
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-11 23:30:09 +0000

    raylib: update 3.5.0 bottle.

--- Formula/raylib.rb
+++ Formula/raylib.rb
@@ -8,10 +8,11 @@ class Raylib < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "2411fb0870c9cec11f2f27f4d6f217fd7cff9351b65569970dcc3e27bee00964"
-    sha256 cellar: :any, big_sur:       "079378b95371d4212b9603a41d3d44a838ad7fd07943cc902064251c644d2ef5"
-    sha256 cellar: :any, catalina:      "78ef0455f7b3602829bf81c13dfeb81ca7867cde572bae477e6c2b9355289035"
-    sha256 cellar: :any, mojave:        "ef45020d299bcb5c03f12f05b1a51791e155dce76073a54572ae5da04142fa7e"
+    sha256 cellar: :any,                 arm64_big_sur: "2411fb0870c9cec11f2f27f4d6f217fd7cff9351b65569970dcc3e27bee00964"
+    sha256 cellar: :any,                 big_sur:       "079378b95371d4212b9603a41d3d44a838ad7fd07943cc902064251c644d2ef5"
+    sha256 cellar: :any,                 catalina:      "78ef0455f7b3602829bf81c13dfeb81ca7867cde572bae477e6c2b9355289035"
+    sha256 cellar: :any,                 mojave:        "ef45020d299bcb5c03f12f05b1a51791e155dce76073a54572ae5da04142fa7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d947333b66a65bc5899aff9d742c1de6010a4190ad0e38b302c3a9d589ba62b"
   end
 
   depends_on "cmake" => :build
