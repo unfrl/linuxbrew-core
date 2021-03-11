b2093cb46a4279e39a73811246f19c946567485f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-11 07:34:38 +0000

    git-annex: update 8.20210310 bottle.

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -8,9 +8,10 @@ class GitAnnex < Formula
   head "git://git-annex.branchable.com/"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "dfab392cf4d1007e60b65a9efeb836289a296186a142d7cc388f8e320ea53ca8"
-    sha256 cellar: :any, catalina: "9b3cd55de3855d984992fba412b091521fb1f1c612fb3e5a466f738bddba8c92"
-    sha256 cellar: :any, mojave:   "2365ae1349e7d9ab3196ec2cf92a8ac1cc28a66217f51ee9e66d6ce9998cd45e"
+    sha256 cellar: :any,                 big_sur:      "dfab392cf4d1007e60b65a9efeb836289a296186a142d7cc388f8e320ea53ca8"
+    sha256 cellar: :any,                 catalina:     "9b3cd55de3855d984992fba412b091521fb1f1c612fb3e5a466f738bddba8c92"
+    sha256 cellar: :any,                 mojave:       "2365ae1349e7d9ab3196ec2cf92a8ac1cc28a66217f51ee9e66d6ce9998cd45e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0340ba3fdc620a5196aa26b6b4c73b53c0f85f28e8a6cb5f3e169c27b21d1b9e"
   end
 
   depends_on "cabal-install" => :build
