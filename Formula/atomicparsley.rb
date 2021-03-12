047fa203f7f892337718d899febf4db0ac10e619
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-12 16:30:33 +0000

    atomicparsley: update 20210124.204813.840499f_1 bottle.

--- Formula/atomicparsley.rb
+++ Formula/atomicparsley.rb
@@ -15,6 +15,7 @@ class Atomicparsley < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "7030626fea429aab956442a66f630639ea17c44c05d948ad1c2c91cde5eefb53"
     sha256 cellar: :any_skip_relocation, catalina:      "9fb54fe0aa1faeef942d48c139bb7e003aaf795c8a47b0d9c6d046bafc3b3fb0"
     sha256 cellar: :any_skip_relocation, mojave:        "b58fc8376ed3e18b7207ede4825c34256854b3abc390260f5f6759e0eb329013"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ded2675e6ba474d50a966414cc0704442719c41020812a8ce74ac0fd1a5cb118"
   end
 
   depends_on "cmake" => :build
