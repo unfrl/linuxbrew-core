2367b15c5817f94b003acd4d1521fa9e88347c8c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-13 19:06:51 +0000

    lazygit: update 0.26 bottle.

--- Formula/lazygit.rb
+++ Formula/lazygit.rb
@@ -10,6 +10,7 @@ class Lazygit < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "d1a75fa3fa6b6964cba0e182726f71d4da13f397edc310a262f21cc8e6b2f97a"
     sha256 cellar: :any_skip_relocation, catalina:      "a4b12ca3cfd22224759328448b1932732690c1a63592e407cb3d40ba71ac0fbf"
     sha256 cellar: :any_skip_relocation, mojave:        "65ff729f7852eeef15759ffe1b30406e34aa28e913949e6e8ab87b1fe95cad50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b82328b6e180f19ecc8e06e514d629da1d347bdb3bc61eaa78e05429de47d732"
   end
 
   depends_on "go" => :build
