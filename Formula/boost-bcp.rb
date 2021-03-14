ccffe7f3eabfe4b28b6c6f570847d51fc643c5e3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 03:28:00 +0000

    boost-bcp: update 1.75.0 bottle.

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -12,6 +12,7 @@ class BoostBcp < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "237fce500e3799cd93f53c5221448c915c9836a18b7b239dc19ec006e4291f80"
     sha256 cellar: :any_skip_relocation, catalina:      "2b679e21d034b4a68c4d1eeaaf6b4f8bbc6b5d456f396ef292009bdd874dda5b"
     sha256 cellar: :any_skip_relocation, mojave:        "78e095ee35b6a0821b1211d0a9e126b8dc62628531f6ed7180bcf3fc598550f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e3dcc40fae5c45dd9fc0dc681af64be3073855fca632ba7e8680e981615708a"
   end
 
   depends_on "boost-build" => :build
