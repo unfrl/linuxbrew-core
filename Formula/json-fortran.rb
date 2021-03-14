8facb3a560fe93de04050ea211b01fec139fc373
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 20:05:52 +0000

    json-fortran: update 8.2.1_1 bottle.

--- Formula/json-fortran.rb
+++ Formula/json-fortran.rb
@@ -8,10 +8,11 @@ class JsonFortran < Formula
   head "https://github.com/jacobwilliams/json-fortran.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7ac0a95d90734c8288c8cb423e3c4c6664e817993a7e426ca27c3806e3726c1a"
-    sha256 cellar: :any, big_sur:       "af1867b20867ccea69da9d21eeb7ec2649b76dbdffc092da138bbe23e4bb1801"
-    sha256 cellar: :any, catalina:      "d2d543adde373aa91e71598e3cde564ab0607ef2545a4d5873784c505e1b3dcd"
-    sha256 cellar: :any, mojave:        "68883cd0e3b1ec90239ca124711ba48d4c50f284384248088038ac0545504218"
+    sha256 cellar: :any,                 arm64_big_sur: "7ac0a95d90734c8288c8cb423e3c4c6664e817993a7e426ca27c3806e3726c1a"
+    sha256 cellar: :any,                 big_sur:       "af1867b20867ccea69da9d21eeb7ec2649b76dbdffc092da138bbe23e4bb1801"
+    sha256 cellar: :any,                 catalina:      "d2d543adde373aa91e71598e3cde564ab0607ef2545a4d5873784c505e1b3dcd"
+    sha256 cellar: :any,                 mojave:        "68883cd0e3b1ec90239ca124711ba48d4c50f284384248088038ac0545504218"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ac891f14e996746194e6a1dcec7bb53ace0e9dd7e145da448f6036d2a9e55f8"
   end
 
   depends_on "cmake" => :build
