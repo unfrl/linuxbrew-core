c20bf84e4f635757677679a5be3bb7f0a8b76437
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 20:01:47 +0000

    sqliteodbc: update 0.9998 bottle.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -11,10 +11,11 @@ class Sqliteodbc < Formula
   end
 
   bottle do
-    sha256 cellar: :any, big_sur:     "5f98876aef9733997e750451ee0e3db30cc2bd1f371aa690f08d7e4038f11958"
-    sha256 cellar: :any, catalina:    "d0105cc73d44561e636923adb520710cdd7e0db835c6b31f151fe8a66a1b4fcc"
-    sha256 cellar: :any, mojave:      "6499af774d13212bf19dfdbd14c18feadf516a5d6afbd2ebe7718d99db1723eb"
-    sha256 cellar: :any, high_sierra: "6220e24f32b5b26c5c983c9f9fb1aaa6aba7c13cad44a7500ecb72c7d7723a80"
+    sha256 cellar: :any,                 big_sur:      "5f98876aef9733997e750451ee0e3db30cc2bd1f371aa690f08d7e4038f11958"
+    sha256 cellar: :any,                 catalina:     "d0105cc73d44561e636923adb520710cdd7e0db835c6b31f151fe8a66a1b4fcc"
+    sha256 cellar: :any,                 mojave:       "6499af774d13212bf19dfdbd14c18feadf516a5d6afbd2ebe7718d99db1723eb"
+    sha256 cellar: :any,                 high_sierra:  "6220e24f32b5b26c5c983c9f9fb1aaa6aba7c13cad44a7500ecb72c7d7723a80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "80fb4127ce50b43d98d4145585ff66471b4a9cc9de9191c2f5a52450b8dadac6"
   end
 
   depends_on "sqlite"
