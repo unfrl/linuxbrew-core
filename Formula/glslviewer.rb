24d3d74988c925a469b64ea9ea78390278962b91
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 07:04:48 +0000

    glslviewer: update 1.6.8 bottle.

--- Formula/glslviewer.rb
+++ Formula/glslviewer.rb
@@ -7,10 +7,11 @@ class Glslviewer < Formula
   head "https://github.com/patriciogonzalezvivo/glslViewer.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:     "f3ce8f0e2fc1954a66507e0035001a53999b3db181800a7e603adb49d2f545e7"
-    sha256 cellar: :any, catalina:    "8af79674f9aec125cac3abaca849ac39aaaf6189685818d95509cec45c026536"
-    sha256 cellar: :any, mojave:      "db43cd0ba38d9cf6a1d73f2be1b4d4d3bc0c8224219ef9a8c15449c89f75bd51"
-    sha256 cellar: :any, high_sierra: "b491ac4f3101dd7c0ba68d66f0df68400522aa1ab9d777f7c1f29ffabb56c3c2"
+    sha256 cellar: :any,                 big_sur:      "f3ce8f0e2fc1954a66507e0035001a53999b3db181800a7e603adb49d2f545e7"
+    sha256 cellar: :any,                 catalina:     "8af79674f9aec125cac3abaca849ac39aaaf6189685818d95509cec45c026536"
+    sha256 cellar: :any,                 mojave:       "db43cd0ba38d9cf6a1d73f2be1b4d4d3bc0c8224219ef9a8c15449c89f75bd51"
+    sha256 cellar: :any,                 high_sierra:  "b491ac4f3101dd7c0ba68d66f0df68400522aa1ab9d777f7c1f29ffabb56c3c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2b32fcea4ed5faaac5a3a179b28057f0bb866c8cc5d2cb56653e6077571f6902"
   end
 
   depends_on "pkg-config" => :build
