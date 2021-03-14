640f291e8729cffb6171859323fd207b73c01c9f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 20:04:27 +0000

    nng: update 1.4.0 bottle.

--- Formula/nng.rb
+++ Formula/nng.rb
@@ -11,10 +11,11 @@ class Nng < Formula
   end
 
   bottle do
-    sha256 arm64_big_sur: "2e6c5612976c754881d86240a6ba850330ab06cc99e6b4d292d354f1ca8e88b3"
-    sha256 big_sur:       "116a7c22766d5db5ae416e06ea53f15e0511520319a7500e2b5c7cc28198c166"
-    sha256 catalina:      "fd68af7c0f9cabe18c673c60e92f4252d10e91b6f979c8bb625d7c48698e1655"
-    sha256 mojave:        "ca12fdc7635dac1cc2b8551ca5d72b51d01a8b4ab775bc6555cc6aaf048c7f20"
+    sha256                               arm64_big_sur: "2e6c5612976c754881d86240a6ba850330ab06cc99e6b4d292d354f1ca8e88b3"
+    sha256                               big_sur:       "116a7c22766d5db5ae416e06ea53f15e0511520319a7500e2b5c7cc28198c166"
+    sha256                               catalina:      "fd68af7c0f9cabe18c673c60e92f4252d10e91b6f979c8bb625d7c48698e1655"
+    sha256                               mojave:        "ca12fdc7635dac1cc2b8551ca5d72b51d01a8b4ab775bc6555cc6aaf048c7f20"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc8052d54c5b4ca278a949ca2d5c1a4375b9c33033ab311729b9dd1e399d48b5"
   end
 
   depends_on "asciidoctor" => :build
