7fe7325d84a5f417997c5f05140de50d720f2acf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-14 08:24:15 +0000

    sysdig: update 0.27.1 bottle.

--- Formula/sysdig.rb
+++ Formula/sysdig.rb
@@ -11,10 +11,11 @@ class Sysdig < Formula
   end
 
   bottle do
-    sha256 big_sur:     "9a5db7570b079c111525694e6aef53bab77f7633fd30550c174fdd0d8b241ce1"
-    sha256 catalina:    "a3343b2e42ca8df82c537170b0338d965bc0c92619f760d86f30d6f898610e5f"
-    sha256 mojave:      "843c5ff29eb7787e1bfc5393f0562e6f97c121504f652f5fd806943dc971b97e"
-    sha256 high_sierra: "f314c5445253bd052d4a7f50b3e879a81514f14c4d44b3887da26e69fbdce17f"
+    sha256 big_sur:      "9a5db7570b079c111525694e6aef53bab77f7633fd30550c174fdd0d8b241ce1"
+    sha256 catalina:     "a3343b2e42ca8df82c537170b0338d965bc0c92619f760d86f30d6f898610e5f"
+    sha256 mojave:       "843c5ff29eb7787e1bfc5393f0562e6f97c121504f652f5fd806943dc971b97e"
+    sha256 high_sierra:  "f314c5445253bd052d4a7f50b3e879a81514f14c4d44b3887da26e69fbdce17f"
+    sha256 x86_64_linux: "a3202679c53b061ee5d3161895f85458177f39a3d7cc7609e9a889f10fafc363"
   end
 
   depends_on "cmake" => :build
