aef67d95a8a8deb14a8e06e3663d33bab7a30769
Michka Popoff <michkapopoff@gmail.com>
2021-03-15 22:30:18 +0100

    softhsm: build with system gcc
    Closes #22593.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/softhsm.rb
+++ Formula/softhsm.rb
@@ -4,6 +4,7 @@ class Softhsm < Formula
   url "https://dist.opendnssec.org/source/softhsm-2.6.1.tar.gz"
   sha256 "61249473054bcd1811519ef9a989a880a7bdcc36d317c9c25457fc614df475f2"
   license "BSD-2-Clause"
+  revision 1 unless OS.mac?
 
   # We check the GitHub repo tags instead of https://dist.opendnssec.org/source/
   # since the aforementioned first-party URL has a tendency to lead to an
@@ -20,7 +21,6 @@ class Softhsm < Formula
     sha256 catalina:      "6da111cdadbcf0127882e2bec5b3844454fd9b4e00a08d1fa49aa2f389b7062c"
     sha256 mojave:        "b7abd86dfec3d10f5e5cde00f2bcd5e0e19e2d9674c50a431db1195c4655dfec"
     sha256 high_sierra:   "73c40f26209dbf29280c16aefdfb492c749d8e14e4cbf83dc2a5b566c22f6bc9"
-    sha256 x86_64_linux:  "04493bcec27031457239fe9308c482bdc42f5ac115fd2dd055e35b2cd5f68566"
   end
 
   head do
@@ -33,12 +33,6 @@ class Softhsm < Formula
   end
 
   depends_on "openssl@1.1"
-
-  unless OS.mac?
-    fails_with gcc: "5"
-    fails_with gcc: "6"
-    depends_on "gcc@7"
-  end
 
   def install
     system "sh", "./autogen.sh" if build.head?
