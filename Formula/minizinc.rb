5340a4bd8dd2114c8af64ea75e5bfd4307625b76
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-16 18:22:16 +0000

    minizinc: update 2.5.4 bottle.

--- Formula/minizinc.rb
+++ Formula/minizinc.rb
@@ -7,10 +7,11 @@ class Minizinc < Formula
   head "https://github.com/MiniZinc/libminizinc.git", branch: "develop"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9256946ee7ba953c1bf1044f8fdc3fa6f90f5e99aedeea1d9fd84ee503b09893"
-    sha256 cellar: :any, big_sur:       "e496b615ec3bcdb6492dd4a2378f27f5c951e88893f8b363a398d9af310c8df2"
-    sha256 cellar: :any, catalina:      "ab12b7b6f4d83c01b8acad60f752c241e279eba8c04cdd1d8eddca279d1ee999"
-    sha256 cellar: :any, mojave:        "54a3283ef836d4efb4c121fbd5941ef37ef1de9ea8186d1ca5cee8ff427d64b4"
+    sha256 cellar: :any,                 arm64_big_sur: "9256946ee7ba953c1bf1044f8fdc3fa6f90f5e99aedeea1d9fd84ee503b09893"
+    sha256 cellar: :any,                 big_sur:       "e496b615ec3bcdb6492dd4a2378f27f5c951e88893f8b363a398d9af310c8df2"
+    sha256 cellar: :any,                 catalina:      "ab12b7b6f4d83c01b8acad60f752c241e279eba8c04cdd1d8eddca279d1ee999"
+    sha256 cellar: :any,                 mojave:        "54a3283ef836d4efb4c121fbd5941ef37ef1de9ea8186d1ca5cee8ff427d64b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ca48b99db532d63e3323bc74e9f7e81c88521f9fa05ab7fde2ffa03f0fe66c7"
   end
 
   depends_on "cmake" => :build
