bfd50c87b816b0a9da66ebb33c859a5d23f89941
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-16 20:18:54 +0000

    elixir: update 1.11.4 bottle.

--- Formula/elixir.rb
+++ Formula/elixir.rb
@@ -11,6 +11,7 @@ class Elixir < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "74bfd7543233a3ea0b1524fa1423410f33bac4e8ee7252508f1cb263037d5672"
     sha256 cellar: :any_skip_relocation, catalina:      "5c4163b77a321f6808c9a1014c136cc93159158340643ceda82658374d92a0d9"
     sha256 cellar: :any_skip_relocation, mojave:        "c79b9f898eac5d4af4059ef72f806a76989d373780630a7652f7512de115f245"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6e8bbd670a3132ae224d4620e06b7c04e0ffd111067bc8251de0aff9489cfb8"
   end
 
   depends_on "erlang"
