91e6362d791374ccadbcf7c8a8a2c2f47e8e0f4e
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-03-16 06:28:09 +0000

    libebml: update 1.4.2_1 bottle.

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -12,6 +12,7 @@ class Libebml < Formula
     sha256 cellar: :any,                 big_sur:       "de4edaae6d3f42a388be996f448b582262e39e923acc9ccef881a20ffa817d38"
     sha256 cellar: :any,                 catalina:      "20a71bb0c2babdc04f179dc77c7a03c2f2f2031e7d8d87fbf9d3c41ee831addc"
     sha256 cellar: :any,                 mojave:        "c3c91dc9f86978012a06f299115bc088e5ea0af6aec2e915d0f8338c4c0edd03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6be2199cf8e4c02b7d8cb0ecc4e51f4498da77b58a7bccc3aa4e356e454d7be5"
   end
 
   depends_on "cmake" => :build
