03cee23ceef760ba743129862cd9dd071cc0076b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-17 21:39:02 +0000

    k6: update 0.31.1 bottle.

--- Formula/k6.rb
+++ Formula/k6.rb
@@ -10,6 +10,7 @@ class K6 < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "7fbe31dd70a66df0c6b7fb438c5062e533426292f979f28c8fd521bcb6d430fe"
     sha256 cellar: :any_skip_relocation, catalina:      "e3a8be0eb13893be9f7c5ee90da5f1e4b3b4d988eaf50fcaeaacf540df047e08"
     sha256 cellar: :any_skip_relocation, mojave:        "080495ac7d5c9d19fd3a55d9f383411fdc3c1dee73d63443122c308da99ac04c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d7c35c2a4607314ea3e9333936da9e682f66c2864c6f8542fdc41e8ed5b3b08d"
   end
 
   depends_on "go" => :build
