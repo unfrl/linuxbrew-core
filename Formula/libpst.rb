3f7a0aba7ece156cfcbaa6921895fa94e38ab72e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-17 05:24:29 +0000

    libpst: update 0.6.75 bottle.

--- Formula/libpst.rb
+++ Formula/libpst.rb
@@ -12,10 +12,11 @@ class Libpst < Formula
 
   bottle do
     rebuild 2
-    sha256 cellar: :any, arm64_big_sur: "669e325cb32cbad435d86606d40012aee6d9101b2ffbc6efc9fa101e9bcdf97f"
-    sha256 cellar: :any, big_sur:       "6f48557a8529e0bc989baaa72788c52289896194e069217bf8fe5cc771207a22"
-    sha256 cellar: :any, catalina:      "cbf301e72e23ecad7be367063b933bb9ce0ea430f5af413ad44f71b04e4ccae3"
-    sha256 cellar: :any, mojave:        "b5dff8dd482a5688ce97bc7407ad7a18d620dc264ba1962e155e862ae2973d2b"
+    sha256 cellar: :any,                 arm64_big_sur: "669e325cb32cbad435d86606d40012aee6d9101b2ffbc6efc9fa101e9bcdf97f"
+    sha256 cellar: :any,                 big_sur:       "6f48557a8529e0bc989baaa72788c52289896194e069217bf8fe5cc771207a22"
+    sha256 cellar: :any,                 catalina:      "cbf301e72e23ecad7be367063b933bb9ce0ea430f5af413ad44f71b04e4ccae3"
+    sha256 cellar: :any,                 mojave:        "b5dff8dd482a5688ce97bc7407ad7a18d620dc264ba1962e155e862ae2973d2b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80dabad2f545d45a720e235efebe37690488a7070ed3730e1fbd0a632156a1dc"
   end
 
   depends_on "pkg-config" => :build
