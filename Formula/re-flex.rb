699792ce289660d9279f0cc5dd36203c4fa05adb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-17 05:46:24 +0000

    re-flex: update 3.0.1 bottle.

--- Formula/re-flex.rb
+++ Formula/re-flex.rb
@@ -10,6 +10,7 @@ class ReFlex < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b81ec2d7ce47750d4e1c7c9f3ef6f1dfb6725b64c15dc91b7a795116088c5ddc"
     sha256 cellar: :any_skip_relocation, catalina:      "7e662642c4a570ffabfc137b9c5259b687bd438deaf0da4909ab18f4c877c69b"
     sha256 cellar: :any_skip_relocation, mojave:        "21297a3a6515110965ace284f6055015439677f6edf811f2803560318f8d286f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "804236c313f3521525dcd0afef9bf8ca9e3c5db5065b5c16ed33bcbcedc914ae"
   end
 
   depends_on "pcre2"
