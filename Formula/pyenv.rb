71ebb80790693a9ef0e8d284aa7a056f80693cbd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-18 18:49:09 +0000

    pyenv: update 1.2.24 bottle.

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -13,10 +13,11 @@ class Pyenv < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "433f3684d795186f16ff6669dc28c5130f0627eeb624f5cdfd9292220ebe7e22"
-    sha256 cellar: :any, big_sur:       "c059ce48f3caa8a7f7a260e18e4491eb70e8250feaef787998d65ec457d6bd46"
-    sha256 cellar: :any, catalina:      "ea35df8bdb58da7af95f22c965b94dbfd62cd2c59482b85c5189ada6e2dea28a"
-    sha256 cellar: :any, mojave:        "1802e9dbbda4d6c2e73fd0c414c36bc04772ce59c6f52e3f68ba4284184efccf"
+    sha256 cellar: :any,                 arm64_big_sur: "433f3684d795186f16ff6669dc28c5130f0627eeb624f5cdfd9292220ebe7e22"
+    sha256 cellar: :any,                 big_sur:       "c059ce48f3caa8a7f7a260e18e4491eb70e8250feaef787998d65ec457d6bd46"
+    sha256 cellar: :any,                 catalina:      "ea35df8bdb58da7af95f22c965b94dbfd62cd2c59482b85c5189ada6e2dea28a"
+    sha256 cellar: :any,                 mojave:        "1802e9dbbda4d6c2e73fd0c414c36bc04772ce59c6f52e3f68ba4284184efccf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c144d06fd780fd9beb6b2f0e8b3d2e895ae339a780f5089863a29e7beeacb2b2"
   end
 
   depends_on "autoconf"
