85abb6ee6203d602c5adf66a9b8bd18b1ae0411c
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-03-20 17:44:47 +0000

    binaryen: update 100_1 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -8,10 +8,11 @@ class Binaryen < Formula
   head "https://github.com/WebAssembly/binaryen.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "44403c6f0ffc22a7f270fe936702d62981535e234f38f8d95544c9c663bacbbd"
-    sha256 cellar: :any, big_sur:       "dc1687811e112b21a6fb4aac217bf12ecedba0ccfe806fdb537a19c48bfa9ec8"
-    sha256 cellar: :any, catalina:      "abdee68b35c42a59d9a71f38f50e704fba8c9b475d7df8f34957c1b1c4221d3c"
-    sha256 cellar: :any, mojave:        "bc4ca1066209a412279aaeafb2c2a5c6472fd5cc8dbcc88c8aa91da0eadee680"
+    sha256 cellar: :any,                 arm64_big_sur: "44403c6f0ffc22a7f270fe936702d62981535e234f38f8d95544c9c663bacbbd"
+    sha256 cellar: :any,                 big_sur:       "dc1687811e112b21a6fb4aac217bf12ecedba0ccfe806fdb537a19c48bfa9ec8"
+    sha256 cellar: :any,                 catalina:      "abdee68b35c42a59d9a71f38f50e704fba8c9b475d7df8f34957c1b1c4221d3c"
+    sha256 cellar: :any,                 mojave:        "bc4ca1066209a412279aaeafb2c2a5c6472fd5cc8dbcc88c8aa91da0eadee680"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "65d39f7deb12ce5d587e63e42468a45abbeb9c619e52116db1d27c8a67708707"
   end
 
   depends_on "cmake" => :build
