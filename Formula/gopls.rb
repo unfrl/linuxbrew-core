7a67596e77bf8ff587c266e97098a4698c090f92
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-20 09:44:36 +0000

    gopls: update 0.6.9 bottle.

--- Formula/gopls.rb
+++ Formula/gopls.rb
@@ -16,6 +16,7 @@ class Gopls < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "29c406b7357ab2082b95110ee9d96f3ce322293ccd248ed611a13fadc56aa88f"
     sha256 cellar: :any_skip_relocation, catalina:      "3f5a24b417b7a52ebe3f9b6095d5e2414636c28c523ba42cf0b1f49f7efe4a3c"
     sha256 cellar: :any_skip_relocation, mojave:        "fc452dcf3a2ba8068ac6e301e90f25addfc1a3022d0621bbf0b8fec8ffab8433"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0bace55b7ee7cb966e1c40606274c0bb912dd19458f52462bacdc19383e5ba71"
   end
 
   depends_on "go" => :build
