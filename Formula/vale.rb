c5240b16d42650d5a4e70678ac1d8144e08d7707
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-21 08:45:12 +0000

    vale: update 2.10.2 bottle.

--- Formula/vale.rb
+++ Formula/vale.rb
@@ -10,6 +10,7 @@ class Vale < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "b3e275586ef94ecde79577a2fce45cdfd5666e3ea084cdf646135a8aeaf25eca"
     sha256 cellar: :any_skip_relocation, catalina:      "ab18f36406506f0250b9067024c674bfb36cdfa737b87ec32a9009bacc8e9c40"
     sha256 cellar: :any_skip_relocation, mojave:        "f57c0467bb39ea4e241027cf72d7f1ac616d61654709da809fcffa9df4797f5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "baedfc991dcbbb8f59b6b4a2f165169998acbc3bb2a007b1c9e4ef11a53cd417"
   end
 
   depends_on "go" => :build
