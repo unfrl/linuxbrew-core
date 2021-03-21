b6bd424e48ca383b0171986a7bf72edc2c37efa6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-21 08:53:13 +0000

    binaryen: update 100_1 bottle.

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -9,10 +9,11 @@ class Binaryen < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "56165cbe52e86afde8f5030d13c60885f1e3a98013d81ae294f93b06e92c5da1"
-    sha256 cellar: :any, big_sur:       "6ec130eba9a753aeef345c1521613a950bc703943c86685a9eca97db367d29b0"
-    sha256 cellar: :any, catalina:      "8843e1dd958ab41d475afda03b3b27e62ea791d0bb7c515ee5b9804429c5c6f0"
-    sha256 cellar: :any, mojave:        "096b306c87fcb4b8c5e566bca323f9087ad04984c5aa36e51cfa90bc919bc512"
+    sha256 cellar: :any,                 arm64_big_sur: "56165cbe52e86afde8f5030d13c60885f1e3a98013d81ae294f93b06e92c5da1"
+    sha256 cellar: :any,                 big_sur:       "6ec130eba9a753aeef345c1521613a950bc703943c86685a9eca97db367d29b0"
+    sha256 cellar: :any,                 catalina:      "8843e1dd958ab41d475afda03b3b27e62ea791d0bb7c515ee5b9804429c5c6f0"
+    sha256 cellar: :any,                 mojave:        "096b306c87fcb4b8c5e566bca323f9087ad04984c5aa36e51cfa90bc919bc512"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4775455b373eb9a1e160e30ce4eaa0eaf3d370de8b33892927d62d63a8e0aee4"
   end
 
   depends_on "cmake" => :build
