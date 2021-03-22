db4cf3af1ae41fc2fddcf1864bbbc842bd44a59d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-22 06:39:44 +0000

    isort: update 5.8.0 bottle.

--- Formula/isort.rb
+++ Formula/isort.rb
@@ -17,6 +17,7 @@ class Isort < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "0ae4d37f79fed6ac903ce991e19888864673fef72f544e4e99e994d880437e52"
     sha256 cellar: :any_skip_relocation, catalina:      "95af85973a39cafec59d9f21a50608e09881ae7b5e70926616dcdf7ad70b7a98"
     sha256 cellar: :any_skip_relocation, mojave:        "253112ca0152460c548df9fac3d6607d73090950241d3af59282dabd74b8a607"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8fa7c556576206501c5a8f987542507c9f5d3b65cd38033bffdb32de3c9b3697"
   end
 
   depends_on "python@3.9"
