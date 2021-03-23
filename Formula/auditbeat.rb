e26790508ab93aed6a8d2f0053c97c6ba15a50d7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-23 22:29:38 +0000

    auditbeat: update 7.12.0 bottle.

--- Formula/auditbeat.rb
+++ Formula/auditbeat.rb
@@ -12,6 +12,7 @@ class Auditbeat < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "87e8ff8d3196c611ad6f35ed46127f4cd323f3a588ff5aedc5652aff85caf4a6"
     sha256 cellar: :any_skip_relocation, catalina:      "95aebf2b0d0d6c4333167b24f1e7d1c8b14afc719a4ed6f9fb35f74589c1afb1"
     sha256 cellar: :any_skip_relocation, mojave:        "2f9c1efbd1935b5c05b4853e7233d82de2379476f53960a1d814518aeccf4b9c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5214c6a380e1c9b82f14ad454e0f832f3bd4a8f2a4eb51fc376296d7fde2c957"
   end
 
   depends_on "go" => :build
