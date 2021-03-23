251b38c17ca3c45de60db8f1a50bebf10024e6ef
Michka Popoff <michkapopoff@gmail.com>
2021-03-23 19:54:15 +0100

    mad: remove bottle for rebuild

--- Formula/mad.rb
+++ Formula/mad.rb
@@ -16,7 +16,6 @@ class Mad < Formula
     sha256 cellar: :any, big_sur:       "0ad06329f73d5dc15cba262feca6e1c582e10ad3b9ca0476e46c37e6d878d0ab"
     sha256 cellar: :any, catalina:      "5416172dc7ccd3c5a5065b3f7dc18c00e83a7e20dfc6b09e0586afc4a76c5722"
     sha256 cellar: :any, mojave:        "5baadb23763805521d306268861ff82fe2055da1eb7976aaa7c78f83d3c2f43a"
-    sha256 cellar: :any, x86_64_linux:  "695b3347205276a26967bc850649784825eb7868c810cc3297c6a3049ddea710"
   end
 
   depends_on "autoconf" => :build
