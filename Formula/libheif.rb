0a278605ed14d5fc24d0922e60065be954fc9969
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-24 18:43:00 +0000

    libheif: update 1.11.0_2 bottle.

--- Formula/libheif.rb
+++ Formula/libheif.rb
@@ -7,10 +7,11 @@ class Libheif < Formula
   revision 2
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6fb1565de0cf3f02dacaf0c1ca8631132900ecd2fb6511d0e1286f854e7368f9"
-    sha256 cellar: :any, big_sur:       "aa29ab6b7ac495382a53400b355cc421c221f8f4aaff2a48ba2ee3a8432278be"
-    sha256 cellar: :any, catalina:      "04efc496cd625e79e884d7207f406bfdb06d60a5b5c347689d98b346e5bc896a"
-    sha256 cellar: :any, mojave:        "46dcec99a6e08a2bf6bafafe61619074c2db05be724aade8f335e808b2c9b346"
+    sha256 cellar: :any,                 arm64_big_sur: "6fb1565de0cf3f02dacaf0c1ca8631132900ecd2fb6511d0e1286f854e7368f9"
+    sha256 cellar: :any,                 big_sur:       "aa29ab6b7ac495382a53400b355cc421c221f8f4aaff2a48ba2ee3a8432278be"
+    sha256 cellar: :any,                 catalina:      "04efc496cd625e79e884d7207f406bfdb06d60a5b5c347689d98b346e5bc896a"
+    sha256 cellar: :any,                 mojave:        "46dcec99a6e08a2bf6bafafe61619074c2db05be724aade8f335e808b2c9b346"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10146de60f9ac4e4e4969a4a7b406ce42fcc452d8bab292a57d244691dfadad4"
   end
 
   depends_on "pkg-config" => :build
