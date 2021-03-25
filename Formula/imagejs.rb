d88ccbccc43aabb24d09b84eeec7b9eea3160430
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-25 04:04:32 +0000

    imagejs: update 0.7.2 bottle.

--- Formula/imagejs.rb
+++ Formula/imagejs.rb
@@ -17,6 +17,7 @@ class Imagejs < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "99e906e8eeb8451f8c2f8408aa990cddb575a02be4cdc5d4ea3f95362d040633"
     sha256 cellar: :any_skip_relocation, catalina:      "7bddae8dab41f73bce7acb3c86a6dc01dcd3edeb5e0abf80b155e498372b8e5e"
     sha256 cellar: :any_skip_relocation, mojave:        "4d071eb79a95c78c190c91ef8295b0a300a0ccdd525b401af2e797767bc54410"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc8e34d0fb9d4425bbb9cbd04bedfafa6d7d51974bbf2181c561981cee9bba73"
   end
 
   def install
