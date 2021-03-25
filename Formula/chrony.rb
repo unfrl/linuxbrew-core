4e18e840fd735b6a7123bcc80553613f2ca5e877
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-25 02:36:49 +0000

    chrony: update 4.0 bottle.

--- Formula/chrony.rb
+++ Formula/chrony.rb
@@ -11,6 +11,7 @@ class Chrony < Formula
     sha256 cellar: :any_skip_relocation, catalina:      "18080256097344abcf595e69bc8e0b15faefbe5e9d5e36f1326e0bae8e089d5d"
     sha256 cellar: :any_skip_relocation, mojave:        "5227442d8a26057125ac087fb30520dc65d65ee287ce56362d08b2f12e5e6f7c"
     sha256 cellar: :any_skip_relocation, high_sierra:   "c92b075e3cfd4419cf2339b2bfd779e2df4479ab58c23b19b396c4e85bdeb300"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f759aa50241d34bcb54d02fc53f4297450bedeea34ebb8534ada37e50496e918"
   end
 
   depends_on "nettle"
