28dbc1aed789fdec5c5a6ecddebc36b327b9e923
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-26 04:05:00 +0000

    ki18n: update 5.79.0_1 bottle.

--- Formula/ki18n.rb
+++ Formula/ki18n.rb
@@ -12,10 +12,11 @@ class Ki18n < Formula
   head "https://invent.kde.org/frameworks/ki18n.git"
 
   bottle do
-    sha256 arm64_big_sur: "61c81f21327971452bcdba40f415d299d7a4e9c727060bcc377b065518b91087"
-    sha256 big_sur:       "4d899f8c4dddb4cdc41a13aa957292c576c5849940e6c775a170cccd5a1b2395"
-    sha256 catalina:      "3845dad0bb48a790e387e9a361ff608fd417b28016c4a010e88cad1086a40b9a"
-    sha256 mojave:        "ced88da8dc87bfa0713f28de2b02a51e505bef317756d3efbb1c435821d9ba6c"
+    sha256                               arm64_big_sur: "61c81f21327971452bcdba40f415d299d7a4e9c727060bcc377b065518b91087"
+    sha256                               big_sur:       "4d899f8c4dddb4cdc41a13aa957292c576c5849940e6c775a170cccd5a1b2395"
+    sha256                               catalina:      "3845dad0bb48a790e387e9a361ff608fd417b28016c4a010e88cad1086a40b9a"
+    sha256                               mojave:        "ced88da8dc87bfa0713f28de2b02a51e505bef317756d3efbb1c435821d9ba6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "01ece043625c7b0c32ea8aa990876d2082bc5fe31b11119f9facc68bc246a298"
   end
 
   depends_on "cmake" => [:build, :test]
