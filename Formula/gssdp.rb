8aafd5df9675090912f34d3f1339f3195a79f974
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-26 07:08:53 +0000

    gssdp: update 1.2.3_1 bottle.

--- Formula/gssdp.rb
+++ Formula/gssdp.rb
@@ -6,11 +6,12 @@ class Gssdp < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "e3bc6dfa4ed41b0629533dbbe8fc4d9132a4349e05aacf0ea8099bf868fd6951"
-    sha256 cellar: :any, big_sur:       "f5b00ceef2fed5c0140a8983fc8fed49cd220d1ad0cf1718125a81a047e370c3"
-    sha256 cellar: :any, catalina:      "9cda1333eede84e831da2553e50989bd5721460b0ab046c95414305c11e29adc"
-    sha256 cellar: :any, mojave:        "de497cd6d3225d91ce49ef33b23928bb8af0d5cdebea072e06c8cf022a7a5dda"
-    sha256 cellar: :any, high_sierra:   "c6c767ccfe0b7220929d94ce06d3c4d5f8f172ab03e2a65900d96e1f2b151595"
+    sha256 cellar: :any,                 arm64_big_sur: "e3bc6dfa4ed41b0629533dbbe8fc4d9132a4349e05aacf0ea8099bf868fd6951"
+    sha256 cellar: :any,                 big_sur:       "f5b00ceef2fed5c0140a8983fc8fed49cd220d1ad0cf1718125a81a047e370c3"
+    sha256 cellar: :any,                 catalina:      "9cda1333eede84e831da2553e50989bd5721460b0ab046c95414305c11e29adc"
+    sha256 cellar: :any,                 mojave:        "de497cd6d3225d91ce49ef33b23928bb8af0d5cdebea072e06c8cf022a7a5dda"
+    sha256 cellar: :any,                 high_sierra:   "c6c767ccfe0b7220929d94ce06d3c4d5f8f172ab03e2a65900d96e1f2b151595"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf2ebb9befdba750d38132adfb70c865c26c5524b09d1f182004925f45bfbbf1"
   end
 
   depends_on "gobject-introspection" => :build
