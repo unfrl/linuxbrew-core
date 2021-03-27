ab7d0b24fd20424ebe8af63503adc03fafdceb61
Michka Popoff <michkapopoff@gmail.com>
2021-03-27 19:27:21 +0100

    stella: build with gcc
    Closes #22757.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/stella.rb
+++ Formula/stella.rb
@@ -4,25 +4,22 @@ class Stella < Formula
   url "https://github.com/stella-emu/stella/releases/download/6.5.2/stella-6.5.2-src.tar.xz"
   sha256 "dc2709d1501d33d9ec82cfeeedd6097993f3e2b117dde62092f2e604ba30bf99"
   license "GPL-2.0-or-later"
+  revision 1 unless OS.mac?
   head "https://github.com/stella-emu/stella.git"
 
   bottle do
     sha256 cellar: :any,                 big_sur:      "a470ccd8535c906aae5aa63c595fae6946d4145afcf75eb626216c7822a52484"
     sha256 cellar: :any,                 catalina:     "19242437c7f91e204b162f8eb542fd76ab6cd4facb62904ecd4f5187ff88da8f"
     sha256 cellar: :any,                 mojave:       "b566cac3954c8b1c773845d3c55a23fc4f720ad41c16d51f4ec18eeddb58965a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "19996c9efbf8c4044276b6ff4f821bc287e1d6888448f37e812dcbab59e1e0d8"
   end
 
   depends_on xcode: :build
   depends_on "libpng"
   depends_on "sdl2"
 
-  # Stella is using c++14
   unless OS.mac?
     fails_with gcc: "5"
-    fails_with gcc: "6"
-    fails_with gcc: "7"
-    depends_on "gcc@8"
+    depends_on "gcc"
   end
 
   uses_from_macos "zlib"
