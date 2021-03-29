0aebc3d63fc7ffba3fe3e61cec9c6ee205e2a3ed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-29 21:02:03 +0000

    libtrng: update 4.24 bottle.

--- Formula/libtrng.rb
+++ Formula/libtrng.rb
@@ -12,10 +12,11 @@ class Libtrng < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "9cfa0851919690b182b5cd227c71e77b7f07f5179ff5d06b52894fb98ca131df"
-    sha256 cellar: :any, big_sur:       "c97a7c825b5a6614dd771cef5f0aebdadb70f5b619e19aa446afff5072ec236d"
-    sha256 cellar: :any, catalina:      "044b708b751a88a22b95e4b75c47a8125fe017d6e69ea39c1177c9bc06c0de85"
-    sha256 cellar: :any, mojave:        "85e7a9b91ec9df836ce6127af7ca09deffd9052136c34ae1b0d3e310467eddc9"
+    sha256 cellar: :any,                 arm64_big_sur: "9cfa0851919690b182b5cd227c71e77b7f07f5179ff5d06b52894fb98ca131df"
+    sha256 cellar: :any,                 big_sur:       "c97a7c825b5a6614dd771cef5f0aebdadb70f5b619e19aa446afff5072ec236d"
+    sha256 cellar: :any,                 catalina:      "044b708b751a88a22b95e4b75c47a8125fe017d6e69ea39c1177c9bc06c0de85"
+    sha256 cellar: :any,                 mojave:        "85e7a9b91ec9df836ce6127af7ca09deffd9052136c34ae1b0d3e310467eddc9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bad44ba108f2de0bba7033a4469c9397ec4204dc0d933bec8a5f1c81cbc3e5ea"
   end
 
   depends_on "cmake" => :build
