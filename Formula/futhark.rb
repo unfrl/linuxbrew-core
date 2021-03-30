c9d1702b747480e556803cdaf4dbfd9e9cd71930
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-30 21:50:50 +0000

    futhark: update 0.19.4 bottle.

--- Formula/futhark.rb
+++ Formula/futhark.rb
@@ -7,9 +7,10 @@ class Futhark < Formula
   head "https://github.com/diku-dk/futhark.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "198d61137b141bf43120943c38dc8844373fb98c200d9a6098e6cd29c19a5f90"
-    sha256 cellar: :any_skip_relocation, catalina: "0b30142279435d5f8121c86322d4ca36d5a7f0164abe92f7e5e0aa2fc3fd555a"
-    sha256 cellar: :any_skip_relocation, mojave:   "63a1cc382ee786c54bd2fbad440e7f1ee180b3e6697c187b5a4c58ab73fcba7b"
+    sha256 cellar: :any_skip_relocation, big_sur:      "198d61137b141bf43120943c38dc8844373fb98c200d9a6098e6cd29c19a5f90"
+    sha256 cellar: :any_skip_relocation, catalina:     "0b30142279435d5f8121c86322d4ca36d5a7f0164abe92f7e5e0aa2fc3fd555a"
+    sha256 cellar: :any_skip_relocation, mojave:       "63a1cc382ee786c54bd2fbad440e7f1ee180b3e6697c187b5a4c58ab73fcba7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9a16def90732cbad5dada165f52ded5743d21703f451159ebdd24aae20309518"
   end
 
   depends_on "cabal-install" => :build
