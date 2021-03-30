38316f2feafaf90116d4cbac51cb16776d818d0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-30 17:07:27 +0000

    git-delta: update 0.7.1 bottle.

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -11,6 +11,7 @@ class GitDelta < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "2c0b4c2decb0c447cdf66bcca647251e6ccc3684f5a060a950c7645c047e1a04"
     sha256 cellar: :any_skip_relocation, catalina:      "adf32ac4190cbf84233ab11c9ee0f39ff8698221fc81ef09f2dcf604631d062d"
     sha256 cellar: :any_skip_relocation, mojave:        "158e488f87574ced953ebc9b8d4de54ba2206eda1d3bda950ad39f9ab303e9e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c3b1bc46d0b6b7460cc7961d01c1e8be714c1e3e7a51d79af7228b5e6641c4b8"
   end
 
   depends_on "rust" => :build
