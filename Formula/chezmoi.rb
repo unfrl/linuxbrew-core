cb3b1c28131c5c8a91cb016623ea54c219e193a9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-30 17:24:58 +0000

    chezmoi: update 2.0.5 bottle.

--- Formula/chezmoi.rb
+++ Formula/chezmoi.rb
@@ -12,6 +12,7 @@ class Chezmoi < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "67e06b0a11870acbdbd39ed6bc5874485663fddab46cc80e86b55543dcf6d4cb"
     sha256 cellar: :any_skip_relocation, catalina:      "eff19428beac963ae024a527b10655b6c5c09e5537c98fdc043e011ba0d03732"
     sha256 cellar: :any_skip_relocation, mojave:        "0a0b16fda39952f86467c9a57ba7d6fd902646aa2197f8025cf9e706825c0e51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66d16faec4d63f5183c0faf465a00d875b4638d96a26afa510cf6ba9ce94f282"
   end
 
   depends_on "go" => :build
