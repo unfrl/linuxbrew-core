fb828491fc286114dca1f66c2ae907feddd21967
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-03-30 17:01:15 +0000

    pygobject3: update 3.40.1 bottle.

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -6,10 +6,11 @@ class Pygobject3 < Formula
   license "LGPL-2.1-or-later"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "ea716cafcd6905b1f49ad50e9e98a1cceb74be3cb641e8a1e2d670d28cc34103"
-    sha256 cellar: :any, big_sur:       "27af5bf268c1c2274d4f97b628b4df0c4fab81c092c8948bf3c6f5e875d6c45b"
-    sha256 cellar: :any, catalina:      "e24bd991a052b651a4d7178dd54b4e6a1e25895c3550cb3b3a1e84dfad66ec34"
-    sha256 cellar: :any, mojave:        "1a57d5580a83038a20d6256f0aa1398f79510c41eaf402cdbf4bba41a9154591"
+    sha256 cellar: :any,                 arm64_big_sur: "ea716cafcd6905b1f49ad50e9e98a1cceb74be3cb641e8a1e2d670d28cc34103"
+    sha256 cellar: :any,                 big_sur:       "27af5bf268c1c2274d4f97b628b4df0c4fab81c092c8948bf3c6f5e875d6c45b"
+    sha256 cellar: :any,                 catalina:      "e24bd991a052b651a4d7178dd54b4e6a1e25895c3550cb3b3a1e84dfad66ec34"
+    sha256 cellar: :any,                 mojave:        "1a57d5580a83038a20d6256f0aa1398f79510c41eaf402cdbf4bba41a9154591"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "febb044382d2e7330693d4349626b07531ede012ac6acae7dde97fb8301440be"
   end
 
   depends_on "meson" => :build
