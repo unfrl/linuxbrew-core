447ad6bb27b36d02080f22d5fdef22fd136ec41a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-01 11:08:11 +0000

    rabbitmq-c: update 0.11.0 bottle.

--- Formula/rabbitmq-c.rb
+++ Formula/rabbitmq-c.rb
@@ -7,10 +7,11 @@ class RabbitmqC < Formula
   head "https://github.com/alanxz/rabbitmq-c.git"
 
   bottle do
-    sha256 arm64_big_sur: "8c99de9859b3c8fd847d02a777f9f607942742c8e8c7ab11961c9f82cdd7521c"
-    sha256 big_sur:       "d8e5e9610c9209804710a07ad2accdd9e07df7c6ec0dbbc987193fb724801c0f"
-    sha256 catalina:      "2f15cd210e177b5d9156d3884f2b5fe735352820cc7b900a1c9dc951f56cb8e0"
-    sha256 mojave:        "ad16aaef6f362020519025f174bd72c4c74d007965209d9c89b55d0d989752cc"
+    sha256                               arm64_big_sur: "8c99de9859b3c8fd847d02a777f9f607942742c8e8c7ab11961c9f82cdd7521c"
+    sha256                               big_sur:       "d8e5e9610c9209804710a07ad2accdd9e07df7c6ec0dbbc987193fb724801c0f"
+    sha256                               catalina:      "2f15cd210e177b5d9156d3884f2b5fe735352820cc7b900a1c9dc951f56cb8e0"
+    sha256                               mojave:        "ad16aaef6f362020519025f174bd72c4c74d007965209d9c89b55d0d989752cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50a68c3486d930f0006e02891711f2953bee814bdadfa8e6db93731150fdb62e"
   end
 
   depends_on "cmake" => :build
