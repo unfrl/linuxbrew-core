eab67359dcbdddc691d39409096ccaf9adc9bc34
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-02 21:11:13 +0000

    pdftoipe: update 7.2.20.1_8 bottle.

--- Formula/pdftoipe.rb
+++ Formula/pdftoipe.rb
@@ -7,10 +7,11 @@ class Pdftoipe < Formula
   revision 8
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "43b0ee0611b0edb25891da16d05b72c527b97a74f27639bfad4d17ef3256ca1f"
-    sha256 cellar: :any, big_sur:       "a9229ec69c846d47beab278fda331680910cabee55d9faaae1c3cb410987083a"
-    sha256 cellar: :any, catalina:      "7cb8aff11c8626ce8b1de9f9292dc6c34fdc270dab3b4ceac5df9a2454acfce4"
-    sha256 cellar: :any, mojave:        "86c1b8f8e9878d4f8ba421c5ace5d78d541c68dc79fde6115e4855de2adb0a63"
+    sha256 cellar: :any,                 arm64_big_sur: "43b0ee0611b0edb25891da16d05b72c527b97a74f27639bfad4d17ef3256ca1f"
+    sha256 cellar: :any,                 big_sur:       "a9229ec69c846d47beab278fda331680910cabee55d9faaae1c3cb410987083a"
+    sha256 cellar: :any,                 catalina:      "7cb8aff11c8626ce8b1de9f9292dc6c34fdc270dab3b4ceac5df9a2454acfce4"
+    sha256 cellar: :any,                 mojave:        "86c1b8f8e9878d4f8ba421c5ace5d78d541c68dc79fde6115e4855de2adb0a63"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9298427c4e6e105fb3670ec576680abe94788231ef190ed93fdb890ba38eb83"
   end
 
   depends_on "pkg-config" => :build
