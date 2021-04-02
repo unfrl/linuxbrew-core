e343d3ea3a5006db2a1c73949c4f08accfc74766
Michka Popoff <michkapopoff@gmail.com>
2021-04-02 23:34:24 +0200

    shared-mime-info: remove workaround

--- Formula/shared-mime-info.rb
+++ Formula/shared-mime-info.rb
@@ -37,9 +37,6 @@ class SharedMimeInfo < Formula
   uses_from_macos "libxml2"
 
   def install
-    # Needed by intltool (xml::parser)
-    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5" unless OS.mac?
-
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
     # Disable the post-install update-mimedb due to crash
     mkdir "build" do
