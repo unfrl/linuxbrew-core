6c0d444e425a290ca84a857a27bf2178637f38a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-06 09:18:18 +0000

    libupnp: update 1.14.4 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -11,10 +11,11 @@ class Libupnp < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "442183f8a3b818a78bdf61f7c8f570428222c9e29cb1c05d558995d3b6a5f2f5"
-    sha256 cellar: :any, big_sur:       "03342b7244d40c4c29cbee728188da749b54973b4341d5cc65e152ad28b3f7b3"
-    sha256 cellar: :any, catalina:      "22be8335b2cae7ea2400aedc18e8de3e97546b8fc0a6a5ce9bb3525a0edbf081"
-    sha256 cellar: :any, mojave:        "976d36ef89fa0f5dd45a3804841d9434a0c385aa642a8b2142f116f65c8d8a5e"
+    sha256 cellar: :any,                 arm64_big_sur: "442183f8a3b818a78bdf61f7c8f570428222c9e29cb1c05d558995d3b6a5f2f5"
+    sha256 cellar: :any,                 big_sur:       "03342b7244d40c4c29cbee728188da749b54973b4341d5cc65e152ad28b3f7b3"
+    sha256 cellar: :any,                 catalina:      "22be8335b2cae7ea2400aedc18e8de3e97546b8fc0a6a5ce9bb3525a0edbf081"
+    sha256 cellar: :any,                 mojave:        "976d36ef89fa0f5dd45a3804841d9434a0c385aa642a8b2142f116f65c8d8a5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bd59b06b4c0749756dec9a798b5d6cddd142322a981c1a41e53e5e11f2e2f83"
   end
 
   def install
