9606a5a3b260a2b09aa6448a5706919f453423a1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-06 18:07:05 +0000

    re2: update 20210401 bottle.

--- Formula/re2.rb
+++ Formula/re2.rb
@@ -19,10 +19,11 @@ class Re2 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "738497c6fee23638e61ce8ccc4b42ca61dfce05d47fcecdcbd1488b567b59ce3"
-    sha256 cellar: :any, big_sur:       "aa7b6288f4e59f6fc9a9b7cab7bf20a48bf06ddd4247e14a70cf3dc6fe3bd5da"
-    sha256 cellar: :any, catalina:      "01071512713d4ff42a2158f00fce4d3ff6e614729afaa765c29f47980832a8cd"
-    sha256 cellar: :any, mojave:        "9fbf16117fa47ac6f0c3b247d13c34f4b1f986e0975cec141414452a6b504864"
+    sha256 cellar: :any,                 arm64_big_sur: "738497c6fee23638e61ce8ccc4b42ca61dfce05d47fcecdcbd1488b567b59ce3"
+    sha256 cellar: :any,                 big_sur:       "aa7b6288f4e59f6fc9a9b7cab7bf20a48bf06ddd4247e14a70cf3dc6fe3bd5da"
+    sha256 cellar: :any,                 catalina:      "01071512713d4ff42a2158f00fce4d3ff6e614729afaa765c29f47980832a8cd"
+    sha256 cellar: :any,                 mojave:        "9fbf16117fa47ac6f0c3b247d13c34f4b1f986e0975cec141414452a6b504864"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "357145a8c8c420a8d647b9e9de80b431dd5c17f0f48226b4b83c7e8dce097d90"
   end
 
   depends_on "cmake" => :build
