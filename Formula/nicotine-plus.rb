69ebee38317e89d2b1fdee5a385b3553df0e1810
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-06 13:20:15 +0000

    nicotine-plus: update 3.0.3 bottle.

--- Formula/nicotine-plus.rb
+++ Formula/nicotine-plus.rb
@@ -13,6 +13,7 @@ class NicotinePlus < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "0997da63eb74d5138330ef9bd0f82635971b29742f13e2d01e2c0f73fd8d64c3"
     sha256 cellar: :any_skip_relocation, catalina:      "872762c2cc58bee642e35770637471bc78533c379a15935e62e0d90406767fd2"
     sha256 cellar: :any_skip_relocation, mojave:        "7c97cf043c4ad24779b9e8f2ac571bcc728044822b482fefaa8f29be663b1cf9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ee27ba412cbf136a3dc1d621f399244a29920ffd2b7fd87d4bd41b202c24a9b"
   end
 
   depends_on "adwaita-icon-theme"
