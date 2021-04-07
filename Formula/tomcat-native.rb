38e44ece80e2d7ce7078398b5007c67d48696a81
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 11:52:46 +0000

    tomcat-native: update 1.2.28 bottle.

--- Formula/tomcat-native.rb
+++ Formula/tomcat-native.rb
@@ -7,10 +7,11 @@ class TomcatNative < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "74e7fc2f2daf20e093d04b2824e54f8ec18c232f4a35022f02a01a4773ffdda1"
-    sha256 cellar: :any, big_sur:       "48199e2a009ef4af86fe6baadb096c8e58f260aceb1cdd133cfafe23b611fd41"
-    sha256 cellar: :any, catalina:      "1c8a86437238fb62588115a1c2488b73285256fc599005344226b63d257e2342"
-    sha256 cellar: :any, mojave:        "fd3c098336a8cd6205c7239aa93215f3fc35c2ef6e1993b37e80911484a90dfc"
+    sha256 cellar: :any,                 arm64_big_sur: "74e7fc2f2daf20e093d04b2824e54f8ec18c232f4a35022f02a01a4773ffdda1"
+    sha256 cellar: :any,                 big_sur:       "48199e2a009ef4af86fe6baadb096c8e58f260aceb1cdd133cfafe23b611fd41"
+    sha256 cellar: :any,                 catalina:      "1c8a86437238fb62588115a1c2488b73285256fc599005344226b63d257e2342"
+    sha256 cellar: :any,                 mojave:        "fd3c098336a8cd6205c7239aa93215f3fc35c2ef6e1993b37e80911484a90dfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4780fb6251df1cad09eecd860e7058b65153b00b083cc822ea91888e851fb547"
   end
 
   depends_on "libtool" => :build
