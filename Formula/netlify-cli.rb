9f31e3b96df078beecc1e5d02cda24384ca649f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-07 12:45:29 +0000

    netlify-cli: update 3.15.1 bottle.

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -13,6 +13,7 @@ class NetlifyCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "bb460dc5ad2c70ff165f49be5816e4d035aef85a4a35c4a03cd52af939937bc7"
     sha256 cellar: :any_skip_relocation, catalina:      "53b63fa7bdc13a2cd0b08a00cf703a4dd4e78f69cd3e485313c7ef2ee4048c8f"
     sha256 cellar: :any_skip_relocation, mojave:        "0f8aef7f12fc7b00e07f06002f95dbf9db4d14bfe6fbea33a6f8c67f7d361afa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d3079f104a276ceb3c696d5165c1757f578a83ccfbf7b519758933848a42188"
   end
 
   depends_on "node"
