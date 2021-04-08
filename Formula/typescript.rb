016321e5f182ae44189e1c0701f013cc36ceba41
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-08 08:35:37 +0000

    typescript: update 4.2.4 bottle.

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -13,6 +13,7 @@ class Typescript < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a088a887d6c6638d808045034dd75177803d86eb313e604d1ab74336ba19d969"
     sha256 cellar: :any_skip_relocation, catalina:      "2c4605739270e5b3681adfd5fe9e3a1deef93b4f1790ed0c6cdb801727ca0080"
     sha256 cellar: :any_skip_relocation, mojave:        "6c53039ac025c59db02018d4a36f9d95000f7b4df5f22d751f26dcad9cc663ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8f2a84755f27a18396e9c2995deb1bc335718af60ad85cac10a769958323ad9"
   end
 
   depends_on "node"
