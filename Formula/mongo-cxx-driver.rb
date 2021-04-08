7fa45d9494402e98169b7ed4def686efb67a4c1e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-08 20:28:12 +0000

    mongo-cxx-driver: update 3.6.3 bottle.

--- Formula/mongo-cxx-driver.rb
+++ Formula/mongo-cxx-driver.rb
@@ -7,10 +7,11 @@ class MongoCxxDriver < Formula
   head "https://github.com/mongodb/mongo-cxx-driver.git"
 
   bottle do
-    sha256               arm64_big_sur: "f4eec5a80c592004a9c7fa39dbac3f271af4af0d834027c10df6b9df4f14c553"
-    sha256 cellar: :any, big_sur:       "bee19ded548d6ac9188e81937de9f8bea55cf7891f4acc212b719831af78697e"
-    sha256 cellar: :any, catalina:      "26a08f03803c3e8e609d9b401f0d8cbac566a1386737088cb9bbcca3e1974523"
-    sha256 cellar: :any, mojave:        "61cc88f607a5ff2cbd96f49df8b81f9f766eb76baff24ace975dc001ae605306"
+    sha256                               arm64_big_sur: "f4eec5a80c592004a9c7fa39dbac3f271af4af0d834027c10df6b9df4f14c553"
+    sha256 cellar: :any,                 big_sur:       "bee19ded548d6ac9188e81937de9f8bea55cf7891f4acc212b719831af78697e"
+    sha256 cellar: :any,                 catalina:      "26a08f03803c3e8e609d9b401f0d8cbac566a1386737088cb9bbcca3e1974523"
+    sha256 cellar: :any,                 mojave:        "61cc88f607a5ff2cbd96f49df8b81f9f766eb76baff24ace975dc001ae605306"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1430e936d1ac682656df66a0adb2b76d20375d732da4b4ad5d3f995ad3a11bc"
   end
 
   depends_on "cmake" => :build
