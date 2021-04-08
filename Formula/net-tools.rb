68c967137e1869a2ea45bf3417aa286a1d858d68
Daniel Nachun <daniel.nachun@gmail.com>
2021-04-07 23:31:25 -0700

    net-tools: use a tagged release, add missing license (#22895)

--- Formula/net-tools.rb
+++ Formula/net-tools.rb
@@ -1,13 +1,12 @@
 class NetTools < Formula
   desc "Linux networking base tools"
   homepage "https://sourceforge.net/projects/net-tools"
-  url "https://git.code.sf.net/p/net-tools/code.git",
-    # not specyfing a tag, cause upstream did not make a release since a long time...
-    revision: "aebd88ef8d6e15f673b62a649a50d07ed727c888"
-  version "1.60+"
+  url "https://downloads.sourceforge.net/project/net-tools/net-tools-2.10.tar.xz"
+  sha256 "b262435a5241e89bfa51c3cabd5133753952f7a7b7b93f32e08cb9d96f580d69"
+  license "GPL-2.0-or-later"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d3356573f08d651305f57af733130adabf68f5f13e92ff4db5cdc0c069a94f73"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "d3356573f08d651305f57af733130adabf68f5f13e92ff4db5cdc0c069a94f73"
   end
 
   depends_on "gettext" => :build
@@ -17,7 +16,7 @@ class NetTools < Formula
   def install
     system "yes '' | make config"
     system "make"
-    system "make", "DESTDIR=#{prefix}", "update"
+    system "make", "DESTDIR=#{prefix}", "install"
   end
 
   test do
