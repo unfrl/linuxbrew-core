af335ce8c472cd4857a643c5534ebcc12d8f89c3
Mike McQuaid <mike@mikemcquaid.com>
2021-04-09 10:58:47 +0100

    sdl2_ttf: remove broken bottle.
    Invalid checksum found through bulk GitHub Packages upload.

--- Formula/sdl2_ttf.rb
+++ Formula/sdl2_ttf.rb
@@ -18,7 +18,6 @@ class Sdl2Ttf < Formula
     sha256 cellar: :any, catalina:      "413959be382ea92bd59af9a29e5909d40db69c571447e2f0dec821cbff612d80"
     sha256 cellar: :any, mojave:        "74582129be8cfea5e556efa95411f9fc2eebf111c7b4f9affc80a7e05fa19cd9"
     sha256 cellar: :any, high_sierra:   "1867ff73485eaa12fc00def01be8e388443ac6c226065218bb435558fdb8bb22"
-    sha256 cellar: :any, x86_64_linux:  "15d01b047c2d7d93dc03572dc9b648fd4fa267fefeea48dd8b713f168d02c431"
   end
 
   depends_on "pkg-config" => :build
