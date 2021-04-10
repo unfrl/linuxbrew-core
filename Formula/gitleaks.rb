75ce3c5f9e6482293e8d4eb2760566f2e34ef57c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-10 16:45:20 +0000

    gitleaks: update 7.4.0 bottle.

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -10,6 +10,7 @@ class Gitleaks < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "14bc654c3ddbd94b7571218d14dfb810ea150d4bf20e195e09e95f3d42b1dcef"
     sha256 cellar: :any_skip_relocation, catalina:      "604a4492ffb2b96ab8ec302688f3c0e4dcdebb2664afde9eee9bc75122cbbca3"
     sha256 cellar: :any_skip_relocation, mojave:        "63a60a21f2b2f729cdbc17c8602c82e9f46a5397384091ce089ed743893f5a05"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21eb5d343519c99cf80e012de726359dc5262a578de0ed2bd15072a00924ab2a"
   end
 
   depends_on "go" => :build
