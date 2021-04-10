ef142ba8aec1d91f764a426d79db441f93b7938d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-10 16:46:37 +0000

    kumactl: update 1.1.2 bottle.

--- Formula/kumactl.rb
+++ Formula/kumactl.rb
@@ -10,6 +10,7 @@ class Kumactl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "179a1258c908dabf17e2e412cd32120514e0258b8ac6008aad3b0623faf2d6c5"
     sha256 cellar: :any_skip_relocation, catalina:      "f147de4ba6bdddac2b3aca4fbe88c93406301c58aab61e5ace3a65dd7ae07b09"
     sha256 cellar: :any_skip_relocation, mojave:        "a007c0b8e94d16bb3b47293a6303b21082728f3291d98381814075db36b889e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d014eba1ed803ced3393208ab4c895f795670c67807f37b853e5f09a0d8394b"
   end
 
   depends_on "go" => :build
