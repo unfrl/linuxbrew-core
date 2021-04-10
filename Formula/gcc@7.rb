24671e21527ebf321bdc94963238601ad4e819d7
Michka Popoff <michkapopoff@gmail.com>
2021-04-10 23:37:53 +0200

    gcc@7: prepare alignment with upstream

--- Formula/gcc@7.rb
+++ Formula/gcc@7.rb
@@ -64,15 +64,7 @@ class GccAT7 < Formula
 
     args = []
 
-    if OS.mac?
-      args += [
-        "--build=x86_64-apple-darwin#{OS.kernel_version}",
-        "--with-system-zlib",
-      ]
-    else
-      # Fix Linux error: gnu/stubs-32.h: No such file or directory.
-      args << "--disable-multilib"
-
+    unless OS.mac?
       # Change the default directory name for 64-bit libraries to `lib`
       # http://www.linuxfromscratch.org/lfs/view/development/chapter06/gcc.html
       inreplace "gcc/config/i386/t-linux64", "m64=../lib64", "m64="
@@ -98,10 +90,10 @@ class GccAT7 < Formula
       "--disable-nls",
     ]
 
-    # Xcode 10 dropped 32-bit support
-    args << "--disable-multilib" if OS.mac? && DevelopmentTools.clang_build_version >= 1000
-
     if OS.mac?
+      # Xcode 10 dropped 32-bit support
+      args << "--disable-multilib" if DevelopmentTools.clang_build_version >= 1000
+
       # System headers may not be in /usr/include
       sdk = MacOS.sdk_path_if_needed
       if sdk
@@ -109,15 +101,13 @@ class GccAT7 < Formula
         args << "--with-sysroot=#{sdk}"
       end
 
-      # Avoid reference to sed shim
-      args << "SED=/usr/bin/sed"
-    end
-
-    # Ensure correct install names when linking against libgcc_s;
-    # see discussion in https://github.com/Homebrew/homebrew/pull/34303
-    if OS.mac?
+      # Ensure correct install names when linking against libgcc_s;
+      # see discussion in https://github.com/Homebrew/homebrew/pull/34303
       inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}"
     end
+
+    # Fix Linux error: gnu/stubs-32.h: No such file or directory.
+    args << "--disable-multilib" unless OS.mac?
 
     mkdir "build" do
       system "../configure", *args
