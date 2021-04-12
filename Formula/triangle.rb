f24334e6157c142d9d5129b5bf358197a28be1a5
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-12 19:30:14 +0000

    triangle: update 1.1.2 bottle.

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -10,6 +10,7 @@ class Triangle < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "ad745cc5ef31c405301232d11cb75b768a001293a9d150704a4c955c8f38a698"
     sha256 cellar: :any_skip_relocation, catalina:      "34e735d76b7417fa426161d4ed2b27b891e0f040070ccef3d6f430a637b9c453"
     sha256 cellar: :any_skip_relocation, mojave:        "66ea6247ba2d105e5953814cdb9746ea2264a22689d879e16ace3c18e42f92f1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82c8ab7b5b5acf5279b70c9de3208dd78b561da0d5c9df461b37e376623a5d56"
   end
 
   depends_on "go" => :build
