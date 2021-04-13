0a342b98e585fb7dd2f8d2794430d2da424caf73
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-13 07:11:33 +0000

    vtk@8.2: update 8.2.0_6 bottle.

--- Formula/vtk@8.2.rb
+++ Formula/vtk@8.2.rb
@@ -7,10 +7,11 @@ class VtkAT82 < Formula
   revision 6
 
   bottle do
-    sha256 arm64_big_sur: "81fc6d19a33fa38e234b7d18bbbbc81fbd0b2996e402ba774c69193b53c358ba"
-    sha256 big_sur:       "8bc79eb0816f3fcbe54ffd10f6db58094aa8814e15e7fb6a7402ea65a1885be4"
-    sha256 catalina:      "b8b797c6faf44ded4565bfb887c14d915f3997fc646aef250c273d2cfbc5d7d8"
-    sha256 mojave:        "228d62e2e0f9933c9a967aa71394d062a23611e7fea37e430daeb49a70df4250"
+    sha256                               arm64_big_sur: "81fc6d19a33fa38e234b7d18bbbbc81fbd0b2996e402ba774c69193b53c358ba"
+    sha256                               big_sur:       "8bc79eb0816f3fcbe54ffd10f6db58094aa8814e15e7fb6a7402ea65a1885be4"
+    sha256                               catalina:      "b8b797c6faf44ded4565bfb887c14d915f3997fc646aef250c273d2cfbc5d7d8"
+    sha256                               mojave:        "228d62e2e0f9933c9a967aa71394d062a23611e7fea37e430daeb49a70df4250"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "706068dc28e32e3751a479034b414ca56b8a004f854d04371bf63af06ae98c44"
   end
 
   keg_only :versioned_formula
