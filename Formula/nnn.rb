212e846f72b6a5e04b48499a94e8aa9cb080f527
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-14 11:28:04 +0000

    nnn: update 4.0 bottle.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -7,10 +7,11 @@ class Nnn < Formula
   head "https://github.com/jarun/nnn.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "3121884b9703f5bc511499904d0cd65c941ce22cb80eeb0a0c650e75de0788e3"
-    sha256 cellar: :any, big_sur:       "e6ab60e20d2ec6e9ce86c7774843141e19ac3f71fdb1022b9e8a9206ee2a8f3a"
-    sha256 cellar: :any, catalina:      "abd54ea24cb7ed46303b925f22466592c0ceaffbe8b8a8ec67ac6421c05bf64a"
-    sha256 cellar: :any, mojave:        "16a859bc4fafbf733092be7c8b03a738f165b358f87b1738dee7b349e2e1c47a"
+    sha256 cellar: :any,                 arm64_big_sur: "3121884b9703f5bc511499904d0cd65c941ce22cb80eeb0a0c650e75de0788e3"
+    sha256 cellar: :any,                 big_sur:       "e6ab60e20d2ec6e9ce86c7774843141e19ac3f71fdb1022b9e8a9206ee2a8f3a"
+    sha256 cellar: :any,                 catalina:      "abd54ea24cb7ed46303b925f22466592c0ceaffbe8b8a8ec67ac6421c05bf64a"
+    sha256 cellar: :any,                 mojave:        "16a859bc4fafbf733092be7c8b03a738f165b358f87b1738dee7b349e2e1c47a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0ed6ae2e98541bf612b612fdc0a881f926f831e0f47b2c2a1e3cb5bf4829f3b"
   end
 
   depends_on "gnu-sed"
