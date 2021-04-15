dd302f3ee68eb0e907b34c35ee50da7be625bcd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-15 05:44:54 +0000

    oniguruma: update 6.9.7 bottle.

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -12,10 +12,11 @@ class Oniguruma < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "8b32f5922207998b2c90b6b03796b1831f5d491fb09d214bdf5d9393114de185"
-    sha256 cellar: :any, big_sur:       "822090a56cfd19f6fa3b2aecffa2ff29c6bee3eb5d3e349bd4a5dc1a9a218ba4"
-    sha256 cellar: :any, catalina:      "d9e578029ccb646a908d07906d7f749b83fbfc1fb94e5ab0b13c4cdb9100ccc4"
-    sha256 cellar: :any, mojave:        "ae496b74f528333b5fd8142513d581de829d93ff52d8d79b3bf1e8a711f39935"
+    sha256 cellar: :any,                 arm64_big_sur: "8b32f5922207998b2c90b6b03796b1831f5d491fb09d214bdf5d9393114de185"
+    sha256 cellar: :any,                 big_sur:       "822090a56cfd19f6fa3b2aecffa2ff29c6bee3eb5d3e349bd4a5dc1a9a218ba4"
+    sha256 cellar: :any,                 catalina:      "d9e578029ccb646a908d07906d7f749b83fbfc1fb94e5ab0b13c4cdb9100ccc4"
+    sha256 cellar: :any,                 mojave:        "ae496b74f528333b5fd8142513d581de829d93ff52d8d79b3bf1e8a711f39935"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4e42097d459de8bcc7e0f1f9d407820cd0a1abd18581d2b118feff03eac220a"
   end
 
   depends_on "autoconf" => :build
