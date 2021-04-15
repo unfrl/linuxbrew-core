8673a39006a29b542f93f90f8692ebe8885cc521
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-15 11:51:32 +0000

    kube-linter: update 0.2.0 bottle.

--- Formula/kube-linter.rb
+++ Formula/kube-linter.rb
@@ -8,9 +8,10 @@ class KubeLinter < Formula
   head "https://github.com/stackrox/kube-linter.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "f8172ff5c72ebaba2fc62a26274dfe5e35765fc3f225342c26eaae6fdabcb50f"
-    sha256 cellar: :any_skip_relocation, catalina: "f2430146fbe1a2dfd0ab2d2cd9e80ebed5ebf83abd2b9939c10e2945993605e1"
-    sha256 cellar: :any_skip_relocation, mojave:   "8572e63e9f3c061608bbfb4a4c1ed0921b783f9d2a5ded68a812f83bff814078"
+    sha256 cellar: :any_skip_relocation, big_sur:      "f8172ff5c72ebaba2fc62a26274dfe5e35765fc3f225342c26eaae6fdabcb50f"
+    sha256 cellar: :any_skip_relocation, catalina:     "f2430146fbe1a2dfd0ab2d2cd9e80ebed5ebf83abd2b9939c10e2945993605e1"
+    sha256 cellar: :any_skip_relocation, mojave:       "8572e63e9f3c061608bbfb4a4c1ed0921b783f9d2a5ded68a812f83bff814078"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "59568dd42f3dfadecfeb5374979d9be1421fc3b04ae232b991d9ec1917a05b0a"
   end
 
   depends_on "go" => :build
