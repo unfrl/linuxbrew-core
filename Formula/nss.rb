db8c22c282a4e3934b2f2360045a5a7ec5b40928
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-16 12:12:56 +0000

    nss: update 3.64 bottle.

--- Formula/nss.rb
+++ Formula/nss.rb
@@ -11,10 +11,11 @@ class Nss < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4df60fe6168e1485df625e6ecf6b0d967907e1dc031022bbf02c43d86dad3c2e"
-    sha256 cellar: :any, big_sur:       "cda883dfab17add553b83f64fad96cb231d1db29cacf4f60164f3f35409aaf1e"
-    sha256 cellar: :any, catalina:      "5d177f67d14001f5813a320859dacb56cc87bd783302e38c00cd8d276f6aed4a"
-    sha256 cellar: :any, mojave:        "72047921999b9062dae212a9211735805c6adb4b889d548cbfd6f5a2b7db013e"
+    sha256 cellar: :any,                 arm64_big_sur: "4df60fe6168e1485df625e6ecf6b0d967907e1dc031022bbf02c43d86dad3c2e"
+    sha256 cellar: :any,                 big_sur:       "cda883dfab17add553b83f64fad96cb231d1db29cacf4f60164f3f35409aaf1e"
+    sha256 cellar: :any,                 catalina:      "5d177f67d14001f5813a320859dacb56cc87bd783302e38c00cd8d276f6aed4a"
+    sha256 cellar: :any,                 mojave:        "72047921999b9062dae212a9211735805c6adb4b889d548cbfd6f5a2b7db013e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a22eb4b909d2237b226b1eee4b504afbf1dbe64206c2063bca01401dea4e7454"
   end
 
   depends_on "nspr"
