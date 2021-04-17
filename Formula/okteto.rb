67be0d351a6d8e5dc78764e1c929c98b38a94690
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-17 11:11:57 +0000

    okteto: update 1.12.7 bottle.

--- Formula/okteto.rb
+++ Formula/okteto.rb
@@ -11,6 +11,7 @@ class Okteto < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "fe1dee28d22b28bd91cd2fd249b0e2a3625cfca77561c87ef5976648c95a3246"
     sha256 cellar: :any_skip_relocation, catalina:      "242bcd95e78ade0ccd0842f61d59e7e63f4f030e1039f92f9f7dc1d2366ccd6a"
     sha256 cellar: :any_skip_relocation, mojave:        "e84998b5b0f3245d92b0f7a237459943c98826b6345bbfb349b2fc43275d53e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0a66c8baba63ad5cd8ecde1286f2a6f8a446bfcb7b2508c620561c3854ec121"
   end
 
   depends_on "go" => :build
