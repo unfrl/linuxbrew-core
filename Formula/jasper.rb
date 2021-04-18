383c6d588c5126401fd6b0d1dcc31b8accc1836b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-18 07:32:15 +0000

    jasper: update 2.0.31 bottle.

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -6,10 +6,11 @@ class Jasper < Formula
   license "JasPer-2.0"
 
   bottle do
-    sha256 arm64_big_sur: "71e7ff8e942a88baa807484a5f0d0160b66ddc5f37d2164072d400aa5a067865"
-    sha256 big_sur:       "adec70da00a25292537178182308b7d78ff730b4e0b8f80e4c000f147a53057e"
-    sha256 catalina:      "22f7db62bfc894c3836dc6c170399f767363efb589b9057a754238be5563f7aa"
-    sha256 mojave:        "2376a32bbfdcddc6f1893ae0e9a5832860fcc1c647397e32830576d8ef1a4d1e"
+    sha256                               arm64_big_sur: "71e7ff8e942a88baa807484a5f0d0160b66ddc5f37d2164072d400aa5a067865"
+    sha256                               big_sur:       "adec70da00a25292537178182308b7d78ff730b4e0b8f80e4c000f147a53057e"
+    sha256                               catalina:      "22f7db62bfc894c3836dc6c170399f767363efb589b9057a754238be5563f7aa"
+    sha256                               mojave:        "2376a32bbfdcddc6f1893ae0e9a5832860fcc1c647397e32830576d8ef1a4d1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a35cb8b6cd483edf648d2e18948024b0871ebaf424c0d75d8ba42fa3fa05424a"
   end
 
   depends_on "cmake" => :build
