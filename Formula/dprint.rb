fdfda73a17eecf23900bfec46b335eb6be084f48
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-18 07:40:25 +0000

    dprint: update 0.13.0 bottle.

--- Formula/dprint.rb
+++ Formula/dprint.rb
@@ -11,6 +11,7 @@ class Dprint < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "33452d1c0253206eda85aa47daafb8ac8b704763492fe9413b73df49fd857f62"
     sha256 cellar: :any_skip_relocation, catalina:      "cc1b40574893b04ecee7f6f8147b0ceb7df96fe426c91eb894660fbb33b7bd73"
     sha256 cellar: :any_skip_relocation, mojave:        "9f81a1a3e5fa078e60e65e60201eded2298db1e01acdac278ae2cbbb3810d1d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "254e3fe66344197ebc075210273b2e293a2534e171b93a00efa584efa07a57c3"
   end
 
   depends_on "rust" => :build
