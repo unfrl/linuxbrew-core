697a852d8ddf33ae328d3f67e52fe7fcf7fb5a18
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-21 21:26:54 +0000

    cglm: update 0.8.1 bottle.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -6,10 +6,11 @@ class Cglm < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "55f7f2b59e6aa0c4461dffb74b2e058649d33785371a3f46352c8c332ce49668"
-    sha256 cellar: :any, big_sur:       "573a6e6574facd161228dc7eaecee9d18ddf8451748a1913854b1c9fd2d10fe9"
-    sha256 cellar: :any, catalina:      "fee2fc016362262fd139e6ccfb7fcff3814e03b8c5d1d117ac5d149885b1101d"
-    sha256 cellar: :any, mojave:        "14e8e9b0e99b29115100a96ef2cbf2539c6a14bfd4d4608eb91b02538603ab61"
+    sha256 cellar: :any,                 arm64_big_sur: "55f7f2b59e6aa0c4461dffb74b2e058649d33785371a3f46352c8c332ce49668"
+    sha256 cellar: :any,                 big_sur:       "573a6e6574facd161228dc7eaecee9d18ddf8451748a1913854b1c9fd2d10fe9"
+    sha256 cellar: :any,                 catalina:      "fee2fc016362262fd139e6ccfb7fcff3814e03b8c5d1d117ac5d149885b1101d"
+    sha256 cellar: :any,                 mojave:        "14e8e9b0e99b29115100a96ef2cbf2539c6a14bfd4d4608eb91b02538603ab61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe2a374005feeb46f4cd594d22bea8e3bde8b92fc99dded47863059a013ca520"
   end
 
   depends_on "autoconf" => :build
