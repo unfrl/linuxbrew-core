5699551c733435ee744442ffe3623f76153602f3
danielnachun <daniel.nachun@gmail.com>
2021-04-21 16:16:44 -0700

    castxml: build with gcc-10
    Closes #23079.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/castxml.rb
+++ Formula/castxml.rb
@@ -22,6 +22,10 @@ class Castxml < Formula
   depends_on "cmake" => :build
   depends_on "llvm"
 
+  depends_on "gcc" unless OS.mac?
+
+  fails_with gcc: "5"
+
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
