e17ee11a9a0309bb4a86b65d7cfce7f8106753cf
danielnachun <daniel.nachun@gmail.com>
2021-04-21 23:22:37 -0700

    php: remove unneeded workaround

--- Formula/php.rb
+++ Formula/php.rb
@@ -412,7 +412,7 @@ class Php < Formula
       Process.wait(pid)
 
       fpm_pid = fork do
-        exec sbin/"php-fpm", *("--allow-to-run-as-root" if Process.uid.zero?), "-y", "fpm.conf"
+        exec sbin/"php-fpm", "-y", "fpm.conf"
       end
       pid = fork do
         exec Formula["httpd"].opt_bin/"httpd", "-X", "-f", "#{testpath}/httpd-fpm.conf"
