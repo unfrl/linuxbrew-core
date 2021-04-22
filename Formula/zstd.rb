9b8fa910747ccac654ebb1c103f3ba5deeb75b45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-22 09:15:02 +0000

    zstd: update 1.4.9_1 bottle.

--- Formula/zstd.rb
+++ Formula/zstd.rb
@@ -8,10 +8,11 @@ class Zstd < Formula
   head "https://github.com/facebook/zstd.git", branch: "dev"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1a1b9a78b09d90da95c31dbdc3d4a62ef57f2c1d875d15a62342fc1556bfba4d"
-    sha256 cellar: :any, big_sur:       "eeb8825bb703294879b70a5018efecb08a42863cb56c870fdd7215d886732778"
-    sha256 cellar: :any, catalina:      "9fff3447bc3ca1d239dbb239b37bd70fabd37087bf61c85407bad8d5c47a831a"
-    sha256 cellar: :any, mojave:        "0da410f0a383f7068b51b0e72969496a631b8aeb46b2cbaf431989e9a8646018"
+    sha256 cellar: :any,                 arm64_big_sur: "1a1b9a78b09d90da95c31dbdc3d4a62ef57f2c1d875d15a62342fc1556bfba4d"
+    sha256 cellar: :any,                 big_sur:       "eeb8825bb703294879b70a5018efecb08a42863cb56c870fdd7215d886732778"
+    sha256 cellar: :any,                 catalina:      "9fff3447bc3ca1d239dbb239b37bd70fabd37087bf61c85407bad8d5c47a831a"
+    sha256 cellar: :any,                 mojave:        "0da410f0a383f7068b51b0e72969496a631b8aeb46b2cbaf431989e9a8646018"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6e1e6bbe270f2d462866bf42750538766b8b73bc2050c051d4433f9764698e2"
   end
 
   depends_on "cmake" => :build
