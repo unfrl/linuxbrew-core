74dae15cbb23a4d28595b1aaa71a2c732a55901e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-24 08:00:36 +0000

    libspng: update 0.6.3 bottle.

--- Formula/libspng.rb
+++ Formula/libspng.rb
@@ -6,10 +6,11 @@ class Libspng < Formula
   license "BSD-2-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "6d41eb3e6c1938743b2214d4a18c7bb13a4908582380cf7583e7cfd74ab0f038"
-    sha256 cellar: :any, big_sur:       "8cba8c9454c2d9892b39cd4b4dff9e32974f82ea5c220dc1e0aa90eb19406711"
-    sha256 cellar: :any, catalina:      "9b5f9ce7f8e56210f9cce38d641517f5bf43dc5d9f45b75c6be9bc5ad76c1f1f"
-    sha256 cellar: :any, mojave:        "8c2f036cc0b40a540d0aab26486f8f66b03b075fa78c7b39e912321115007bfd"
+    sha256 cellar: :any,                 arm64_big_sur: "6d41eb3e6c1938743b2214d4a18c7bb13a4908582380cf7583e7cfd74ab0f038"
+    sha256 cellar: :any,                 big_sur:       "8cba8c9454c2d9892b39cd4b4dff9e32974f82ea5c220dc1e0aa90eb19406711"
+    sha256 cellar: :any,                 catalina:      "9b5f9ce7f8e56210f9cce38d641517f5bf43dc5d9f45b75c6be9bc5ad76c1f1f"
+    sha256 cellar: :any,                 mojave:        "8c2f036cc0b40a540d0aab26486f8f66b03b075fa78c7b39e912321115007bfd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a18861271fe4cc30bad7965701dcb37e8ea03897eeb188e34718a5357b7c60f1"
   end
 
   depends_on "meson" => :build
