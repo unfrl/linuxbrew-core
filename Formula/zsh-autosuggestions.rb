0b6354ebeeb462ce4ef44e300052b14d1df7bc24
Dawid Dziurla <dawidd0811@gmail.com>
2021-04-25 16:39:22 +0200

    zsh-autosuggestions: needs zsh for test
    Closes #23113.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/zsh-autosuggestions.rb
+++ Formula/zsh-autosuggestions.rb
@@ -9,6 +9,8 @@ class ZshAutosuggestions < Formula
     sha256 cellar: :any_skip_relocation, all: "6f79c9ded23bb32288fc81bcb969ee71b2c6cdba4f88c2c999db6eaf3286bb8c"
   end
 
+  uses_from_macos "zsh" => :test
+
   def install
     pkgshare.install "zsh-autosuggestions.zsh"
   end
