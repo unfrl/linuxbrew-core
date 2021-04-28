14351abcfbbe601a09d9026edfa6a24adddbb3d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-28 05:48:45 +0000

    benthos: update 3.45.1 bottle.

--- Formula/benthos.rb
+++ Formula/benthos.rb
@@ -10,6 +10,7 @@ class Benthos < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "8520475c98be156fde31698a4b5e9aa5c94233b6e7f252aad41ed8ca70b7ab43"
     sha256 cellar: :any_skip_relocation, catalina:      "628ad1f62d4fb14649e22d4c7861b5960ccb417cb0b2465763a37ee5f4a7ece3"
     sha256 cellar: :any_skip_relocation, mojave:        "a4a85273fd690de32ba1e9a0d03edc30f5d98b946fe2f0c84eb73db513a6ef3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6117e2785169a1829118cf3be025cdb68cfa571c0720a5c3c6a7a5957f234e28"
   end
 
   depends_on "go" => :build
