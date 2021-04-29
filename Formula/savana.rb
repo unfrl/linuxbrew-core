9ec456cbfda342da9e7e4e4a1924e0f48c35b8ab
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-29 21:52:08 +0000

    savana: add 1.2 bottle.

--- Formula/savana.rb
+++ Formula/savana.rb
@@ -5,6 +5,10 @@ class Savana < Formula
   sha256 "608242a0399be44f41ff324d40e82104b3c62908bc35177f433dcfc5b0c9bf55"
   license "LGPL-3.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "50482ff8d1af023051b548cf6bf070342ef2e6a3285c8415f50551088fd3b090"
+  end
+
   depends_on "openjdk"
 
   def install
