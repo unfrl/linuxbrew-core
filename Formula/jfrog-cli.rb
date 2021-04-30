88129b2144d4f87ee75cf480f3d8adbb19dfd594
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 11:22:30 +0000

    jfrog-cli: update 1.47.0 bottle.

--- Formula/jfrog-cli.rb
+++ Formula/jfrog-cli.rb
@@ -10,6 +10,7 @@ class JfrogCli < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c5b64391aa80365dd8f0e1391d5ee3662f3350c5505d7c9d21b8f043e7f80690"
     sha256 cellar: :any_skip_relocation, catalina:      "f3f345ea6bef9958d6124a16fbbade8dcc08eaa25560736a9ab364c8a7c34013"
     sha256 cellar: :any_skip_relocation, mojave:        "2300a47217317b451baf7b7a7beeb3affeaf5e89b46f3490cbfac3cbf0058797"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25a8deb3110ccd3bea4a8024b909f38312cc290eb697f7eceae1e6b53a34acd3"
   end
 
   depends_on "go" => :build
