a7f228c4d7d7e223ac31d98f9e6f82e39572a8cb
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-30 20:02:17 +0000

    tssh: update 2.1.0 bottle.

--- Formula/tssh.rb
+++ Formula/tssh.rb
@@ -10,6 +10,7 @@ class Tssh < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "81ba6ad8ced1af17b7cc50c53666a1789fd45ac522c700bed8ce74b60ce156da"
     sha256 cellar: :any_skip_relocation, catalina:      "25c95255b2c535a584697d87a14a1e8c70930d78f21cb056e2697bf79232f400"
     sha256 cellar: :any_skip_relocation, mojave:        "d4b7738f0b5cc63bfcbca2594bbd1514843f290649aea95880b223f71ba7ab9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "964c62fbdeec1e301f0d5dbafbcbd9dddf62c57a53f79f15965e4e8acf8608dd"
   end
 
   depends_on "go" => :build
