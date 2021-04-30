72dfb8710eba918f15be31b534fa1a791b246231
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-30 11:28:26 +0000

    format-udf: add 1.8.0 bottle.

--- Formula/format-udf.rb
+++ Formula/format-udf.rb
@@ -5,6 +5,10 @@ class FormatUdf < Formula
   sha256 "52854097db9044d729fbd7cff012f4b554df01c15225ee17ec159c71da174c8d"
   license "GPL-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "777bfcdbf07b70e2378c1ee2300729b7293acf88862fb450e38f7b4dc3ca1b85"
+  end
+
   def install
     bin.install "format-udf.sh" => "format-udf"
   end
