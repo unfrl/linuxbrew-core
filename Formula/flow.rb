f392ecedd269340bbce7f4f8dcb8e78b19dda5de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-04-30 20:46:48 +0000

    flow: update 0.150.0 bottle.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -7,9 +7,10 @@ class Flow < Formula
   head "https://github.com/facebook/flow.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "119f00bcc233dd70c2ca8c05c85d86f0c0efce921b6a459174ce57e53d1c28b7"
-    sha256 cellar: :any_skip_relocation, catalina: "f5e482fd49d0eecada8cc9496a157057d66aed68e1f187c906ad16ef3296d8f5"
-    sha256 cellar: :any_skip_relocation, mojave:   "26d9ac15e260e8b2a6caf55d5d7894cd3149f14879c0c06715dc1c5938ebe09e"
+    sha256 cellar: :any_skip_relocation, big_sur:      "119f00bcc233dd70c2ca8c05c85d86f0c0efce921b6a459174ce57e53d1c28b7"
+    sha256 cellar: :any_skip_relocation, catalina:     "f5e482fd49d0eecada8cc9496a157057d66aed68e1f187c906ad16ef3296d8f5"
+    sha256 cellar: :any_skip_relocation, mojave:       "26d9ac15e260e8b2a6caf55d5d7894cd3149f14879c0c06715dc1c5938ebe09e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ea761bc2b87ed1740cddee73445524376d579d1ac55f2f4a805a587b923e9d54"
   end
 
   depends_on "ocaml" => :build
