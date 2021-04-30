ba321be5924f5a3621bc3ae58de3454abb77296f
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-04-30 11:28:45 +0000

    ssh-copy-id: add 8.6p1 bottle.

--- Formula/ssh-copy-id.rb
+++ Formula/ssh-copy-id.rb
@@ -8,6 +8,10 @@ class SshCopyId < Formula
   license "SSH-OpenSSH"
   head "https://github.com/openssh/openssh-portable.git"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f7eabbbc20c02c577d18d2524f935dfdcb8c77916f926842f0cfa8d6640ee912"
+  end
+
   keg_only :provided_by_macos
 
   def install
