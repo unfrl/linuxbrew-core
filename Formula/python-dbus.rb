c1a5d01b4fea0460c4d24fd81dee122ae0a8a990
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-02 12:18:08 +0000

    python-dbus: add 1.2.14_1 bottle.

--- Formula/python-dbus.rb
+++ Formula/python-dbus.rb
@@ -5,6 +5,10 @@ class PythonDbus < Formula
   sha256 "b10206ba3dd641e4e46411ab91471c88e0eec1749860e4285193ee68df84ac31"
   revision 1
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "248dae77030f91f860eaf269bf7d1705a682a7717d84b4dc1a035e1be5d219e8"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "dbus"
   depends_on "dbus-glib"
