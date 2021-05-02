7482aa979f727699e93411e9a0c55f7ca96612e8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-02 09:01:30 +0000

    pnpm: update 6.2.4 bottle.

--- Formula/pnpm.rb
+++ Formula/pnpm.rb
@@ -12,6 +12,7 @@ class Pnpm < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "31d5e21c1089c59f1e49e28badeceab94697e3a012bd09c85e033b6fd23f294c"
     sha256 cellar: :any_skip_relocation, catalina:      "31d5e21c1089c59f1e49e28badeceab94697e3a012bd09c85e033b6fd23f294c"
     sha256 cellar: :any_skip_relocation, mojave:        "31d5e21c1089c59f1e49e28badeceab94697e3a012bd09c85e033b6fd23f294c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5338ee144d903ad77ee3eba63cd8449783ab897e8a9873e3e4b574a784f1f3b8"
   end
 
   depends_on "node"
