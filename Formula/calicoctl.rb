b7d340bdff4b4676538749d534844d80d326eef9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 21:20:10 +0000

    calicoctl: update 3.18.3 bottle.

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -12,6 +12,7 @@ class Calicoctl < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "e89e80c43a6f743530c42bb6b5e371bb449f64dda3bc1671f01f464905be9c18"
     sha256 cellar: :any_skip_relocation, catalina:      "2f76e3802048775984e9cdd7db889130d158a983d6b52543f22ee58de6b7ea34"
     sha256 cellar: :any_skip_relocation, mojave:        "53465951c381d8fadcf36894e78dbd92f300785f6562b98bbf519efabcd47aae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b240ae82e43ec977ec79c690a9d600ee23b505a158438159146eae2166012bef"
   end
 
   depends_on "go" => :build
