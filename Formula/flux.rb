7fc6de59d99c101dfd24bf76743930e9d8a49ffd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-04 06:12:53 +0000

    flux: update 0.114.0 bottle.

--- Formula/flux.rb
+++ Formula/flux.rb
@@ -17,6 +17,7 @@ class Flux < Formula
     sha256 cellar: :any,                 big_sur:       "ca9188977d8826d13bac9bafea43c5391b1eacfa587455e05dafac9682a7d088"
     sha256 cellar: :any,                 catalina:      "2b2389f62ee0a653daadb4ad9c22aae1ec4f88022adbf5f5825b2a59756f6d48"
     sha256 cellar: :any,                 mojave:        "5322e65485fe326186e79989d319a8c6d85fc2a595613172298e650729643c60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d7fe6d1a2830a37ec4fed00b91da837491b7848cb7dd8ab93e469022b77f33e7"
   end
 
   depends_on "go" => :build
