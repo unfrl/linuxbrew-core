4711da8ced3b6fef1b41d64dcd78df76bcf73f3f
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-04 14:17:12 +0000

    boot-clj: add 2.8.3_2 bottle.

--- Formula/boot-clj.rb
+++ Formula/boot-clj.rb
@@ -6,6 +6,10 @@ class BootClj < Formula
   license "EPL-1.0"
   revision 2
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1ff2ccd4a0e9b1f97acc2e070cd1c13f3a31ee7c2a557126f7fb64b63df2cf6a"
+  end
+
   depends_on "openjdk"
 
   def install
