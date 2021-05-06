79d300c9174aa9f8fa54869398bd95c602db855e
danielnachun <daniel.nachun@gmail.com>
2021-05-06 01:33:11 -0700

    wayland-protocols: rebottle
    Closes #23188.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/wayland-protocols.rb
+++ Formula/wayland-protocols.rb
@@ -5,10 +5,6 @@ class WaylandProtocols < Formula
   sha256 "b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "64e9fc4ff59725f13c60644b9157d099d283eaedc35a451f2b5d6814a3cc9b3c"
-  end
-
   depends_on "pkg-config" => [:build, :test]
   depends_on "wayland" => :build
   depends_on :linux
