a534f1ac49f60492ae088f9d2abdb1dd449fe00f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-06 05:53:54 +0000

    dante: update 1.4.3 bottle.

--- Formula/dante.rb
+++ Formula/dante.rb
@@ -5,10 +5,11 @@ class Dante < Formula
   sha256 "418a065fe1a4b8ace8fbf77c2da269a98f376e7115902e76cda7e741e4846a5d"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7b25a50f17292cdad4dd0e52de401117411fc6bb660c66bedbdbc8c7759dea9a"
-    sha256 cellar: :any, big_sur:       "098dc6c46d4ee77860f8fefcd44bc21533bf70423add42de899910757796d410"
-    sha256 cellar: :any, catalina:      "4b33f0996ade01cae7bc72f40cf7c8011f86133755e782cc40a15a0d610560c1"
-    sha256 cellar: :any, mojave:        "f6348c63fff9dbf5392ccb1b769e9643e248e00913aba9bcb24dc928f153b526"
+    sha256 cellar: :any,                 arm64_big_sur: "7b25a50f17292cdad4dd0e52de401117411fc6bb660c66bedbdbc8c7759dea9a"
+    sha256 cellar: :any,                 big_sur:       "098dc6c46d4ee77860f8fefcd44bc21533bf70423add42de899910757796d410"
+    sha256 cellar: :any,                 catalina:      "4b33f0996ade01cae7bc72f40cf7c8011f86133755e782cc40a15a0d610560c1"
+    sha256 cellar: :any,                 mojave:        "f6348c63fff9dbf5392ccb1b769e9643e248e00913aba9bcb24dc928f153b526"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dbf8ee6ceaac44eeee0a090b9f61aaa8821fdd3d1af3fc8ca63cc029abfe4df4"
   end
 
   def install
