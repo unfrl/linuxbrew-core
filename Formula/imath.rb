cb57878bcd613545e00b938887fbbca992527205
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-09 07:20:41 +0000

    imath: update 3.0.1 bottle.

--- Formula/imath.rb
+++ Formula/imath.rb
@@ -6,10 +6,11 @@ class Imath < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 arm64_big_sur: "f69d88d25f012ccdd25c4a7036ebdc5deb2172d2d462cca5795a705d2f0b8a1e"
-    sha256 big_sur:       "d821e18ce9ea2dccf94a43d7facd5a2fd7e3ab327e066ee02d21676d2931262e"
-    sha256 catalina:      "567b606d357b6b959d3ce32e5abe3abcd9b1d43494d7c11a7f48726657a47fb1"
-    sha256 mojave:        "fd2aa308bbed5163af6fc50dbb9c3af3f9a5f3aaff0b29b1e71c4019c661ea87"
+    sha256                               arm64_big_sur: "f69d88d25f012ccdd25c4a7036ebdc5deb2172d2d462cca5795a705d2f0b8a1e"
+    sha256                               big_sur:       "d821e18ce9ea2dccf94a43d7facd5a2fd7e3ab327e066ee02d21676d2931262e"
+    sha256                               catalina:      "567b606d357b6b959d3ce32e5abe3abcd9b1d43494d7c11a7f48726657a47fb1"
+    sha256                               mojave:        "fd2aa308bbed5163af6fc50dbb9c3af3f9a5f3aaff0b29b1e71c4019c661ea87"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6af256aa74c3fe80a741e3472070242361047453248e91091859e0a314619e70"
   end
 
   depends_on "cmake" => :build
