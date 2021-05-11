dff04e6232275bc0f4735defb882a52a11d9f392
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-11 08:10:22 +0000

    schema-evolution-manager: add 0.9.45 bottle.

--- Formula/schema-evolution-manager.rb
+++ Formula/schema-evolution-manager.rb
@@ -5,6 +5,11 @@ class SchemaEvolutionManager < Formula
   sha256 "268a7a09a2175ed081593843fd03f330afb42710b2af0c9bc456b5e8546057c2"
   license "Apache-2.0"
 
+  bottle do
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "09384d7652e7a93fb3ffd96f85aadefcec47a0823672700041d96346a07ea046"
+  end
+
   uses_from_macos "ruby"
 
   def install
