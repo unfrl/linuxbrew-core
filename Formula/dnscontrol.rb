30d96a514abb814ceb5da163aa3dc500f8fe7dde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-17 07:49:55 +0000

    dnscontrol: update 3.9.0 bottle.

--- Formula/dnscontrol.rb
+++ Formula/dnscontrol.rb
@@ -16,6 +16,7 @@ class Dnscontrol < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "dc113a15d6cb60307fc1e5c181b47cccbcf664d26e90bf3d9c4d2d4d616575a8"
     sha256 cellar: :any_skip_relocation, catalina:      "7292ecff37f7ad67a091a9c6e2ca58eebd212e44ae73261b803f0f77b6f60f30"
     sha256 cellar: :any_skip_relocation, mojave:        "13c1fb3a2ff2df3fd8a24e14e06430b9d8c1c27502c88b5ef9ceac0d9dbbeba9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d90928a60e1d01c8a7f52b62a9145480d0108ff49612ff4faf6ccb6ba20e7abb"
   end
 
   depends_on "go" => :build
