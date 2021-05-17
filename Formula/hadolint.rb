c7d034a1eea6a163e5ff81e1ccc46b4469827cf8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-17 19:50:34 +0000

    hadolint: update 2.4.1 bottle.

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -6,9 +6,10 @@ class Hadolint < Formula
   license "GPL-3.0-only"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "e139f730ae1c5508ee710cbcf0f6453f0c26e8b40beefe910999cc97157fcd0b"
-    sha256 cellar: :any_skip_relocation, catalina: "f4b116bbfbc9cb73c045b5369fa2ec92085a025f1065d3c4c0a9ab27e96ce52f"
-    sha256 cellar: :any_skip_relocation, mojave:   "e13abd67b5d34f4bd372a3f6c5ddf6ba8c0fed41a5b00e4a36b30b755c396e66"
+    sha256 cellar: :any_skip_relocation, big_sur:      "e139f730ae1c5508ee710cbcf0f6453f0c26e8b40beefe910999cc97157fcd0b"
+    sha256 cellar: :any_skip_relocation, catalina:     "f4b116bbfbc9cb73c045b5369fa2ec92085a025f1065d3c4c0a9ab27e96ce52f"
+    sha256 cellar: :any_skip_relocation, mojave:       "e13abd67b5d34f4bd372a3f6c5ddf6ba8c0fed41a5b00e4a36b30b755c396e66"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "559747e68f2f581ae95a0b2eef1bdcf163875c92135ede174ae6a3be03252a96"
   end
 
   depends_on "ghc" => :build
