3dc029a2cafb4395f09d6689f93a41a417150540
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-18 20:47:57 +0000

    shogun: update 6.1.4_11 bottle.

--- Formula/shogun.rb
+++ Formula/shogun.rb
@@ -8,10 +8,11 @@ class Shogun < Formula
   revision 11
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "4e39c797d38f15383d76c312d84313ce3757b33d6cb42393de432db7a9ee6391"
-    sha256 cellar: :any, big_sur:       "bfefc3fb2694eb343be5fcb54b6b922ee462f6c3f4cbe38bf7392600db03ba5d"
-    sha256 cellar: :any, catalina:      "88ac63fbb529bf14938d9b5032aa58a6718f27a5953a47ec4498404953c982a9"
-    sha256 cellar: :any, mojave:        "589e96815f03a551445e9817da9781e5f4613a80411cb3d42085aaa5fd491be4"
+    sha256 cellar: :any,                 arm64_big_sur: "4e39c797d38f15383d76c312d84313ce3757b33d6cb42393de432db7a9ee6391"
+    sha256 cellar: :any,                 big_sur:       "bfefc3fb2694eb343be5fcb54b6b922ee462f6c3f4cbe38bf7392600db03ba5d"
+    sha256 cellar: :any,                 catalina:      "88ac63fbb529bf14938d9b5032aa58a6718f27a5953a47ec4498404953c982a9"
+    sha256 cellar: :any,                 mojave:        "589e96815f03a551445e9817da9781e5f4613a80411cb3d42085aaa5fd491be4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e96c890c90c2eab21593920d4a62fca59d449a283555b41358f521846ab685f7"
   end
 
   depends_on "cmake" => :build
