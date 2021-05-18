e77f6df64b00ce6e18f020e6b6a56e9652df7884
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-18 20:23:28 +0000

    protobuf-c: update 1.3.3_5 bottle.

--- Formula/protobuf-c.rb
+++ Formula/protobuf-c.rb
@@ -7,10 +7,11 @@ class ProtobufC < Formula
   revision 5
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "df70c1823bcc700c5cb2d774b7449d2abd5f7b47328038204f9d62077d107b13"
-    sha256 cellar: :any, big_sur:       "c2ce7d930cf3d8c82d88623cb10e6d9e0b0827dd430ce44edc63a9aabdbb47e3"
-    sha256 cellar: :any, catalina:      "a4568baed3b991990221ee0c90183e5e09911f7b4ad45c79570409b5f659b739"
-    sha256 cellar: :any, mojave:        "f6b3ee99315a6ff4d418d208afd6722fa271c72c6f7ad52d6535c5666640a3fc"
+    sha256 cellar: :any,                 arm64_big_sur: "df70c1823bcc700c5cb2d774b7449d2abd5f7b47328038204f9d62077d107b13"
+    sha256 cellar: :any,                 big_sur:       "c2ce7d930cf3d8c82d88623cb10e6d9e0b0827dd430ce44edc63a9aabdbb47e3"
+    sha256 cellar: :any,                 catalina:      "a4568baed3b991990221ee0c90183e5e09911f7b4ad45c79570409b5f659b739"
+    sha256 cellar: :any,                 mojave:        "f6b3ee99315a6ff4d418d208afd6722fa271c72c6f7ad52d6535c5666640a3fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f0e1df718070986c608cee27da4bbe20e1727c099b6ddfed54e2e148f761f28"
   end
 
   depends_on "pkg-config" => :build
