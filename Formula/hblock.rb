d8009a935badbf31ed40dbdbe9759c0c20690517
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-19 11:08:17 +0000

    hblock: add 3.2.2 bottle.

--- Formula/hblock.rb
+++ Formula/hblock.rb
@@ -5,6 +5,10 @@ class Hblock < Formula
   sha256 "151e5ce32403f65892b0a15df04310ee4d115e12da9d38a7289f13f20e753d92"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "51f4bbda8d5d2ebbb7c0f0a77d8c2c04c7ec9640046b07f67c573e74c1e49861"
+  end
+
   uses_from_macos "curl"
 
   def install
