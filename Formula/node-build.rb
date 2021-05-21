0ced00c5ab1e6650e037c6bc43fc75f21dda1713
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-21 05:46:48 +0000

    node-build: add 4.9.41 bottle.

--- Formula/node-build.rb
+++ Formula/node-build.rb
@@ -11,6 +11,10 @@ class NodeBuild < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "07b00b9207d80dd7cbca2e9374419c465c0c3926430af11661dd74b6a0eff8a6"
+  end
+
   depends_on "autoconf"
   depends_on "openssl@1.1"
   depends_on "pkg-config"
