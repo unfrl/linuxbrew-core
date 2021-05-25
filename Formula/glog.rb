44990197f60cacaea8d14bad01fb6ea8d355c988
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-25 13:21:25 +0000

    glog: update 0.5.0 bottle.

--- Formula/glog.rb
+++ Formula/glog.rb
@@ -7,10 +7,11 @@ class Glog < Formula
   head "https://github.com/google/glog.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "7cc04172531a192d8783f7d9d7fbaf48b8ab8849c896b089a371bd993726c30c"
-    sha256 cellar: :any, big_sur:       "cf69cacfd059791bf7c9196dfd38a170464cda435257eabe5a80428dbcd191be"
-    sha256 cellar: :any, catalina:      "934918ceea54e2afe84fe04b179cc86fd210e804ed57d3700d2ecd0bcfc784c3"
-    sha256 cellar: :any, mojave:        "8d66cd6d6e718969aa23d9c1fbf442fdb0da2824d40259aae5905074e838507d"
+    sha256 cellar: :any,                 arm64_big_sur: "7cc04172531a192d8783f7d9d7fbaf48b8ab8849c896b089a371bd993726c30c"
+    sha256 cellar: :any,                 big_sur:       "cf69cacfd059791bf7c9196dfd38a170464cda435257eabe5a80428dbcd191be"
+    sha256 cellar: :any,                 catalina:      "934918ceea54e2afe84fe04b179cc86fd210e804ed57d3700d2ecd0bcfc784c3"
+    sha256 cellar: :any,                 mojave:        "8d66cd6d6e718969aa23d9c1fbf442fdb0da2824d40259aae5905074e838507d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6897a97584caaabc71ce6fd78b5dd8c1fa031e553720d13a2d280296cd30c63f"
   end
 
   depends_on "cmake" => :build
