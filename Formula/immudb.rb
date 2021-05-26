bb23c3f7c5ad5513fb0e34129c89bc216165e17a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-26 06:30:30 +0000

    immudb: update 1.0.0 bottle.

--- Formula/immudb.rb
+++ Formula/immudb.rb
@@ -15,6 +15,7 @@ class Immudb < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "a7cb5382bf0bbd23c3c406201d6ff46a6b917233312f0f1851ebed4a3d8efb90"
     sha256 cellar: :any_skip_relocation, catalina:      "2fe1d5127c7eefd97980c9700ecfe2d0b8fa3a2fa5030e39c889314c9665bf45"
     sha256 cellar: :any_skip_relocation, mojave:        "760bc5ebf53840b82798ba3dfe8566a95edb38ad7bf0c21ab7c48e4082d16d46"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5b6801ec445913a0a77eb4e854e3f1e4aede6b8185c2307f89c0cebbc27008a0"
   end
 
   depends_on "go" => :build
