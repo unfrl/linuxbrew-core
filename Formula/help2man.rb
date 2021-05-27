475e78c39087dc9b5fb23abe3e5d8cd8d53aa726
Michka Popoff <michkapopoff@gmail.com>
2021-05-27 09:44:57 +0200

    help2man: revision bump for perl
    Closes #23334.
    
    Signed-off-by: Michka Popoff <3406519+iMichka@users.noreply.github.com>

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -5,14 +5,13 @@ class Help2man < Formula
   mirror "https://ftpmirror.gnu.org/help2man/help2man-1.48.3.tar.xz"
   sha256 "8361ff3c643fbd391064e97e5f54592ca28b880eaffbf566a68e0ad800d1a8ac"
   license "GPL-3.0-or-later"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "8508861b550d6e89b39702beae40a4c3ff78c9c0d248ea642516edb5f74f0f09"
     sha256 cellar: :any,                 big_sur:       "daa1e20f7225b1b4e64b2b022712a767d238a1dbe07dda53c63dfe1f0f0f8275"
     sha256 cellar: :any,                 catalina:      "337cedb9b9f97c1a1f4e08f3ff79142501d85da4f997c1859cd1dcb05653a836"
     sha256 cellar: :any,                 mojave:        "7d2a720ba3e5020ae28f32cc5705ffd9c8ec7b8b52aed3272d7d43e82eb91c3c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d912a3caa52a5eb6352139c32529cdfc21ea240ae17d1470e395148c3628cccc"
   end
 
   depends_on "gettext" if Hardware::CPU.intel?
