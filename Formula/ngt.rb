5ceeba36096fe2ac622048b1e4ef8238693103eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-27 10:57:41 +0000

    ngt: update 1.13.5 bottle.

--- Formula/ngt.rb
+++ Formula/ngt.rb
@@ -12,10 +12,11 @@ class Ngt < Formula
 
   bottle do
     rebuild 1
-    sha256 cellar: :any, arm64_big_sur: "a8afc60cb99ba0d670e544ffd24fda92f2154bb7075b8ed2b994de17ca5a5293"
-    sha256 cellar: :any, big_sur:       "97c08cfd1f04afd27b09bde575124c5a60b19e6057ccb07f26ea3f4f4f85f3a6"
-    sha256 cellar: :any, catalina:      "6d4de1454b7aaad5c8eb14420039fcc974a82fcd970f1bb92e07ad90b26ecef1"
-    sha256 cellar: :any, mojave:        "96650e0eea4d200761d8fc239c47fa04a6f20b6237896fd2fa014ddcb16fb5f6"
+    sha256 cellar: :any,                 arm64_big_sur: "a8afc60cb99ba0d670e544ffd24fda92f2154bb7075b8ed2b994de17ca5a5293"
+    sha256 cellar: :any,                 big_sur:       "97c08cfd1f04afd27b09bde575124c5a60b19e6057ccb07f26ea3f4f4f85f3a6"
+    sha256 cellar: :any,                 catalina:      "6d4de1454b7aaad5c8eb14420039fcc974a82fcd970f1bb92e07ad90b26ecef1"
+    sha256 cellar: :any,                 mojave:        "96650e0eea4d200761d8fc239c47fa04a6f20b6237896fd2fa014ddcb16fb5f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c99bee889e640d10078f65bfbf32fb93173e334472c3ff83d7dc4e5b09892c1c"
   end
 
   depends_on "cmake" => :build
