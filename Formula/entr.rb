7cd50675285eee4acc2dc03880431e821b7845a7
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-05-28 10:29:45 +0000

    entr: update 4.9_1 bottle.

--- Formula/entr.rb
+++ Formula/entr.rb
@@ -17,6 +17,7 @@ class Entr < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "cbf5fc86b826f3dc20159380700fe9998db67e70677ea419462faf2757346caa"
     sha256 cellar: :any_skip_relocation, catalina:      "4c073f34400c6d631eac1f8636779f8fb3f36b7639817201c8258ba2ba651ceb"
     sha256 cellar: :any_skip_relocation, mojave:        "4f8f5b3b27a067c9e0220cc3f5f221b9350dcf64c668043d11a7bcd0468765bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81e61224bd7ec68279a682b70e81d2d5d6821f12777155fc62b2f3550043a61f"
   end
 
   # Fix v4.9 segfault on the Mac: https://github.com/eradman/entr/issues/74
