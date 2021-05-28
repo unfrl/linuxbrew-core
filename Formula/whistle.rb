e5997c47ef65ca52f04a5a197fce324637f688f2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-05-28 20:44:05 +0000

    whistle: add 2.7.2 bottle.

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -7,6 +7,10 @@ class Whistle < Formula
   sha256 "fe02c63472198252187bd9504d7d42d56a7c31ad90210152d325ed20cc94cbe0"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "177ef66e37b0aa265493c59b8af8ddf27d432574628217196a359c4886f776cd"
+  end
+
   depends_on "node"
 
   def install
