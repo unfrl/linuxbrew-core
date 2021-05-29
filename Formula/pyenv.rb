43b83c2a0b34c5020260123a366388362e5e8a3e
Michka Popoff <michkapopoff@gmail.com>
2021-05-29 15:58:20 +0200

    pyenv: make python a test dependency (#23372)

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -24,7 +24,7 @@ class Pyenv < Formula
   depends_on "openssl@1.1"
   depends_on "pkg-config"
   depends_on "readline"
-  depends_on "python@3.9" unless OS.mac?
+  depends_on "python@3.9" => :test unless OS.mac?
 
   uses_from_macos "bzip2"
   uses_from_macos "libffi"
