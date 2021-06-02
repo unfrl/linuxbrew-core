0291fdd91105fd9b2e0e838a048559d1d6d7c226
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-02 21:43:46 +0000

    imath: update 3.0.4 bottle.

--- Formula/imath.rb
+++ Formula/imath.rb
@@ -6,10 +6,11 @@ class Imath < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "d566baed11c63ab4a459fae705f97886ad32a3e568b54c323dd56ba27aef258a"
-    sha256 cellar: :any, big_sur:       "b959374888c1cc36b4133da024076bffa52ffdf9c9832ce16abf5201fd0ee568"
-    sha256 cellar: :any, catalina:      "aec5a5567cfa9df3ae811b55d7fd987d4cdee8e6c4cf13c42e2858dad1815242"
-    sha256 cellar: :any, mojave:        "b0364f416a66f09f4908ec029a5c1ef2bb1f3bbb0a2c463bccffe349568756a6"
+    sha256 cellar: :any,                 arm64_big_sur: "d566baed11c63ab4a459fae705f97886ad32a3e568b54c323dd56ba27aef258a"
+    sha256 cellar: :any,                 big_sur:       "b959374888c1cc36b4133da024076bffa52ffdf9c9832ce16abf5201fd0ee568"
+    sha256 cellar: :any,                 catalina:      "aec5a5567cfa9df3ae811b55d7fd987d4cdee8e6c4cf13c42e2858dad1815242"
+    sha256 cellar: :any,                 mojave:        "b0364f416a66f09f4908ec029a5c1ef2bb1f3bbb0a2c463bccffe349568756a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "126c199c16fd985d5e740d531443281f927135f4a8ada12e11c6473bc8f34b0c"
   end
 
   depends_on "cmake" => :build
