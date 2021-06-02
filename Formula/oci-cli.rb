c91db60c50297d3c8f1152d5a1760cd07a1fc9d3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-02 21:50:00 +0000

    oci-cli: update 2.25.1 bottle.

--- Formula/oci-cli.rb
+++ Formula/oci-cli.rb
@@ -9,10 +9,11 @@ class OciCli < Formula
   head "https://github.com/oracle/oci-cli.git"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "045392ecf1704cc5bff464db4a2fae5dbe0ce38e442a8ce9e166a255ca88843a"
-    sha256 cellar: :any, big_sur:       "c741f46776f4407648d2615400013e7a4f8d7b816215e64d5cedb8f570972b45"
-    sha256 cellar: :any, catalina:      "99f73e414c3cdb1cd280103cd40fed1d4b016868adb7c53efd69e9b2f4845085"
-    sha256 cellar: :any, mojave:        "e4ce28db371c3e7a39e367924ed3d7c2d96342061511559a5373425195b883e2"
+    sha256 cellar: :any,                 arm64_big_sur: "045392ecf1704cc5bff464db4a2fae5dbe0ce38e442a8ce9e166a255ca88843a"
+    sha256 cellar: :any,                 big_sur:       "c741f46776f4407648d2615400013e7a4f8d7b816215e64d5cedb8f570972b45"
+    sha256 cellar: :any,                 catalina:      "99f73e414c3cdb1cd280103cd40fed1d4b016868adb7c53efd69e9b2f4845085"
+    sha256 cellar: :any,                 mojave:        "e4ce28db371c3e7a39e367924ed3d7c2d96342061511559a5373425195b883e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b31f83b5c7cd4af2652ec62ec78ef815f896d09474bff91d27075dcf1e5461ff"
   end
 
   depends_on "python@3.9"
