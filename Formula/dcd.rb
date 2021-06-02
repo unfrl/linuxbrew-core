c706b19e823a44634c2be850044e9d49fed7121b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-02 21:44:21 +0000

    dcd: update 0.13.3 bottle.

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -8,9 +8,10 @@ class Dcd < Formula
   head "https://github.com/dlang-community/dcd.git"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "20696fce98533b2c22ba295ef173610957ab942dc60e6afa4041ca0f62b89f98"
-    sha256 cellar: :any_skip_relocation, catalina: "ac2ad349f223a80f91b37565b4ada4ced8dd27f4dab2bd9a3dfc8461ad359dee"
-    sha256 cellar: :any_skip_relocation, mojave:   "01ed6652319b7aa33b4a34201be816489bab1f3ebca7581727db4921514f6bef"
+    sha256 cellar: :any_skip_relocation, big_sur:      "20696fce98533b2c22ba295ef173610957ab942dc60e6afa4041ca0f62b89f98"
+    sha256 cellar: :any_skip_relocation, catalina:     "ac2ad349f223a80f91b37565b4ada4ced8dd27f4dab2bd9a3dfc8461ad359dee"
+    sha256 cellar: :any_skip_relocation, mojave:       "01ed6652319b7aa33b4a34201be816489bab1f3ebca7581727db4921514f6bef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5c02a8d4f54df37baafbe1cae77ccedde56329cc6364f69b49d7fadc604b3f87"
   end
 
   depends_on "dmd" => :build
