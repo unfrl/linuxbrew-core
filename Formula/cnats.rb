903df6ba10771bb491c69e8ed4e87bf4c850624b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-02 21:47:08 +0000

    cnats: update 2.5.0 bottle.

--- Formula/cnats.rb
+++ Formula/cnats.rb
@@ -6,10 +6,11 @@ class Cnats < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "1ca08203f353fd313afa34ee69f5f91c980779bc3e8842ec55932c933dbc8ac6"
-    sha256 cellar: :any, big_sur:       "c4b31706b10c63793156ec7174fe88665d1d52a5cf40036b3790b6c5e4c8c36d"
-    sha256 cellar: :any, catalina:      "f47660174a6996c9dc11459dba19b25b6085b2025ec71a2534dad5f839785a82"
-    sha256 cellar: :any, mojave:        "7f240dd1c2df37557eec9cffff09d03b696eab091d8bd15c0328c7ae3b5afe09"
+    sha256 cellar: :any,                 arm64_big_sur: "1ca08203f353fd313afa34ee69f5f91c980779bc3e8842ec55932c933dbc8ac6"
+    sha256 cellar: :any,                 big_sur:       "c4b31706b10c63793156ec7174fe88665d1d52a5cf40036b3790b6c5e4c8c36d"
+    sha256 cellar: :any,                 catalina:      "f47660174a6996c9dc11459dba19b25b6085b2025ec71a2534dad5f839785a82"
+    sha256 cellar: :any,                 mojave:        "7f240dd1c2df37557eec9cffff09d03b696eab091d8bd15c0328c7ae3b5afe09"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5016b6a7d8888c8744bc248471dcf5722113f58ca0334302f181cee592016bcc"
   end
 
   depends_on "cmake" => :build
