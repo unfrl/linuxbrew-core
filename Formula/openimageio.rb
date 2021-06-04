e5d1a41c6c114b06836dc815d9915b9c7d32adb3
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-04 14:01:33 +0000

    openimageio: update 2.2.15.1 bottle.

--- Formula/openimageio.rb
+++ Formula/openimageio.rb
@@ -13,10 +13,11 @@ class Openimageio < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "234df6f7b3eda72c108f81838a5541e7e3a69f04328e502c62a64ac2a93f621d"
-    sha256 cellar: :any, big_sur:       "a0807afc887f880005465c91d0ed6c37801ff316dda04c81a1c1432243b75588"
-    sha256 cellar: :any, catalina:      "62b267f53517973e8c018057eba39c751553fa1839362c8b95aede135b669bd2"
-    sha256 cellar: :any, mojave:        "cdd0daa342392f8de170d30443731a31d40c9c338a8758d80130e3c9c951eb11"
+    sha256 cellar: :any,                 arm64_big_sur: "234df6f7b3eda72c108f81838a5541e7e3a69f04328e502c62a64ac2a93f621d"
+    sha256 cellar: :any,                 big_sur:       "a0807afc887f880005465c91d0ed6c37801ff316dda04c81a1c1432243b75588"
+    sha256 cellar: :any,                 catalina:      "62b267f53517973e8c018057eba39c751553fa1839362c8b95aede135b669bd2"
+    sha256 cellar: :any,                 mojave:        "cdd0daa342392f8de170d30443731a31d40c9c338a8758d80130e3c9c951eb11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2eeced7b83b80b32396077f6f7d2d77d2d578f0526f108bf5427ad6bd153e56c"
   end
 
   depends_on "cmake" => :build
