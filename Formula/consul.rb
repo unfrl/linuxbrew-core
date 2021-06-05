9ffc0ee7f2c5337bf64711c6d6f4eb12c2840830
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-05 09:40:29 +0000

    consul: update 1.9.6 bottle.

--- Formula/consul.rb
+++ Formula/consul.rb
@@ -13,9 +13,10 @@ class Consul < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, big_sur:  "2a396a822a98275144d59a0caf3dccef184484b4acd02684b63db15c2b26a52e"
-    sha256 cellar: :any_skip_relocation, catalina: "ae11c89db309a6ea6a9ad05a918a53498fa5463de720f082bffa1a51e34d3143"
-    sha256 cellar: :any_skip_relocation, mojave:   "d863e04c43a5a89df5b2692ee264e2cbc1836230c50a01bdcb394b1bf7ac6a3f"
+    sha256 cellar: :any_skip_relocation, big_sur:      "2a396a822a98275144d59a0caf3dccef184484b4acd02684b63db15c2b26a52e"
+    sha256 cellar: :any_skip_relocation, catalina:     "ae11c89db309a6ea6a9ad05a918a53498fa5463de720f082bffa1a51e34d3143"
+    sha256 cellar: :any_skip_relocation, mojave:       "d863e04c43a5a89df5b2692ee264e2cbc1836230c50a01bdcb394b1bf7ac6a3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2ec64f3af458436c173573caeed67dbe5f0bc7ddfd1d5f4ea24a109db7a52987"
   end
 
   depends_on "go" => :build
