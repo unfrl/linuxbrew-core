3405ed2ba45ccc6fd57118c0f23b0f661150f8ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-07 06:27:37 +0000

    fastlane: update 2.185.0 bottle.

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -12,10 +12,11 @@ class Fastlane < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "b51316b523d8a04b28708e99df536a21b8ad28c3b470e27208369e3fa4d385e6"
-    sha256 cellar: :any, big_sur:       "dac134a7cb24fd8f96d9d9e155c588682e6bed78d88af25f8cbbc52bb9a95ac8"
-    sha256 cellar: :any, catalina:      "e697f3784c18e7085afb90f53da73fd444ce964478e022e29a683ce92d6a6744"
-    sha256 cellar: :any, mojave:        "da3fa9500aa579067e895b3c62f9772c2fa94b7dc5086e75507c64f979d5090a"
+    sha256 cellar: :any,                 arm64_big_sur: "b51316b523d8a04b28708e99df536a21b8ad28c3b470e27208369e3fa4d385e6"
+    sha256 cellar: :any,                 big_sur:       "dac134a7cb24fd8f96d9d9e155c588682e6bed78d88af25f8cbbc52bb9a95ac8"
+    sha256 cellar: :any,                 catalina:      "e697f3784c18e7085afb90f53da73fd444ce964478e022e29a683ce92d6a6744"
+    sha256 cellar: :any,                 mojave:        "da3fa9500aa579067e895b3c62f9772c2fa94b7dc5086e75507c64f979d5090a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6deb572cee7fe7ef0bf855c62f2358fb9ded5c177412add21f8488c2014dd5e"
   end
 
   depends_on "ruby@2.7"
