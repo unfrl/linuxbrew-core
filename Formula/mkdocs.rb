f01d968a9d4de323000db77dd4a4b95eb2d8e4ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-10 09:21:24 +0000

    mkdocs: update 1.2.1 bottle.

--- Formula/mkdocs.rb
+++ Formula/mkdocs.rb
@@ -12,6 +12,7 @@ class Mkdocs < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "0795b30efd608a959240d65f496125134fd546690d87cf4d393115d7a10b3ecb"
     sha256 cellar: :any_skip_relocation, catalina:      "052d025a4e9640cb9d85727e11faca2843145c4542971fe8a79de98adf0cbb3d"
     sha256 cellar: :any_skip_relocation, mojave:        "a8336940714d9bc75d8de37e11b2ca06b58270325d3ae92e8559a9c3139eaba8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2e57f0f5ef676e4cc81a4500fed79cdb3dea77f730c851d7758b08e5e6e9e38"
   end
 
   depends_on "python@3.9"
