7dc1aa771a1bd55eb7861bae65dda182c28ecef0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-11 05:50:10 +0000

    inframap: update 0.6.6 bottle.

--- Formula/inframap.rb
+++ Formula/inframap.rb
@@ -11,6 +11,7 @@ class Inframap < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "958f736b7e0c02a142ec51bd43291e9bebca1abcfbd4afce02c74432ee1f838a"
     sha256 cellar: :any_skip_relocation, catalina:      "00f393f750d9ba38730b3ec3a3292b1cf6ddecd54ee113bc8102b74dca6bf655"
     sha256 cellar: :any_skip_relocation, mojave:        "636171542156a61576550bb3bf5d47de02d59a8baef3af71a4a86254837b1bd6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "874720ed28b3c93c24e761ad55d4f433cbdb4c2e84b2d7872404fc5d4d9101e4"
   end
 
   depends_on "go" => :build
