74fc82a68b2ec624df2d58cffa02bbb395019281
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-11 12:26:00 +0000

    ctop: update 0.7.6 bottle.

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -11,6 +11,7 @@ class Ctop < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "c654814649f0fdefb159bda66af0926fe0615c6ac185f6234a88642e7add2947"
     sha256 cellar: :any_skip_relocation, catalina:      "517e520dc241ade15a6f493b394efa25e212e779ec2d6f1bfd6b9ecd0f0a9627"
     sha256 cellar: :any_skip_relocation, mojave:        "8186e049abc8b0e5f1253c5f310ffb6884b7defad2a8ed6fe50dceee573e5bcd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24170ce39945a417c46fd2a1719f4d7c7f8064df0b8372b4af94af673a4678e2"
   end
 
   depends_on "go" => :build
