07d06efbf03f15253a4659582aff2c6e2b303e31
Issy Long <me@issyl0.co.uk>
2021-06-13 19:33:47 +0100

    ruby-build: Remove `all` bottle; let CI rebuild `linux` bottle
    - According to
      https://github.com/Homebrew/linuxbrew-core/pull/23439#issuecomment-855604213,
      this is the process for `all` bottles until the cores are merged.
    - This one slipped through in a recent merge
      (https://github.com/Homebrew/linuxbrew-core/issues/23488).
    
    Closes #23514.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/ruby-build.rb
+++ Formula/ruby-build.rb
@@ -6,10 +6,6 @@ class RubyBuild < Formula
   license "MIT"
   head "https://github.com/rbenv/ruby-build.git"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "6ff77bcf5c3f8bf14719f438427e6250f2ec387775ab0d5dd3d7e896c4caaf1d"
-  end
-
   depends_on "autoconf"
   depends_on "pkg-config"
   depends_on "readline"
