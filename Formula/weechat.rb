cfa2a78985b56259cd3b62daa26c838585a231f2
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-14 16:56:32 +0000

    weechat: update 3.2 bottle.

--- Formula/weechat.rb
+++ Formula/weechat.rb
@@ -11,6 +11,7 @@ class Weechat < Formula
     sha256 big_sur:       "0261a4b52e5fd25067fc2d9af3af59090cadd466c36cd69fcf7a635fffcf0bd6"
     sha256 catalina:      "4d939d2a34065cdae47590d50b6b2aa3c7595b79af0957cb7535c7ba25ee0255"
     sha256 mojave:        "d9af6b42a994c3a8ee5632563dd5cc285395d0dcaa908f7b928ce5c1e3e845d7"
+    sha256 x86_64_linux:  "84f4215107e3e47e3afd8b167f62efc05bc7e0ca14b010e89b5ee6f2acac14f6"
   end
 
   depends_on "asciidoctor" => :build
