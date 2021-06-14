430d2f5903e772b3c74d6b378b6d6ca90910407b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-14 20:07:19 +0000

    python-yq: update 2.12.2 bottle.

--- Formula/python-yq.rb
+++ Formula/python-yq.rb
@@ -10,6 +10,7 @@ class PythonYq < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "3598941278045e43a5129d58e81f112ea35f3871a7354c8247bf598f7435d797"
     sha256 cellar: :any_skip_relocation, catalina:      "3598941278045e43a5129d58e81f112ea35f3871a7354c8247bf598f7435d797"
     sha256 cellar: :any_skip_relocation, mojave:        "3598941278045e43a5129d58e81f112ea35f3871a7354c8247bf598f7435d797"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f7369da386d517f765231a0f2c44950821857474f745c5093a640681efc7257"
   end
 
   depends_on "jq"
