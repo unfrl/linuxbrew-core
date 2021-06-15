e194cba42e119640410d1a4fb0d75b4ad35d14b4
Michka Popoff <michkapopoff@gmail.com>
2021-06-15 08:25:47 +0200

    poco: add sha256 back after merge

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -17,6 +17,7 @@ class Poco < Formula
     sha256 cellar: :any, catalina:      "0755dff1346ea80aa6202ce3e8269c608960abd4bf0a4566e56075cc99364b57"
     sha256 cellar: :any, mojave:        "7abccb2c17823c6dda9dee9e5918fa28ef846d8095252681c83c47bbb674f5c8"
     sha256 cellar: :any, high_sierra:   "70cea3a570e187c3e70a8dbbe1ad2e43be1c159d0d9118c1bfc1a8cc6441e2a4"
+    sha256 cellar: :any, x86_64_linux:  "b37b8c0ada07503592acc52c57c24c3ed52df26908b520e4e5284c30453d1101"
   end
 
   depends_on "cmake" => :build
