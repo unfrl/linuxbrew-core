03fa290a29233aa61feca4435d0adcc7dd682926
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-16 05:42:09 +0000

    marked: add 2.1.0 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -7,6 +7,10 @@ class Marked < Formula
   sha256 "febda707750e32c829578ffa35626ec03c32f5b65a106f81c3799d14766790e9"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c4e01092595503908e7a76942ef9ace0e803bbb6a2228814a8ea817209a7bd1f"
+  end
+
   depends_on "node"
 
   def install
