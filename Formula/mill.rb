78f8148e473ccc0d2daf6984e701b49d0ad81388
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-18 08:51:39 +0000

    mill: add 0.9.8 bottle.

--- Formula/mill.rb
+++ Formula/mill.rb
@@ -5,6 +5,10 @@ class Mill < Formula
   sha256 "0246eba6c743d17003f31916c83d74a7fd95898423c95abcc549dbad3546f7b6"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1e823aa9b5e31f4b17b381933f2293c430c476783a25562cf350eec055157a07"
+  end
+
   depends_on "openjdk"
 
   def install
