540b5edb3cbdadc2399535e573c5bc82acc41c91
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-22 20:12:28 +0000

    osinfo-db: add 20210621 bottle.

--- Formula/osinfo-db.rb
+++ Formula/osinfo-db.rb
@@ -10,6 +10,10 @@ class OsinfoDb < Formula
     regex(/href=.*?osinfo-db[._-]v?(\d+(?:\.\d+)*)\.t/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a66b32ea6bd90d92638c64e5a909eae512b04124e247c0e6bad501e3b5e00749"
+  end
+
   depends_on "osinfo-db-tools" => [:build, :test]
 
   def install
