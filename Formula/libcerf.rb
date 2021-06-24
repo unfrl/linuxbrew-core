60aa8bc0a2d175754d180478ed2aea4ad9d7342f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-24 06:28:41 +0000

    libcerf: update 1.16 bottle.

--- Formula/libcerf.rb
+++ Formula/libcerf.rb
@@ -13,10 +13,11 @@ class Libcerf < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "12b0794891c9fb76a5e0ce84d693e9a4f344809211b3b829a36f6dd08eb6f599"
-    sha256 cellar: :any, big_sur:       "76b3640332901e939eb60a0b4d765ffa0007b889b875f12966c3184970916085"
-    sha256 cellar: :any, catalina:      "54ab9a449d9a6309bf92c6feabd7bed33c30173878905135855280deaa429721"
-    sha256 cellar: :any, mojave:        "3f594f4f8692aabda12cdd37fd2d6af5e66ab12635d8e032ec8f181221a4162b"
+    sha256 cellar: :any,                 arm64_big_sur: "12b0794891c9fb76a5e0ce84d693e9a4f344809211b3b829a36f6dd08eb6f599"
+    sha256 cellar: :any,                 big_sur:       "76b3640332901e939eb60a0b4d765ffa0007b889b875f12966c3184970916085"
+    sha256 cellar: :any,                 catalina:      "54ab9a449d9a6309bf92c6feabd7bed33c30173878905135855280deaa429721"
+    sha256 cellar: :any,                 mojave:        "3f594f4f8692aabda12cdd37fd2d6af5e66ab12635d8e032ec8f181221a4162b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25b2badb09c066dd6ac3bfd66209b70c167aaab2189a10eb0fa6d51197aa456d"
   end
 
   depends_on "cmake" => :build
