717524abc88628415426c2b9913272de63f04fd7
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-24 20:53:51 +0000

    erlang@22: update 22.3.4.20_1 bottle.

--- Formula/erlang@22.rb
+++ Formula/erlang@22.rb
@@ -13,10 +13,11 @@ class ErlangAT22 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "bf9e22eb6c20285d33d208ef2097efa88bacf01da8447e4cc0a59ef0de70e8ed"
-    sha256 cellar: :any, big_sur:       "cfba73ed8488d94e9f8ca6f9b36fd9f0bbd2d2399b382a33202ffcd70743b9c4"
-    sha256 cellar: :any, catalina:      "89beffd9e5522bc326988a12ac254a9255f767f47df92a8dd5f952a3a0843d53"
-    sha256 cellar: :any, mojave:        "62dbe9073c0354474c8e1a52ca24a84c96cac266066ce0d1b386b40863342e6c"
+    sha256 cellar: :any,                 arm64_big_sur: "bf9e22eb6c20285d33d208ef2097efa88bacf01da8447e4cc0a59ef0de70e8ed"
+    sha256 cellar: :any,                 big_sur:       "cfba73ed8488d94e9f8ca6f9b36fd9f0bbd2d2399b382a33202ffcd70743b9c4"
+    sha256 cellar: :any,                 catalina:      "89beffd9e5522bc326988a12ac254a9255f767f47df92a8dd5f952a3a0843d53"
+    sha256 cellar: :any,                 mojave:        "62dbe9073c0354474c8e1a52ca24a84c96cac266066ce0d1b386b40863342e6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2486f5eaa04c8bd863847f77c9999b0feaea7e995dca8ff82da5616807aadb89"
   end
 
   keg_only :versioned_formula
