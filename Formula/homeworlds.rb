7ad53e54c68bbc8e3349b2039cd0792445dcdf3e
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-06-24 20:49:02 +0000

    homeworlds: update 20141022_1 bottle.

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -8,10 +8,11 @@ class Homeworlds < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any, arm64_big_sur: "c094bde41b9eeccc8c9d62b2e7e808f4ca9addfeb19f81a2e2e41f26f3871363"
-    sha256 cellar: :any, big_sur:       "6b3e8e07aa49deb2996591fe781c0442f1812c8425665da5f83bb857428e57e4"
-    sha256 cellar: :any, catalina:      "bc82b3b105c956df5e7f629c0ac69f29cb861992b2476a8228692f333f12854a"
-    sha256 cellar: :any, mojave:        "85ac4667ac133f9a45870166c29a7c0dc1ed7cbb51216db987fb8601a001ebf6"
+    sha256 cellar: :any,                 arm64_big_sur: "c094bde41b9eeccc8c9d62b2e7e808f4ca9addfeb19f81a2e2e41f26f3871363"
+    sha256 cellar: :any,                 big_sur:       "6b3e8e07aa49deb2996591fe781c0442f1812c8425665da5f83bb857428e57e4"
+    sha256 cellar: :any,                 catalina:      "bc82b3b105c956df5e7f629c0ac69f29cb861992b2476a8228692f333f12854a"
+    sha256 cellar: :any,                 mojave:        "85ac4667ac133f9a45870166c29a7c0dc1ed7cbb51216db987fb8601a001ebf6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48b54ac2e57adbdd468f98d18a0d454b2dc63f00c92697463a63a2eb6483ebe3"
   end
 
   depends_on "wxmac"
