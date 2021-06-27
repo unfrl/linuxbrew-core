cceb5e697652944d44145de5d609fcfe1ec0f888
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-27 08:15:02 +0000

    idris2: update 0.4.0 bottle.

--- Formula/idris2.rb
+++ Formula/idris2.rb
@@ -7,9 +7,10 @@ class Idris2 < Formula
   head "https://github.com/idris-lang/Idris2.git"
 
   bottle do
-    sha256 cellar: :any, big_sur:  "e323eab293f27d82f14b3ab5e68023db2cbaca8d26b911d96a7c7d8971d44b53"
-    sha256 cellar: :any, catalina: "5919c36d63f78921d94062b46927e7dd6ae52ea6c5318b599e9522d28b1cc243"
-    sha256 cellar: :any, mojave:   "847646c54f71dea739dfb24e167c41d8a14c9c02e1f6d08fa639d5a468c6d4cf"
+    sha256 cellar: :any,                 big_sur:      "e323eab293f27d82f14b3ab5e68023db2cbaca8d26b911d96a7c7d8971d44b53"
+    sha256 cellar: :any,                 catalina:     "5919c36d63f78921d94062b46927e7dd6ae52ea6c5318b599e9522d28b1cc243"
+    sha256 cellar: :any,                 mojave:       "847646c54f71dea739dfb24e167c41d8a14c9c02e1f6d08fa639d5a468c6d4cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "96a178dcc80e65b388d4a1e245f718221efdd3e684ba8d9adc37c7b390b2e5e9"
   end
 
   depends_on "gmp" => :build
