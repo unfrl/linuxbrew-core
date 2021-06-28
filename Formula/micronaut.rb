0b5d98f175bb6649a036e537298a919ce0451715
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-28 17:29:52 +0000

    micronaut: update 2.5.7 bottle.

--- Formula/micronaut.rb
+++ Formula/micronaut.rb
@@ -15,6 +15,7 @@ class Micronaut < Formula
     sha256 cellar: :any_skip_relocation, big_sur:       "527c4672ad95f7a9648afc51aaef352f2d2f4c8614603c2770d76156f58a867a"
     sha256 cellar: :any_skip_relocation, catalina:      "2198fd9de75e213ba610de94229b609765dfca01f80a45b0d0a2bd347522b8cf"
     sha256 cellar: :any_skip_relocation, mojave:        "a99c9da5ef8e114bbc7ca11fde8f1ab332ab6c9192d6ab660117157dc287a38a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a92f23521e8562161f693e0dae0cc48d46244fa41a87727deab04f50d846e13"
   end
 
   if Hardware::CPU.arm?
