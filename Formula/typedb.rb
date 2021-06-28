c8fafe9089e3b3353834dc9a2b656d59f7d84a65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-28 22:52:30 +0000

    typedb: add 2.2.0 bottle.

--- Formula/typedb.rb
+++ Formula/typedb.rb
@@ -5,6 +5,10 @@ class Typedb < Formula
   sha256 "906f4e101c377826c61dc3d7a7958611cb0ee31d240989a58666499c6b8f08d5"
   license "AGPL-3.0-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "615fd8748ff58c366219a35058cc59dc90b06ad626ceb36295893c3891d84880"
+  end
+
   depends_on "openjdk@11"
 
   def install
