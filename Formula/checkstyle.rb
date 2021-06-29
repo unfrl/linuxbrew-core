1fc7a1fd106694dddfa2ec2198dcfa90dbed3d05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-06-29 05:14:47 +0000

    checkstyle: add 8.44 bottle.

--- Formula/checkstyle.rb
+++ Formula/checkstyle.rb
@@ -5,6 +5,10 @@ class Checkstyle < Formula
   sha256 "b574d4e40e26290d030d6084dd55170777db6a71f214d488713309dcd4658fa0"
   license "LGPL-2.1-or-later"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "54ddf28018c443775f9e06bd37ced4e199819df669b00d8769f2d1eef7623b11"
+  end
+
   depends_on "openjdk"
 
   def install
