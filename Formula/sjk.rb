5d5a3ebadfd88e16f9ea9437d0543ed9a9a2bff5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2021-07-06 09:49:40 +0000

    sjk: add 0.18 bottle.

--- Formula/sjk.rb
+++ Formula/sjk.rb
@@ -5,6 +5,10 @@ class Sjk < Formula
   sha256 "583284174dde91c32acdef01550d376b2e15be8573e5d99007651451f5bc8854"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fbef52d37081d50d8822e1b3582d5311c645c5c8c47c613336b298c78cb8ff81"
+  end
+
   depends_on "openjdk"
 
   def install
