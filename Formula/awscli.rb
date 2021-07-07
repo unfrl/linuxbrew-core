9530d134d7d95f05b5ddff9b55d952a140db58a5
Michka Popoff <michkapopoff@gmail.com>
2021-07-07 09:09:32 +0000

    awscli: remove sha256 after merge

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -13,7 +13,6 @@ class Awscli < Formula
     sha256                               big_sur:       "1cdbd794a59c4ce0cb0724808b1a0d4b6938c9bbca5e36b49fabc3c20957eb96"
     sha256                               catalina:      "79173c4c1567e57014118db6510db89c1773a64dcf32d288d52cce8d1e477bd0"
     sha256                               mojave:        "09f57750b94d3b7470127a8c43cd1f22cd14305b60c395a3163e31754274820c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d43d101a493b1a89a0a3be53bcb7bbd812c86880d87acdbc7635f1a51cc2500"
   end
 
   depends_on "cmake" => :build
