89a002aa79c7a72168abfc7454dfd8ad348a8991
Michka Popoff <3406519+iMichka@users.noreply.github.com>
2021-07-08 11:16:11 +0000

    snowpack: update 3.8.0 bottle.

--- Formula/snowpack.rb
+++ Formula/snowpack.rb
@@ -12,6 +12,7 @@ class Snowpack < Formula
     sha256                               big_sur:       "8b402108bf4fd23aa5af95c109a3948bb8f59edd5294617fcca8010ccefb31d2"
     sha256                               catalina:      "429f76f8b69fea46562aa2f3d5a901416516f0d2db6f2c431f2473f3bc0e6ee0"
     sha256                               mojave:        "c281ae5b29f9a6b48dee884c2fb46e17c68dda31d914ef1ea6a0c1c19befb2db"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6b5c9d01b1aae29f5b988d3f6af76952b8418573f0cb3ae7065101a0e3329d4"
   end
 
   depends_on "node"
