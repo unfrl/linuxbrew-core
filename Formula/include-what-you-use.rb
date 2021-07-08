0a617f086b485b84c1b63a914104798bc1830b82
Michka Popoff <michkapopoff@gmail.com>
2021-07-08 16:19:21 +0000

    include-what-you-use: delete after merge

--- Formula/include-what-you-use.rb
+++ Formula/include-what-you-use.rb
@@ -19,7 +19,6 @@ class IncludeWhatYouUse < Formula
     sha256 cellar: :any,                 big_sur:       "6added7e28d64e99c0cd5f8a20bf70b22e96e22a0a730c5f13387f21e24d1c9c"
     sha256 cellar: :any,                 catalina:      "8c0e40ec7d0a3b930e4f3e1584c7f8e5a535bad43ce34b3d9ce686d92357feb4"
     sha256 cellar: :any,                 mojave:        "ec174811dd5ff4aafa13ee2d58d5f35a83e899522bcfefe1f841485c529244ff"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23ae2f9158f619f8b7ddb40e63bd257f0cfab08ca82f8e72f9a6e2aa8a40663c"
   end
 
   depends_on "cmake" => :build
