3c11c1e1b42968940cf904ae67aaf1b044ed8e1c
Michka Popoff <michkapopoff@gmail.com>
2021-07-08 21:40:13 +0200

    hadolint: remove workaround (#23764)

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -19,12 +19,6 @@ class Hadolint < Formula
   uses_from_macos "xz"
 
   def install
-    unless OS.mac?
-      gmp = Formula["gmp"]
-      ENV.prepend_path "LD_LIBRARY_PATH", gmp.lib
-      ENV.prepend_path "LIBRARY_PATH", gmp.lib
-    end
-
     # Let `stack` handle its own parallelization
     jobs = ENV.make_jobs
     ENV.deparallelize
