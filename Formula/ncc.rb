b06ef12c3990b481acd17e86bd8d45eb985a20c3
Michka Popoff <michkapopoff@gmail.com>
2021-07-08 06:37:53 +0000

    ncc: delete bottle after merge

--- Formula/ncc.rb
+++ Formula/ncc.rb
@@ -7,11 +7,6 @@ class Ncc < Formula
   sha256 "1d59e41eaea1c38f83805ef89774b9c76786c5729b54df00a2cdfc74915d6254"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "08a7c339b0b3b35d4f7a6973323ba0e11569cf305c278bf7ee29da060c2e2ad6"
-    sha256 cellar: :any_skip_relocation, all:          "990f7be8de80e67b76e5000d560137e24833c70fa7b43327e0d0434111c3442d"
-  end
-
   depends_on "node"
 
   def install
