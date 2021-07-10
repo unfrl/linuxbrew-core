5f5b47f540aabcfb183d1b00b5b3f1068a8ac00d
Nuri Jung <jnooree@snu.ac.kr>
2021-07-11 06:48:22 +0900

    onedrive: update to v2.4.12 (#23737)
    * Update onedrive to v2.4.12

--- Formula/onedrive.rb
+++ Formula/onedrive.rb
@@ -1,13 +1,8 @@
 class Onedrive < Formula
   desc "Folder synchronization with OneDrive"
   homepage "https://github.com/abraunegg/onedrive"
-  url "https://github.com/abraunegg/onedrive/archive/v2.4.5.tar.gz"
-  sha256 "1f1f5e1f2f37376b6d96bda2426552a94a8b195f545b4fb7f3668c4fe2e8f6a0"
-  revision 1
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5e35136dbec90713f07ceffcebbbad5605e805c7c7a03444a42cfdba4b9e9893" # linuxbrew-core
-  end
+  url "https://github.com/abraunegg/onedrive/archive/v2.4.12.tar.gz"
+  sha256 "4f6aa46fc28e859b63c0a2c190f66c3286ec0bde3f54f77af0ddea6b62bba37a"
 
   depends_on "dmd" => :build
   depends_on "pkg-config" => :build
@@ -15,12 +10,18 @@ class Onedrive < Formula
   depends_on :linux
   depends_on "sqlite"
 
+  patch do
+    url "https://github.com/abraunegg/onedrive/commit/761cf3eb878fd370576329127055eec06b975672.patch?full_index=1"
+    sha256 "9a892e5e6bed7695754d49f9561d59f22180db7340493ec7f167bee767b8c28e"
+  end
+
   def install
     ENV["DC"] = "dmd"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
-    bash_completion.install "contrib/completions/complete.bash"
+    bash_completion.install "contrib/completions/complete.bash" => "onedrive"
     zsh_completion.install "contrib/completions/complete.zsh" => "_onedrive"
+    fish_completion.install "contrib/completions/complete.fish" => "onedrive.fish"
   end
 
   test do
